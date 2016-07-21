203365824645cdae5565bf289244be2478a627ca
Aleksey Sidorov <aleksei.sidorov@xdev.re>
2016-07-21 15:54:10 +0300

    Move db related code into db.rs

---
+++ src/storage/db.rs
@@ -0,0 +1,70 @@
+use std::collections::BTreeMap;
+
+use super::{Map, Error};
+
+// TODO In this implementation there are extra memory allocations when key is passed into specific database.
+// Think about key type. Maybe we can use keys with fixed length?
+pub trait Database: Map<[u8], Vec<u8>> + Sized {
+    fn fork<'a>(&'a self) -> Fork<'a, Self> {
+        Fork {
+            database: self,
+            changes: BTreeMap::new(),
+        }
+    }
+
+    fn merge(&mut self, patch: Patch) -> Result<(), Error>;
+}
+
+pub enum Change {
+    Put(Vec<u8>),
+    Delete,
+}
+
+pub struct Patch {
+    pub changes: BTreeMap<Vec<u8>, Change>,
+}
+
+pub struct Fork<'a, T: Database + 'a> {
+    database: &'a T,
+    changes: BTreeMap<Vec<u8>, Change>,
+}
+
+impl<'a, T: Database + 'a> Fork<'a, T> {
+    pub fn patch(self) -> Patch {
+        Patch { changes: self.changes }
+    }
+}
+
+impl<'a, T> Map<[u8], Vec<u8>> for Fork<'a, T>
+    where T: Database + 'a
+{
+    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, Error> {
+        match self.changes.get(key) {
+            Some(change) => {
+                let v = match *change {
+                    Change::Put(ref v) => Some(v.clone()),
+                    Change::Delete => None,
+                };
+                Ok(v)
+            }
+            None => self.database.get(key),
+        }
+    }
+
+    fn put(&mut self, key: &[u8], value: Vec<u8>) -> Result<(), Error> {
+        self.changes.insert(key.to_vec(), Change::Put(value));
+        Ok(())
+    }
+
+    fn delete(&mut self, key: &[u8]) -> Result<(), Error> {
+        self.changes.insert(key.to_vec(), Change::Delete);
+        Ok(())
+    }
+}
+
+impl<'a, T: Database + 'a + ?Sized> Database for Fork<'a, T> {
+    fn merge(&mut self, patch: Patch) -> Result<(), Error> {
+        self.changes.extend(patch.changes.into_iter());
+        Ok(())
+    }
+}
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -3,29 +3,27 @@ mod tests;
 
 use std::slice::SliceConcatExt;
 use std::convert::AsRef;
-use std::sync::Arc;
 use std::collections::BTreeMap;
-use std::mem;
 use std::fmt::Debug;
-use std::marker::PhantomData;
-use std::cell::Cell;
 use std::num::{Zero, One};
 use std::ops::{Add, Sub};
 
-use byteorder::{ByteOrder, BigEndian};
-
 use ::crypto::Hash;
-use ::messages::{MessageBuffer, Message, TxMessage, Precommit, Propose};
+use ::messages::{TxMessage, Precommit, Propose};
 
 mod leveldb;
 mod memorydb;
 mod map_table;
 mod list_table;
+mod fields;
+mod db;
 
+pub use self::db::{Database, Fork, Patch, Change};
 pub use self::memorydb::MemoryDB;
 pub use self::leveldb::LevelDB;
 pub use self::map_table::MapTable;
 pub use self::list_table::ListTable;
+pub use self::fields::{StorageValue};
 
 pub struct Storage<T: Database> {
     db: T,
@@ -84,145 +82,6 @@ impl<'a, T> Storage<Fork<'a, T>>
 {
     pub fn patch(self) -> Patch {
         self.db.patch()
-    }
-}
-
-// TODO In this implementation there are extra memory allocations when key is passed into specific database.
-// Think about key type. Maybe we can use keys with fixed length?
-pub trait Database: Map<[u8], Vec<u8>> + Sized {
-    fn fork<'a>(&'a self) -> Fork<'a, Self> {
-        Fork {
-            database: self,
-            changes: BTreeMap::new(),
-        }
-    }
-
-    fn merge(&mut self, patch: Patch) -> Result<(), Error>;
-}
-
-pub enum Change {
-    Put(Vec<u8>),
-    Delete,
-}
-
-pub struct Patch {
-    changes: BTreeMap<Vec<u8>, Change>,
-}
-
-pub struct Fork<'a, T: Database + 'a> {
-    database: &'a T,
-    changes: BTreeMap<Vec<u8>, Change>,
-}
-
-impl<'a, T: Database + 'a> Fork<'a, T> {
-    fn patch(self) -> Patch {
-        Patch { changes: self.changes }
-    }
-}
-
-impl<'a, T> Map<[u8], Vec<u8>> for Fork<'a, T>
-    where T: Database + 'a
-{
-    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, Error> {
-        match self.changes.get(key) {
-            Some(change) => {
-                let v = match *change {
-                    Change::Put(ref v) => Some(v.clone()),
-                    Change::Delete => None,
-                };
-                Ok(v)
-            }
-            None => self.database.get(key),
-        }
-    }
-
-    fn put(&mut self, key: &[u8], value: Vec<u8>) -> Result<(), Error> {
-        self.changes.insert(key.to_vec(), Change::Put(value));
-        Ok(())
-    }
-
-    fn delete(&mut self, key: &[u8]) -> Result<(), Error> {
-        self.changes.insert(key.to_vec(), Change::Delete);
-        Ok(())
-    }
-}
-
-impl<'a, T: Database + 'a + ?Sized> Database for Fork<'a, T> {
-    fn merge(&mut self, patch: Patch) -> Result<(), Error> {
-        self.changes.extend(patch.changes.into_iter());
-        Ok(())
-    }
-}
-
-pub trait StorageValue {
-    fn serialize(self) -> Vec<u8>;
-    fn deserialize(v: Vec<u8>) -> Self;
-}
-
-impl StorageValue for u32 {
-    // TODO: return Cow<[u8]>
-    fn serialize(self) -> Vec<u8> {
-        let mut v = vec![0; mem::size_of::<u32>()];
-        BigEndian::write_u32(&mut v, self);
-        v
-    }
-
-    fn deserialize(v: Vec<u8>) -> Self {
-        BigEndian::read_u32(&v)
-    }
-}
-
-impl StorageValue for u64 {
-    fn serialize(self) -> Vec<u8> {
-        let mut v = vec![0; mem::size_of::<u64>()];
-        BigEndian::write_u64(&mut v, self);
-        v
-    }
-
-    fn deserialize(v: Vec<u8>) -> Self {
-        BigEndian::read_u64(&v)
-    }
-}
-
-impl StorageValue for Hash {
-    fn serialize(self) -> Vec<u8> {
-        self.as_ref().to_vec()
-    }
-
-    fn deserialize(v: Vec<u8>) -> Self {
-        Hash::from_slice(&v).unwrap()
-    }
-}
-
-impl<T> StorageValue for T
-    where T: Message
-{
-    fn serialize(self) -> Vec<u8> {
-        self.raw().as_ref().as_ref().to_vec()
-    }
-
-    fn deserialize(v: Vec<u8>) -> Self {
-        Message::from_raw(Arc::new(MessageBuffer::from_vec(v))).unwrap()
-    }
-}
-
-impl StorageValue for TxMessage {
-    fn serialize(self) -> Vec<u8> {
-        self.raw().as_ref().as_ref().to_vec()
-    }
-
-    fn deserialize(v: Vec<u8>) -> Self {
-        TxMessage::from_raw(Arc::new(MessageBuffer::from_vec(v))).unwrap()
-    }
-}
-
-impl StorageValue for Vec<u8> {
-    fn serialize(self) -> Vec<u8> {
-        self
-    }
-
-    fn deserialize(v: Vec<u8>) -> Self {
-        v
     }
 }
 
