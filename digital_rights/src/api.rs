bacefaf734f1642e6e56df39645fc4eb441cab6d
Aleksey Sidorov <aleksei.sidorov@xdev.re>
2016-10-07 17:14:34 +0300

    More information about owners and distributors

--- digital_rights/src/api.rs
+++ digital_rights/src/api.rs
@@ -5,7 +5,7 @@ use exonum::storage::{Map, List, Database, Result as StorageResult};
 use exonum::blockchain::Blockchain;
 use blockchain_explorer::{TransactionInfo, HexField};
 
-use super::{Role,DigitalRightsTx, DigitalRightsBlockchain, ContentShare, Uuid, Fingerprint, Content, Contract};
+use super::{Role,DigitalRightsTx, DigitalRightsBlockchain, ContentShare, Uuid, Fingerprint, Content, Contract, Report, Ownership};
 
 impl Serialize for DigitalRightsTx {
     fn serialize<S>(&self, ser: &mut S) -> Result<(), S::Error>
@@ -66,7 +66,8 @@ pub struct OwnerInfo {
     pub role: &'static str,
     pub name: String,
     pub pub_key: HexField<PublicKey>,
-    pub ownership: HexField<Hash>,
+    pub ownership_hash: HexField<Hash>,
+    pub ownership: Vec<OwnershipInfo>,
 }
 
 #[derive(Debug, Serialize)]
@@ -92,10 +93,30 @@ pub struct ContentInfo {
 
 #[derive(Debug, Serialize)]
 pub struct ContractInfo {
-    pub fingerprint: HexField<Fingerprint>,
+    pub content: ContentInfo,
     pub plays: u64,
     pub amount: u64,
     pub reports_hash: HexField<Hash>,
+    pub reports: Vec<ReportInfo>
+}
+
+#[derive(Debug, Serialize)]
+pub struct OwnershipInfo {
+    pub content: ContentInfo,
+    pub plays: u64,
+    pub amount: u64,
+    pub reports_hash: HexField<Hash>,
+    pub reports: Vec<ReportInfo>
+}
+
+#[derive(Debug, Serialize)]
+pub struct ReportInfo {
+    pub distributor_id: u16,
+    pub fingerprint: HexField<Fingerprint>,
+    pub time: u64,
+    pub plays: u64,
+    pub amount: u64,
+    pub comment: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -118,15 +139,29 @@ pub struct NewReport {
 }
 
 impl ContractInfo {
-    pub fn new(contract: Contract) -> ContractInfo {
+    pub fn new(contract: Contract, content: ContentInfo, reports: Vec<ReportInfo>) -> ContractInfo {
         ContractInfo {
-            fingerprint: HexField(*contract.fingerprint()),
+            content: content,
             plays: contract.plays(),
             amount: contract.amount(),
-            reports_hash: HexField(*contract.reports_hash())
+            reports_hash: HexField(*contract.reports_hash()),
+            reports: reports
         }
     }
 }
+
+impl OwnershipInfo {
+    pub fn new(ownership: Ownership, content: ContentInfo, reports: Vec<ReportInfo>) -> OwnershipInfo {
+        OwnershipInfo {
+            content: content,
+            plays: ownership.plays(),
+            amount: ownership.amount(),
+            reports_hash: HexField(*ownership.reports_hash()),
+            reports: reports
+        }
+    }
+}
+
 
 impl ContentInfo {
     pub fn new(fingerprint: Fingerprint, content: Content) -> ContentInfo {
@@ -137,6 +172,21 @@ impl ContentInfo {
             price_per_listen: content.price_per_listen(),
             min_plays: content.min_plays(),
             distributors: content.distributors().into()
+        }
+    }
+}
+
+impl ReportInfo {
+    pub fn new(report: Report) -> ReportInfo {
+        let time = report.time();
+        let nsec = (time.sec as u64) * 1_000_000_000 + time.nsec as u64;
+        ReportInfo {
+            distributor_id: report.distributor_id(),
+            fingerprint: HexField(*report.fingerprint()),
+            time: nsec,
+            plays: report.plays(),
+            amount: report.amount(),
+            comment: report.comment().into(),
         }
     }
 }
@@ -158,12 +208,23 @@ impl<D: Database> DigitalRightsApi<D> {
     pub fn owner_info(&self, id: u16) -> StorageResult<Option<OwnerInfo>> {
         let view = self.blockchain.view();
         if let Some(owner) = view.owners().get(id as u64)? {
+            let mut ownership = Vec::new();
+            for owner_content in view.owner_contents(id).values()? {
+                let fingerprint = owner_content.fingerprint().clone();
+                let r = view.contents().get(&fingerprint)?.unwrap();
+                let content = ContentInfo::new(fingerprint, r);
+                let reports = self.find_reports(Role::Owner(id), &fingerprint)?;
+
+                ownership.push(OwnershipInfo::new(owner_content, content, reports));
+            }
+
             let info = OwnerInfo {
                 id: id,
                 role: "owner",
                 name: owner.name().to_string(),
                 pub_key: HexField(*owner.pub_key()),
-                ownership: HexField(*owner.ownership_hash()),
+                ownership_hash: HexField(*owner.ownership_hash()),
+                ownership: ownership
             };
             Ok(Some(info))
         } else {
@@ -177,7 +238,12 @@ impl<D: Database> DigitalRightsApi<D> {
             let available_content = self.available_contents(id)?;
             let mut contracts = Vec::new();            
             for contract in view.distributor_contracts(id).values()? {
-                contracts.push(ContractInfo::new(contract));
+                let fingerprint = contract.fingerprint().clone();
+                let r = view.contents().get(&fingerprint)?.unwrap();
+                let content = ContentInfo::new(fingerprint, r);
+                let reports = self.find_reports(Role::Distributor(id), &fingerprint)?;
+
+                contracts.push(ContractInfo::new(contract, content, reports));
             }
 
             let info = DistributorInfo {
@@ -203,6 +269,29 @@ impl<D: Database> DigitalRightsApi<D> {
             .filter(|&(_, ref content)| !content.distributors().contains(&distributor_id))
             .map(|(fingerprint, content)| ContentInfo::new(fingerprint, content))
             .collect();
+        Ok(v)
+    }
+
+    pub fn find_reports(&self, id: Role, fingerprint: &Fingerprint) -> StorageResult<Vec<ReportInfo>> {
+        let view = self.blockchain.view();
+        let uuids = match id {
+            Role::Owner(id) => {
+                view
+                    .owner_reports(id, fingerprint)
+                    .values()?
+            }
+            Role::Distributor(id) => {
+                view
+                    .distributor_reports(id, fingerprint)
+                    .values()?
+            }
+        };
+
+        let mut v = Vec::new();
+        for uuid in uuids {
+            let report = view.reports().get(&uuid)?.unwrap();
+            v.push(ReportInfo::new(report));
+        }
         Ok(v)
     }
 }
