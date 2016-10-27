4bf718c7db1ea4a360a1101571e54436be63e945
Aleksey Sidorov <aleksei.sidorov@xdev.re>
2016-10-27 12:54:41 +0300

    Fix typo

--- exonum/src/messages/error.rs
+++ exonum/src/messages/error.rs
@@ -2,7 +2,7 @@
 pub enum Error {
     UnexpectedlyShortPayload { actual_size: u32, minimum_size: u32 },
     IncorrectBoolean { position: u32, value: u8 },
-    IncorrectSegmentRefference { position: u32, value: u32 },
+    IncorrectSegmentReference { position: u32, value: u32 },
     IncorrectSegmentSize { position: u32, value: u32 },
     Utf8 {
         position: u32,
