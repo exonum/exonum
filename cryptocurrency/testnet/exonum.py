821aab812d0d62de3a219b704d01bc86d866d2a0
Aleksey Sidorov <aleksei.sidorov@xdev.re>
2016-11-01 15:36:00 +0300

    Refactor testnet run script

--- cryptocurrency/testnet/exonum.py
+++ cryptocurrency/testnet/exonum.py
@@ -24,13 +24,14 @@ class ExonumApi(unittest.TestCase):
         self.timeout = 1
         # Configure http session
         self.session = requests.Session()
-        retries = Retry(total=20,
+        retries = Retry(total=10,
                 backoff_factor=1,
                 status_forcelist=[ 500, 502, 503, 504 ])
         self.session.mount('http://', HTTPAdapter(max_retries=retries))
 
     def tearDown(self):
         self.session.close()
+        self.session = None
     
     def url(self, endpoint):
         return self.host + "/" + endpoint
