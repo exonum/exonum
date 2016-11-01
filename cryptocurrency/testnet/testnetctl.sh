9be286653f6b55e47e68f8cb9a7a9615d2dcc375
Aleksey Sidorov <aleksei.sidorov@xdev.re>
2016-11-01 16:26:15 +0300

    Add presets for testnetctl and restore deploy

--- cryptocurrency/testnet/testnetctl.sh
+++ cryptocurrency/testnet/testnetctl.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-node_executable=cryptocurrency
 destdir=$2
+template=$3
+startport=$4
+
+node_executable=cryptocurrency
 scriptdir=`dirname "$BASH_SOURCE"`
 supervisor_conf=${destdir}/etc/supervisord.conf
 sock_file=/tmp/
@@ -10,18 +13,18 @@ start() {
     mkdir -p ${destdir}/log/supervisor
     mkdir ${destdir}/run
     mkdir ${destdir}/db
-    cp -R ${scriptdir}/etc ${destdir}
-    $node_executable -c ${destdir}/testnet.conf generate 9 -p 9000
+    cp -R ${scriptdir}/${template}/etc ${destdir} || exit 1
+    $node_executable -c ${destdir}/testnet.conf generate 9 -p ${startport} || exit 1
     cd $destdir
-    supervisord
-    supervisorctl start leveldb:*
+    supervisord || exit 1
+    supervisorctl start leveldb:* || exit 1
 }
 
 stop() {
     cd ${destdir}
-    if [ -e /tmp/supervisor_tst_cryptocurrency.sock ]
+    if [ -e /tmp/supervisor_${template}_cryptocurrency.sock ]
     then
-        supervisorctl shutdown
+        supervisorctl shutdown || exit 1
     fi 
     rm -rf "./db"
     rm -rf "./etc"
