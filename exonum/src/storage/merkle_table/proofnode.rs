6a106ec01814fa68446b0ce267baefdc9feff178
Oleg Kuzmin <oleg.kuzmin@xdev.re>
2016-11-28 21:27:56 +0200

    Split merkle_patricia_table. Removed Base64Field. Implemented manual serialization for ProofPathToKey.
    Former-commit-id: 43a8d6383add6673f18e03af3cb8865ae613df3d

--- exonum/src/storage/merkle_table/proofnode.rs
+++ exonum/src/storage/merkle_table/proofnode.rs
@@ -1,12 +1,12 @@
 use ::storage::StorageValue;
 use ::storage::utils::bytes_to_hex;
-use base64::encode;
+use ::storage::fields::repr_stor_val; 
 use ::crypto::{hash, Hash};
 use std::fmt;
 use serde::{Serialize, Serializer};
-const LEFT_DESC: &'static str = "left"; 
-const RIGHT_DESC: &'static str = "right"; 
-const VAL_DESC: &'static str = "value"; 
+const LEFT_DESC: &'static str = "L";
+const RIGHT_DESC: &'static str = "R";
+const VAL_DESC: &'static str = "val";
 #[allow(dead_code)]
 pub fn proof_indices_values<V: StorageValue + Clone>(proof: &Proofnode<V>) -> Vec<(usize, &V)> {
     let mut res = Vec::new();
@@ -21,47 +21,42 @@ pub enum Proofnode<V: StorageValue + Clone> {
     Leaf(V),
 }
 
-fn representation_storage_value<T: StorageValue + Clone>(value: &T) -> String {
-    let vec_bytes = value.clone().serialize(); 
-    encode(&vec_bytes)
-}
-
-impl<V:StorageValue + Clone> Serialize for Proofnode<V> {
-    
+impl<V: StorageValue + Clone> Serialize for Proofnode<V> {
     fn serialize<S>(&self, ser: &mut S) -> Result<(), S::Error>
-        where S: Serializer {
-            use self::Proofnode::*; 
-            let mut state; 
-            match *self {
-                Full(ref left_proof, ref right_proof) => {
-                    state = ser.serialize_struct("Full", 2)?; 
+        where S: Serializer
+    {
+        use self::Proofnode::*;
+        let mut state;
+        match *self {
+            Full(ref left_proof, ref right_proof) => {
+                state = ser.serialize_struct("Full", 2)?;
+                ser.serialize_struct_elt(&mut state, LEFT_DESC, left_proof)?;
+                ser.serialize_struct_elt(&mut state, RIGHT_DESC, right_proof)?;
+            } 
+            Left(ref left_proof, ref option_hash) => {
+                if let Some(ref hash) = *option_hash {
+                    state = ser.serialize_struct("Left", 2)?;
+                    let hash_repr = repr_stor_val(hash);
                     ser.serialize_struct_elt(&mut state, LEFT_DESC, left_proof)?;
-                    ser.serialize_struct_elt(&mut state, RIGHT_DESC, right_proof)?; 
-                }, 
-                Left(ref left_proof, ref option_hash) => {
-                    if let Some(ref hash) = *option_hash {
-                        state = ser.serialize_struct("Left", 2)?; 
-                        let hash_repr = representation_storage_value(hash);
-                        ser.serialize_struct_elt(&mut state, LEFT_DESC, left_proof)?;
-                        ser.serialize_struct_elt(&mut state, RIGHT_DESC, hash_repr)?;
-                    } else {
-                        state = ser.serialize_struct("Left", 1)?;
-                        ser.serialize_struct_elt(&mut state, LEFT_DESC, left_proof)?;
-                    }
-                }, 
-                Right(ref hash, ref right_proof) => {
-                    state = ser.serialize_struct("Right", 2)?; 
-                    let hash_repr = representation_storage_value(hash);
-                    ser.serialize_struct_elt(&mut state, LEFT_DESC, hash_repr)?;                     
-                    ser.serialize_struct_elt(&mut state, RIGHT_DESC, right_proof)?;
-                }, 
-                Leaf(ref val) => {
-                    state = ser.serialize_struct("Leaf", 1)?;
-                    let val_repr = representation_storage_value(val);
-                    ser.serialize_struct_elt(&mut state, VAL_DESC, val_repr)?; 
+                    ser.serialize_struct_elt(&mut state, RIGHT_DESC, hash_repr)?;
+                } else {
+                    state = ser.serialize_struct("Left", 1)?;
+                    ser.serialize_struct_elt(&mut state, LEFT_DESC, left_proof)?;
                 }
+            } 
+            Right(ref hash, ref right_proof) => {
+                state = ser.serialize_struct("Right", 2)?;
+                let hash_repr = repr_stor_val(hash);
+                ser.serialize_struct_elt(&mut state, LEFT_DESC, hash_repr)?;
+                ser.serialize_struct_elt(&mut state, RIGHT_DESC, right_proof)?;
+            } 
+            Leaf(ref val) => {
+                state = ser.serialize_struct("Leaf", 1)?;
+                let val_repr = repr_stor_val(val);
+                ser.serialize_struct_elt(&mut state, VAL_DESC, val_repr)?;
             }
-            ser.serialize_struct_end(state)
+        }
+        ser.serialize_struct_end(state)
     }
 }
 
@@ -107,23 +102,24 @@ impl<V: StorageValue + Clone> Proofnode<V> {
 
 impl<V: StorageValue + fmt::Debug + Clone> fmt::Debug for Proofnode<V> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use self::Proofnode::*; 
+        use self::Proofnode::*;
         match *self {
-            Full(ref left, ref right) => {
-                write!(f, "{{ left: {:?}, right: {:?} }}", left, right)
-            }
+            Full(ref left, ref right) => write!(f, "{{ left: {:?}, right: {:?} }}", left, right),
             Left(ref left_proof, ref right_hash) => {
-                let hash_repr: String; 
+                let hash_repr: String;
                 if let Some(ref digest) = *right_hash {
-                    hash_repr = bytes_to_hex(digest); 
+                    hash_repr = bytes_to_hex(digest);
                 } else {
-                    hash_repr = "None".to_string(); 
+                    hash_repr = "None".to_string();
                 }
-                write!(f, "{{ left: {:?}, right_hash: {:?} }}", left_proof, hash_repr)
+                write!(f,
+                       "{{ left: {:?}, right_hash: {:?} }}",
+                       left_proof,
+                       hash_repr)
             } 
             Right(ref left_hash, ref right) => {
-                let hash_repr: String; 
-                hash_repr = bytes_to_hex(left_hash); 
+                let hash_repr: String;
+                hash_repr = bytes_to_hex(left_hash);
                 write!(f, "{{ left_hash: {:?}, right: {:?} }}", hash_repr, right)
             }
             Leaf(ref val) => write!(f, "{{ val: {:?} }}", val), 
