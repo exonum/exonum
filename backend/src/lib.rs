46fb6baa898d8dcc4000e07eb6c8d6cd2295c28d
Aleksey Sidorov <aleksei.sidorov@xdev.re>
2016-12-05 17:59:46 +0300

    Store file hashes instead files in blockchain

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -16,19 +16,15 @@ use exonum::blockchain::{View, Blockchain};
 use blockchain_explorer::TransactionInfo;
 
 pub const TIMESTAMPING_TRANSACTION_MESSAGE_ID: u16 = 128;
-pub const TIMESTAMPING_FILE_SIZE_LIMIT: u64 = 20 * 1024 * 1024;
 
 message! {
     TimestampTx {
         const ID = TIMESTAMPING_TRANSACTION_MESSAGE_ID;
-        const SIZE = 72;
+        const SIZE = 48;
 
-        file_name:      &str        [00 => 08]
-        description:    &str        [08 => 16]
-        mime:           &str        [16 => 24]
-        time:           Timespec    [24 => 32]
-        hash:           &Hash       [32 => 64]
-        data:           &[u8]       [64 => 72]
+        description:    &str        [00 => 08]
+        time:           Timespec    [08 => 16]
+        hash:           &Hash       [16 => 48]
     }
 }
 
@@ -42,14 +38,11 @@ message! {
 
 storage_value! {
     Content {
-        const SIZE = 72;
+        const SIZE = 48;
 
-        file_name:          &str        [00 => 08]
-        description:        &str        [08 => 16]
-        mime:               &str        [16 => 24]
-        time:               Timespec    [24 => 32]
-        data_hash:          &Hash       [32 => 64]
-        data:               &[u8]       [64 => 72]
+        description:        &str        [00 => 08]
+        time:               Timespec    [08 => 16]
+        data_hash:          &Hash       [16 => 48]
     }
 }
 
@@ -58,7 +51,6 @@ impl Serialize for TimestampTx {
         where S: Serializer
     {
         let mut state = ser.serialize_struct("transaction", 4)?;
-        ser.serialize_struct_elt(&mut state, "file_name", self.file_name())?;
         ser.serialize_struct_elt(&mut state, "description", self.description())?;
         ser.serialize_struct_elt(&mut state, "time", self.time().sec)?;
         ser.serialize_struct_elt(&mut state, "hash", self.hash().to_hex())?;
@@ -71,7 +63,6 @@ impl Serialize for Content {
         where S: Serializer
     {
         let mut state = ser.serialize_struct("content", 4)?;
-        ser.serialize_struct_elt(&mut state, "file_name", self.file_name())?;
         ser.serialize_struct_elt(&mut state, "description", self.description())?;
         ser.serialize_struct_elt(&mut state, "time", self.time().sec)?;
         ser.serialize_struct_elt(&mut state, "hash", self.data_hash().to_hex())?;
@@ -133,8 +124,8 @@ impl<D> Blockchain for TimestampingBlockchain<D>
     type Transaction = TimestampTx;
     type View = TimestampingView<D::Fork>;
 
-    fn verify_tx(tx: &Self::Transaction) -> bool {
-        tx.data().len() < TIMESTAMPING_FILE_SIZE_LIMIT as usize
+    fn verify_tx(_: &Self::Transaction) -> bool {
+        true
     }
 
     fn state_hash(view: &Self::View) -> Result<Hash, Error> {
@@ -146,13 +137,10 @@ impl<D> Blockchain for TimestampingBlockchain<D>
     }
 
     fn execute(view: &Self::View, tx: &Self::Transaction) -> Result<(), Error> {
-        let file = Content::new(tx.file_name(),
-                                tx.description(),
-                                tx.mime(),
+        let content = Content::new(tx.description(),
                                 tx.time(),
-                                tx.hash(),
-                                tx.data());
-        view.contents().put(tx.hash(), file)?;
+                                tx.hash());
+        view.contents().put(tx.hash(), content)?;
         Ok(())
     }
 }
@@ -167,38 +155,26 @@ mod tests {
 
     #[test]
     fn test_timestamp_tx() {
-        let file_name = "Проверка файла с кучей буков.jpg";
         let description = "Test Description";
-        let mime = "application/json";
         let time = time::now().to_timespec();
-        let data = b"js9sdhcsdh32or830ru8043ru-wf9-12u8u3280y8hfwoefnsdljs";
-        let hash = hash(data);
+        let hash = hash(b"js9sdhcsdh32or830ru8043ru-wf9-12u8u3280y8hfwoefnsdljs");
         let (_, sec_key) = gen_keypair();
 
-        let tx = TimestampTx::new(file_name, description, mime, time, &hash, data, &sec_key);
-        assert_eq!(tx.file_name(), file_name);
+        let tx = TimestampTx::new(description, time, &hash, &sec_key);
         assert_eq!(tx.description(), description);
-        assert_eq!(tx.mime(), mime);
         assert_eq!(tx.time(), time);
         assert_eq!(tx.hash(), &hash);
-        assert_eq!(tx.data(), data.as_ref());
     }
 
     #[test]
     fn test_file_content() {
-        let file_name = "Проверка файла с кучей буков.jpg";
         let description = "Test Description";
-        let mime = "application/json";
         let time = time::now().to_timespec();
-        let data = b"js9sdhcsdh32or830ru8043ru-wf9-12u8u3280y8hfwoefnsdljs";
-        let hash = hash(data);
+        let hash = hash(b"js9sdhcsdh32or830ru8043ru-wf9-12u8u3280y8hfwoefnsdljs");
 
-        let content = Content::new(file_name, description, mime, time, &hash, data);
-        assert_eq!(content.file_name(), file_name);
+        let content = Content::new(description, time, &hash);
         assert_eq!(content.description(), description);
-        assert_eq!(content.mime(), mime);
         assert_eq!(content.time(), time);
         assert_eq!(content.data_hash(), &hash);
-        assert_eq!(content.data(), data.as_ref());
     }
 }
