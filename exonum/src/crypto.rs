5a9e78e42b70aa8c23923ab78a358ef73cd9e7e2
Oleg Kuzmin <oleg.kuzmin@xdev.re>
2016-12-09 20:58:43 +0200

    implemented ser/de traits and index traits for wrapped sodium types

--- exonum/src/crypto.rs
+++ exonum/src/crypto.rs
@@ -9,7 +9,9 @@ use sodiumoxide::crypto::sign::ed25519::{PublicKey as PublicKeySodium,
                                          verify_detached, gen_keypair as gen_keypair_sodium,
                                          keypair_from_seed};
 use sodiumoxide::crypto::hash::sha256::{Digest, hash as hash_sodium};
-use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use serde::{Serialize, Serializer};
+use serde::de::{self, Visitor, Deserialize, Deserializer};
+use std::ops::{Index, Range, RangeFrom, RangeTo, RangeFull}; 
 
 pub use hex::{ToHex, FromHex, FromHexError};
 
@@ -36,179 +38,161 @@ pub fn hash(m: &[u8]) -> Hash {
     let dig = hash_sodium(m);
     Hash(dig) 
 }
+macro_rules! implement_public_sodium_wrapper {
+    ($name:ident, $name_from:ident, $size:expr) => (
+        #[derive(PartialEq, Eq, Clone, Copy, PartialOrd, Ord, Hash, Debug)]
+        pub struct $name($name_from); 
 
-#[derive(PartialEq, Eq, Clone, Copy, PartialOrd, Ord, Hash, Debug)]
-pub struct PublicKey(PublicKeySodium); 
+        impl $name {
+            pub fn new(ba: [u8; $size]) -> $name {
+                $name($name_from(ba))
+            }
 
-impl PublicKey {
-    pub fn from_slice(bs: &[u8]) -> Option<PublicKey> {
-        PublicKeySodium::from_slice(bs).map(PublicKey)
-    }
-}
-impl AsRef<[u8]> for PublicKey {
-    fn as_ref(&self) -> &[u8] {
-        self.0.as_ref()
-    }
-}
-impl Serialize for PublicKey {
-    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
-    {
-        self.0.serialize(serializer)
-    }
-}
-
-impl Deserialize for PublicKey {
-    fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-        where D: Deserializer
-    {
-        let pubkey = Deserialize::deserialize(deserializer)?;
-        Ok(PublicKey(pubkey))
-    }
-}
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct SecretKey(SecretKeySodium);
-
-impl Serialize for SecretKey {
-    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
-    {
-        self.0.serialize(serializer)
-    }
+            pub fn from_slice(bs: &[u8]) -> Option<$name> {
+                $name_from::from_slice(bs).map($name)
+            }
+        }
+        impl AsRef<[u8]> for $name {
+            fn as_ref(&self) -> &[u8] {
+                self.0.as_ref()
+            }
+        }
+        )
 }
 
-impl Deserialize for SecretKey {
-    fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-        where D: Deserializer
-    {
-        let secrkey: SecretKeySodium = Deserialize::deserialize(deserializer)?;
-        Ok(SecretKey(secrkey))
-    }
-}
-impl SecretKey {
-    pub fn from_slice(bs: &[u8]) -> Option<SecretKey> {
-        SecretKeySodium::from_slice(bs).map(SecretKey)
-    }
-}
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct Seed(SeedSodium); 
-impl Seed {
-    pub fn from_slice(bs: &[u8]) -> Option<Seed> {
-        SeedSodium::from_slice(bs).map(Seed)
-    }
+macro_rules! implement_private_sodium_wrapper {
+    ($name:ident, $name_from:ident, $size:expr) => (
+        #[derive(Clone, PartialEq, Eq, Debug)]
+        pub struct $name($name_from); 
+
+        impl $name {
+            pub fn new(ba: [u8; $size]) -> $name {
+                $name($name_from(ba))
+            }
+
+            pub fn from_slice(bs: &[u8]) -> Option<$name> {
+                $name_from::from_slice(bs).map($name)
+            }
+        }
+        )
 }
 
-#[derive(PartialEq, Eq, Clone, Copy, PartialOrd, Ord, Hash, Debug)]
-pub struct Signature(SignatureSodium); 
-impl Signature {
-    pub fn from_slice(bs: &[u8]) -> Option<Signature> {
-        SignatureSodium::from_slice(bs).map(Signature)
-    }
-}
-impl AsRef<[u8]> for Signature {
-    fn as_ref(&self) -> &[u8] {
-        self.0.as_ref()
-    }
-}
-
-#[derive(PartialEq, Eq, Clone, Copy, PartialOrd, Ord, Hash, Debug)]
-pub struct Hash(Digest); 
-
-impl Hash {
-    pub fn new(ba: [u8; HASH_SIZE]) -> Hash {
-        Hash(Digest(ba))
-    }
-
-    pub fn from_slice(bs: &[u8]) -> Option<Hash> {
-        Digest::from_slice(bs).map(Hash)
-    }
-}
-impl AsRef<[u8]> for Hash {
-    fn as_ref(&self) -> &[u8] {
-        self.0.as_ref()
-    }
-}
-
-impl Serialize for Hash {
-    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
-    {
-        self.0.serialize(serializer)
-    }
-}
-
-impl Deserialize for Hash {
-    fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-        where D: Deserializer
-    {
-        let digest = Deserialize::deserialize(deserializer)?;
-        Ok(Hash(digest))
-    }
-}
+implement_public_sodium_wrapper! {PublicKey, PublicKeySodium, PUBLIC_KEY_LENGTH}
+implement_public_sodium_wrapper! {Hash, Digest, HASH_SIZE}
+implement_public_sodium_wrapper! {Signature, SignatureSodium, SIGNATURE_LENGTH}
+implement_private_sodium_wrapper! {SecretKey, SecretKeySodium, SECRET_KEY_LENGTH}
+implement_private_sodium_wrapper! {Seed, SeedSodium, SEED_LENGTH}
 
 pub trait HexValue: Sized {
     fn to_hex(&self) -> String;
     fn from_hex<T: AsRef<str>>(v: T) -> Result<Self, FromHexError>;
 }
 
+macro_rules! implement_serde {
+($name:ident) => (
+    impl HexValue for $name {
+        fn to_hex(&self) -> String {
+            let inner = &self.0; 
+            inner.0.as_ref().to_hex()
+        }
+        fn from_hex<T: AsRef<str>>(v: T) -> Result<Self, FromHexError> {
+            let bytes: Vec<u8> = FromHex::from_hex(v.as_ref())?;
+            if let Some(self_value) = Self::from_slice(bytes.as_ref()) {
+                Ok(self_value)
+            } else {
+                Err(FromHexError::InvalidHexLength)
+            }
+        }
+    }
+
+    impl Serialize for $name
+    {
+        fn serialize<S>(&self, ser: &mut S) -> Result<(), S::Error>
+        where S: Serializer
+        {
+            ser.serialize_str(&HexValue::to_hex(self))
+        }
+    }
+
+    impl Deserialize for $name
+    {
+        fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
+        where D: Deserializer
+        {
+            struct HexVisitor;
+
+            impl Visitor for HexVisitor
+            {
+                type Value = $name;
+
+                fn visit_str<E>(&mut self, s: &str) -> Result<Self::Value, E>
+                where E: de::Error
+                {
+                    $name::from_hex(s).map_err(|_| de::Error::custom("Invalid hex"))
+                }
+            }
+            deserializer.deserialize_str(HexVisitor)
+        }
+    }
+    )
+}
+
+implement_serde! {Hash} 
+implement_serde! {PublicKey}
+implement_serde! {SecretKey}
+implement_serde! {Seed}
+implement_serde! {Signature}
+
 impl HexValue for Vec<u8> {
     fn to_hex(&self) -> String {
-        let r: &[u8] = self.as_ref();
-        r.to_hex()
+        ToHex::to_hex(self)
     }
     fn from_hex<T: AsRef<str>>(v: T) -> Result<Self, FromHexError> {
         FromHex::from_hex(v.as_ref())
     }
 }
-
-impl HexValue for Hash {
-    fn to_hex(&self) -> String {
-        self.as_ref().to_hex()
-    }
-    fn from_hex<T: AsRef<str>>(v: T) -> Result<Self, FromHexError> {
-        let bytes: Vec<u8> = FromHex::from_hex(v.as_ref())?;
-        if let Some(hash) = Hash::from_slice(bytes.as_ref()) {
-            Ok(hash)
-        } else {
-            Err(FromHexError::InvalidHexLength)
+macro_rules! implement_index_traits {
+    ($newtype:ident) => (
+        impl Index<Range<usize>> for $newtype {
+            type Output = [u8];
+            fn index(&self, _index: Range<usize>) -> &[u8] {
+                let inner  = &self.0;
+                inner.0.index(_index)
+            }
         }
-    }
-}
-
-impl HexValue for PublicKey {
-    fn to_hex(&self) -> String {
-        self.as_ref().to_hex()
-    }
-    fn from_hex<T: AsRef<str>>(v: T) -> Result<Self, FromHexError> {
-        let bytes: Vec<u8> = FromHex::from_hex(v.as_ref())?;
-        if let Some(hash) = Self::from_slice(bytes.as_ref()) {
-            Ok(hash)
-        } else {
-            Err(FromHexError::InvalidHexLength)
+        impl Index<RangeTo<usize>> for $newtype {
+            type Output = [u8];
+            fn index(&self, _index: RangeTo<usize>) -> &[u8] {
+                let inner  = &self.0;
+                inner.0.index(_index)
+            }
         }
-    }
-}
-
-impl HexValue for SecretKey {
-    fn to_hex(&self) -> String {
-        let sod_secr: &SecretKeySodium = &self.0; 
-        sod_secr.0.as_ref().to_hex()
-    }
-    fn from_hex<T: AsRef<str>>(v: T) -> Result<Self, FromHexError> {
-        let bytes: Vec<u8> = FromHex::from_hex(v.as_ref())?;
-        if let Some(hash) = Self::from_slice(bytes.as_ref()) {
-            Ok(hash)
-        } else {
-            Err(FromHexError::InvalidHexLength)
+        impl Index<RangeFrom<usize>> for $newtype {
+            type Output = [u8];
+            fn index(&self, _index: RangeFrom<usize>) -> &[u8] {
+                let inner  = &self.0;
+                inner.0.index(_index)
+            }
         }
-    }
+        impl Index<RangeFull> for $newtype {
+            type Output = [u8];
+            fn index(&self, _index: RangeFull) -> &[u8] {
+                let inner  = &self.0;
+                inner.0.index(_index)
+            }
+        })
 }
+implement_index_traits! {Hash}
+implement_index_traits! {PublicKey}
+implement_index_traits! {SecretKey}
+implement_index_traits! {Seed}
+implement_index_traits! {Signature}
 
 #[cfg(test)]
 mod tests {
-    use super::{hash, gen_keypair, Hash, PublicKey, SecretKey};
+    use super::{hash, gen_keypair, Hash, PublicKey, SecretKey, Seed, Signature};
     use super::HexValue;
-
+    use serde_json; 
     #[test]
     fn test_hash() {
         let h = hash(&[]);
@@ -223,5 +207,47 @@ mod tests {
         let s1 = SecretKey::from_hex(s.to_hex()).unwrap();
         assert_eq!(p1, p);
         assert_eq!(s1, s);
+    }
+
+    #[test]
+    fn test_ser_deser() {
+        let h = Hash::new([207; 32]); 
+        let json_h = serde_json::to_string(&h).unwrap(); 
+        println!("{}", json_h );
+        let h1 = serde_json::from_str(&json_h).unwrap(); 
+        assert_eq!(h,h1);
+
+        let h = PublicKey::new([208; 32]); 
+        let json_h = serde_json::to_string(&h).unwrap(); 
+        println!("{}", json_h );
+        let h1 = serde_json::from_str(&json_h).unwrap(); 
+        assert_eq!(h,h1);
+
+        let h = Signature::new([209; 64]); 
+        let json_h = serde_json::to_string(&h).unwrap(); 
+        println!("{}", json_h );
+        let h1 = serde_json::from_str(&json_h).unwrap(); 
+        assert_eq!(h,h1);
+
+        let h = Seed::new([210; 32]); 
+        let json_h = serde_json::to_string(&h).unwrap(); 
+        println!("{}", json_h );
+        let h1 = serde_json::from_str(&json_h).unwrap(); 
+        assert_eq!(h,h1);
+
+        let h = SecretKey::new([211; 64]); 
+        let json_h = serde_json::to_string(&h).unwrap(); 
+        println!("{}", json_h );
+        let h1 = serde_json::from_str(&json_h).unwrap(); 
+        assert_eq!(h,h1);
+    }
+
+    #[test]
+    fn test_range_sodium() {
+        let h = hash(&[]) ; 
+        println!(" {:?}", h );
+        let sub_range = &h[10..20]; 
+        println!("{:?}", sub_range);
+        assert_eq!(&[244u8, 200, 153, 111, 185, 36, 39, 174, 65, 228], sub_range );
     }
 }
