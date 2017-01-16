b060e73d0a80089d68fb4e2552d5e300c5f76dc2
Mykhailo Serhiichuk <mykhailo.serhiichuk@xdev.re>
2017-01-16 10:36:26 +0200

    store cryptocurrency as a separate gitlab project

---
+++ cryptocurrency-backend/src/lib.rs
@@ -0,0 +1,359 @@
+#![feature(type_ascription)]
+#![feature(proc_macro)]
+
+extern crate rand;
+extern crate time;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate byteorder;
+#[macro_use]
+extern crate log;
+
+#[macro_use(message, storage_value)]
+extern crate exonum;
+extern crate blockchain_explorer;
+
+pub mod api;
+pub mod wallet;
+
+use std::ops::Deref;
+
+use byteorder::{ByteOrder, LittleEndian};
+
+use exonum::messages::{RawMessage, Message, Error as MessageError};
+use exonum::crypto::{PublicKey, Hash, hash};
+use exonum::storage::{Map, Database, Fork, Error, MerklePatriciaTable, MapTable, MerkleTable, List};
+use exonum::blockchain::{Blockchain, View};
+
+use wallet::{Wallet, WalletId};
+
+pub const TX_TRANSFER_ID: u16 = 128;
+pub const TX_ISSUE_ID: u16 = 129;
+pub const TX_WALLET_ID: u16 = 130;
+
+message! {
+    TxTransfer {
+        const ID = TX_TRANSFER_ID;
+        const SIZE = 80;
+
+        from:        &PublicKey  [00 => 32]
+        to:          &PublicKey  [32 => 64]
+        amount:      i64         [64 => 72]
+        seed:        u64         [72 => 80]
+    }
+}
+
+message! {
+    TxIssue {
+        const ID = TX_ISSUE_ID;
+        const SIZE = 48;
+
+        wallet:      &PublicKey  [00 => 32]
+        amount:      i64         [32 => 40]
+        seed:        u64         [40 => 48]
+    }
+}
+
+message! {
+    TxCreateWallet {
+        const ID = TX_WALLET_ID;
+        const SIZE = 40;
+
+        pub_key:     &PublicKey  [00 => 32]
+        name:        &str        [32 => 40]
+    }
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum CurrencyTx {
+    Transfer(TxTransfer),
+    Issue(TxIssue),
+    CreateWallet(TxCreateWallet),
+}
+
+impl From<RawMessage> for CurrencyTx {
+    fn from(raw: RawMessage) -> Self {
+        CurrencyTx::from_raw(raw).unwrap()
+    }
+}
+
+impl Message for CurrencyTx {
+    fn raw(&self) -> &RawMessage {
+        match *self {
+            CurrencyTx::Transfer(ref msg) => msg.raw(),
+            CurrencyTx::Issue(ref msg) => msg.raw(),
+            CurrencyTx::CreateWallet(ref msg) => msg.raw(),
+        }
+    }
+
+    fn from_raw(raw: RawMessage) -> Result<Self, MessageError> {
+        Ok(match raw.message_type() {
+            TX_TRANSFER_ID => CurrencyTx::Transfer(TxTransfer::from_raw(raw)?),
+            TX_ISSUE_ID => CurrencyTx::Issue(TxIssue::from_raw(raw)?),
+            TX_WALLET_ID => CurrencyTx::CreateWallet(TxCreateWallet::from_raw(raw)?),
+            _ => panic!("Undefined message type"),
+        })
+    }
+
+    fn hash(&self) -> Hash {
+        match *self {
+            CurrencyTx::Transfer(ref msg) => msg.hash(),
+            CurrencyTx::Issue(ref msg) => msg.hash(),
+            CurrencyTx::CreateWallet(ref msg) => msg.hash(),
+        }
+    }
+
+    fn verify(&self, pub_key: &PublicKey) -> bool {
+        match *self {
+            CurrencyTx::Transfer(ref msg) => msg.verify(pub_key),
+            CurrencyTx::Issue(ref msg) => msg.verify(pub_key),
+            CurrencyTx::CreateWallet(ref msg) => msg.verify(pub_key),
+        }
+    }
+}
+
+impl CurrencyTx {
+    pub fn pub_key(&self) -> &PublicKey {
+        match *self {
+            CurrencyTx::Transfer(ref msg) => msg.from(),
+            CurrencyTx::Issue(ref msg) => msg.wallet(),
+            CurrencyTx::CreateWallet(ref msg) => msg.pub_key(),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct CurrencyBlockchain<D: Database> {
+    pub db: D,
+}
+
+pub struct CurrencyView<F: Fork> {
+    pub fork: F,
+}
+
+impl<F> View<F> for CurrencyView<F>
+    where F: Fork
+{
+    type Transaction = CurrencyTx;
+
+    fn from_fork(fork: F) -> Self {
+        CurrencyView { fork: fork }
+    }
+}
+
+impl<F> Deref for CurrencyView<F>
+    where F: Fork
+{
+    type Target = F;
+
+    fn deref(&self) -> &Self::Target {
+        &self.fork
+    }
+}
+
+impl<D: Database> Deref for CurrencyBlockchain<D> {
+    type Target = D;
+
+    fn deref(&self) -> &D {
+        &self.db
+    }
+}
+
+impl<F> CurrencyView<F>
+    where F: Fork
+{
+    pub fn wallets(&self) -> MerkleTable<MapTable<F, [u8], Vec<u8>>, u64, Wallet> {
+        MerkleTable::new(MapTable::new(vec![20], &self))
+    }
+
+    pub fn wallet_ids(&self) -> MerklePatriciaTable<MapTable<F, [u8], Vec<u8>>, PublicKey, u64> {
+        MerklePatriciaTable::new(MapTable::new(vec![21], &self))
+    }
+
+    pub fn wallet(&self, pub_key: &PublicKey) -> Result<Option<(WalletId, Wallet)>, Error> {
+        if let Some(id) = self.wallet_ids().get(pub_key)? {
+            let wallet_pair = self.wallets().get(id)?.map(|wallet| (id, wallet));
+            return Ok(wallet_pair);
+        }
+        Ok(None)
+    }
+
+    pub fn wallet_history(&self,
+                          id: WalletId)
+                          -> MerkleTable<MapTable<F, [u8], Vec<u8>>, u64, Hash> {
+        let mut prefix = vec![22; 9];
+        LittleEndian::write_u64(&mut prefix[1..], id);
+        MerkleTable::new(MapTable::new(prefix, &self))
+    }
+}
+
+impl<D> Blockchain for CurrencyBlockchain<D>
+    where D: Database
+{
+    type Database = D;
+    type Transaction = CurrencyTx;
+    type View = CurrencyView<D::Fork>;
+
+    fn verify_tx(tx: &Self::Transaction) -> bool {
+        tx.verify(tx.pub_key())
+    }
+
+    fn state_hash(view: &Self::View) -> Result<Hash, Error> {
+        let wallets = view.wallets();
+        let wallet_ids = view.wallet_ids();
+
+        let mut hashes = Vec::new();
+        hashes.extend_from_slice(wallets.root_hash()?.as_ref());
+        hashes.extend_from_slice(wallet_ids.root_hash()?.as_ref());
+
+        Ok(hash(&hashes))
+    }
+
+    fn execute(view: &Self::View, tx: &Self::Transaction) -> Result<(), Error> {
+        let tx_hash = tx.hash();
+        match *tx {
+            CurrencyTx::Transfer(ref msg) => {
+                let from = view.wallet(msg.from())?;
+                let to = view.wallet(msg.to())?;
+                if let (Some(mut from), Some(mut to)) = (from, to) {
+                    if from.1.balance() < msg.amount() {
+                        return Ok(());
+                    }
+                    let from_history = view.wallet_history(from.0);
+                    let to_history = view.wallet_history(to.0);
+                    from_history.append(tx_hash)?;
+                    to_history.append(tx_hash)?;
+
+                    from.1.transfer_to(&mut to.1, msg.amount());
+                    from.1.set_history_hash(&from_history.root_hash()?);
+                    to.1.set_history_hash(&to_history.root_hash()?);
+
+                    view.wallets().set(from.0, from.1)?;
+                    view.wallets().set(to.0, to.1)?;
+                }
+            }
+            CurrencyTx::Issue(ref msg) => {
+                if let Some((id, mut wallet)) = view.wallet(msg.wallet())? {
+                    let history = view.wallet_history(id);
+                    history.append(tx_hash)?;
+
+                    let new_amount = wallet.balance() + msg.amount();
+                    wallet.set_balance(new_amount);
+                    wallet.set_history_hash(&history.root_hash()?);
+                    view.wallets().set(id, wallet)?;
+                }
+            }
+            CurrencyTx::CreateWallet(ref msg) => {
+                if let Some(_) = view.wallet_ids().get(msg.pub_key())? {
+                    return Ok(());
+                }
+
+                let id = view.wallets().len()?;
+                view.wallet_history(id).append(tx_hash)?;
+
+                let wallet = Wallet::new(msg.pub_key(),
+                                         msg.name(),
+                                         0,
+                                         &view.wallet_history(id).root_hash()?);
+                view.wallets().append(wallet)?;
+                view.wallet_ids().put(msg.pub_key(), id)?;
+            }
+        };
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use byteorder::{ByteOrder, LittleEndian};
+
+    use exonum::crypto::gen_keypair;
+    use exonum::storage::MemoryDB;
+    use exonum::blockchain::Blockchain;
+    use exonum::messages::Message;
+
+    use super::{CurrencyTx, CurrencyBlockchain, TxCreateWallet, TxIssue, TxTransfer};
+
+    #[test]
+    fn test_tx_create_wallet() {
+        let (p, s) = gen_keypair();
+        let n = "babd, Юникод еще работает";
+
+        let tx = TxCreateWallet::new(&p, n, &s);
+        assert_eq!(tx.pub_key(), &p);
+        assert_eq!(tx.name(), n);
+
+        let tx2 = TxCreateWallet::from_raw(tx.raw().clone()).unwrap();
+        assert_eq!(tx2.pub_key(), &p);
+        assert_eq!(tx2.name(), n);
+    }
+
+    #[test]
+    fn test_wallet_prefix() {
+        let id = 4096;
+        let mut prefix = vec![10; 9];
+        LittleEndian::write_u64(&mut prefix[1..], id);
+        assert_eq!(prefix, vec![10, 0, 16, 0, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_wallet_history() {
+        let b = CurrencyBlockchain { db: MemoryDB::new() };
+        let v = b.view();
+
+        let (p1, s1) = gen_keypair();
+        let (p2, s2) = gen_keypair();
+
+        let cw1 = TxCreateWallet::new(&p1, "tx1", &s1);
+        let cw2 = TxCreateWallet::new(&p2, "tx2", &s2);
+        CurrencyBlockchain::<MemoryDB>::execute(&v, &CurrencyTx::CreateWallet(cw1.clone()))
+            .unwrap();
+        CurrencyBlockchain::<MemoryDB>::execute(&v, &CurrencyTx::CreateWallet(cw2.clone()))
+            .unwrap();
+        let w1 = v.wallet(&p1).unwrap().unwrap();
+        let w2 = v.wallet(&p2).unwrap().unwrap();
+
+        assert_eq!(w1.0, 0);
+        assert_eq!(w2.0, 1);
+        assert_eq!(w1.1.name(), "tx1");
+        assert_eq!(w1.1.balance(), 0);
+        assert_eq!(w2.1.name(), "tx2");
+        assert_eq!(w2.1.balance(), 0);
+        let rh1 = v.wallet_history(w1.0).root_hash().unwrap();
+        let rh2 = v.wallet_history(w2.0).root_hash().unwrap();
+        assert_eq!(&rh1, w1.1.history_hash());
+        assert_eq!(&rh2, w2.1.history_hash());
+
+        let iw1 = TxIssue::new(&p1, 1000, 1, &s1);
+        let iw2 = TxIssue::new(&p2, 100, 2, &s2);
+        CurrencyBlockchain::<MemoryDB>::execute(&v, &CurrencyTx::Issue(iw1.clone())).unwrap();
+        CurrencyBlockchain::<MemoryDB>::execute(&v, &CurrencyTx::Issue(iw2.clone())).unwrap();
+        let w1 = v.wallet(&p1).unwrap().unwrap();
+        let w2 = v.wallet(&p2).unwrap().unwrap();
+
+        assert_eq!(w1.1.balance(), 1000);
+        assert_eq!(w2.1.balance(), 100);
+        let rh1 = v.wallet_history(w1.0).root_hash().unwrap();
+        let rh2 = v.wallet_history(w2.0).root_hash().unwrap();
+        assert_eq!(&rh1, w1.1.history_hash());
+        assert_eq!(&rh2, w2.1.history_hash());
+
+        let tw = TxTransfer::new(&p1, &p2, 400, 3, &s1);
+        CurrencyBlockchain::<MemoryDB>::execute(&v, &CurrencyTx::Transfer(tw.clone())).unwrap();
+        let w1 = v.wallet(&p1).unwrap().unwrap();
+        let w2 = v.wallet(&p2).unwrap().unwrap();
+
+        assert_eq!(w1.1.balance(), 600);
+        assert_eq!(w2.1.balance(), 500);
+        let rh1 = v.wallet_history(w1.0).root_hash().unwrap();
+        let rh2 = v.wallet_history(w2.0).root_hash().unwrap();
+        assert_eq!(&rh1, w1.1.history_hash());
+        assert_eq!(&rh2, w2.1.history_hash());
+
+        let h1 = v.wallet_history(w1.0).values().unwrap();
+        let h2 = v.wallet_history(w2.0).values().unwrap();
+        assert_eq!(h1, vec![cw1.hash(), iw1.hash(), tw.hash()]);
+        assert_eq!(h2, vec![cw2.hash(), iw2.hash(), tw.hash()]);
+    }
+}
---
+++ cryptocurrency-backend/src/main.rs
@@ -0,0 +1,343 @@
+#![feature(type_ascription)]
+
+#[macro_use]
+extern crate rustless;
+extern crate jsonway;
+extern crate iron;
+extern crate hyper;
+extern crate valico;
+extern crate env_logger;
+extern crate clap;
+extern crate serde;
+extern crate time;
+extern crate rand;
+#[macro_use]
+extern crate log;
+
+extern crate exonum;
+extern crate blockchain_explorer;
+extern crate cryptocurrency;
+
+use std::net::SocketAddr;
+use std::thread;
+use std::default::Default;
+
+use clap::{Arg, App};
+use rustless::json::ToJson;
+use rustless::{Application, Api, Nesting, Versioning, Response};
+use rustless::batteries::cookie::{Cookie, CookieExt, CookieJar};
+use rustless::batteries::swagger;
+use rustless::errors;
+use valico::json_dsl;
+use hyper::status::StatusCode;
+use rand::{Rng, thread_rng};
+
+use exonum::blockchain::GenesisConfig;
+use exonum::node::{Node, NodeConfig, TxSender, NodeChannel};
+use exonum::storage::Database;
+use exonum::storage::{Result as StorageResult, Error as StorageError};
+use exonum::crypto::{gen_keypair, PublicKey, SecretKey, HexValue, FromHexError};
+use exonum::messages::Message;
+use exonum::events::Error as EventsError;
+
+use blockchain_explorer::ValueNotFound;
+use blockchain_explorer::helpers::{GenerateCommand, RunCommand, DatabaseType};
+
+use cryptocurrency::{CurrencyBlockchain, CurrencyTx, TxIssue, TxTransfer, TxCreateWallet};
+use cryptocurrency::api::CurrencyApi;
+
+pub type CurrencyTxSender<B> = TxSender<B, NodeChannel<B>>;
+
+
+fn save_keypair_in_cookies(storage: &mut CookieJar,
+                           public_key: &PublicKey,
+                           secret_key: &SecretKey) {
+    let p = storage.permanent();
+
+    p.add(Cookie::new("public_key".to_string(), public_key.to_hex()));
+    p.add(Cookie::new("secret_key".to_string(), secret_key.to_hex()));
+}
+
+fn load_hex_value_from_cookie<'a>(storage: &'a CookieJar, key: &str) -> StorageResult<Vec<u8>> {
+    if let Some(cookie) = storage.find(key) {
+        if let Ok(value) = HexValue::from_hex(cookie.value) {
+            return Ok(value);
+        }
+    }
+    Err(StorageError::new(format!("Unable to find value with given key {}", key)))
+}
+
+fn load_keypair_from_cookies(storage: &CookieJar) -> StorageResult<(PublicKey, SecretKey)> {
+    let p = storage.permanent();
+
+    let public_key = PublicKey::from_slice(load_hex_value_from_cookie(&p, "public_key")?.as_ref());
+    let secret_key = SecretKey::from_slice(load_hex_value_from_cookie(&p, "secret_key")?.as_ref());
+
+    let public_key = public_key.ok_or(StorageError::new("Unable to read public key"))?;
+    let secret_key = secret_key.ok_or(StorageError::new("Unable to read secret key"))?;
+    Ok((public_key, secret_key))
+}
+
+fn blockchain_explorer_api<D: Database>(api: &mut Api,
+                                        b: CurrencyBlockchain<D>,
+                                        cfg: GenesisConfig) {
+    blockchain_explorer::make_api::<CurrencyBlockchain<D>, CurrencyTx>(api, b, cfg);
+}
+
+fn cryptocurrency_api<D: Database>(api: &mut Api,
+                                   blockchain: CurrencyBlockchain<D>,
+                                   channel: CurrencyTxSender<CurrencyBlockchain<D>>,
+                                   cfg: GenesisConfig) {
+    api.namespace("wallets", move |api| {
+        let ch = channel.clone();
+        api.post("create", move |endpoint| {
+            endpoint.summary("Create a new wallet for user with given name");
+            endpoint.params(|params| {
+                params.req_typed("name", json_dsl::string());
+            });
+
+            endpoint.handle(move |client, params| {
+                let name = params.find("name").unwrap().as_str().unwrap();
+                // TODO make secure
+                let (public_key, secret_key) = gen_keypair();
+                {
+                    let mut cookies = client.request.cookies();
+                    save_keypair_in_cookies(&mut cookies, &public_key, &secret_key);
+                }
+                let tx = TxCreateWallet::new(&public_key, &name, &secret_key);
+
+                let tx_hash = tx.hash().to_hex();
+                match ch.send(CurrencyTx::CreateWallet(tx)) {
+                    Ok(_) => {
+                        let json = &jsonway::object(|json| json.set("tx_hash", tx_hash)).unwrap();
+                        client.json(json)
+                    }
+                    Err(e) => client.error(e),
+                }
+            })
+        });
+
+        let ch = channel.clone();
+        api.post("issue", move |endpoint| {
+            endpoint.params(|params| {
+                params.req_typed("amount", json_dsl::i64());
+            });
+
+            endpoint.handle(move |client, params| {
+                let (public_key, secret_key) = {
+                    let r = {
+                        let cookies = client.request.cookies();
+                        load_keypair_from_cookies(&cookies)
+                    };
+                    match r {
+                        Ok((p, s)) => (p, s),
+                        Err(e) => return client.error(e),
+                    }
+                };
+
+                let amount = params.find("amount").unwrap().as_i64().unwrap();
+                let seed = thread_rng().gen::<u64>();
+                let tx = TxIssue::new(&public_key, amount, seed, &secret_key);
+
+                let tx_hash = tx.hash().to_hex();
+                match ch.send(CurrencyTx::Issue(tx)) {
+                    Ok(_) => {
+                        let json = &jsonway::object(|json| json.set("tx_hash", tx_hash)).unwrap();
+                        client.json(json)
+                    }
+                    Err(e) => client.error(e),
+                }
+            })
+        });
+
+        let ch = channel.clone();
+        api.post("transfer", move |endpoint| {
+            endpoint.params(|params| {
+                params.req_typed("amount", json_dsl::i64());
+                params.req_typed("to", json_dsl::string());
+            });
+
+            endpoint.handle(move |client, params| {
+                let (from_key, secret_key) = {
+                    let r = {
+                        let cookies = client.request.cookies();
+                        load_keypair_from_cookies(&cookies)
+                    };
+                    match r {
+                        Ok((p, s)) => (p, s),
+                        Err(e) => return client.error(e),
+                    }
+                };
+                // TODO remove unwrap
+                let amount = params.find("amount").unwrap().as_i64().unwrap();
+                let to = params.find("to").unwrap().as_str().unwrap();
+                let seed = thread_rng().gen::<u64>();
+                match PublicKey::from_hex(to) {
+                    Ok(to_key) => {
+                        let tx = TxTransfer::new(&from_key, &to_key, amount, seed, &secret_key);
+
+                        let tx_hash = tx.hash().to_hex();
+                        match ch.send(CurrencyTx::Transfer(tx)) {
+                            Ok(_) => {
+                                let json = &jsonway::object(|json| json.set("tx_hash", tx_hash))
+                                    .unwrap();
+                                client.json(json)
+                            }
+                            Err(e) => client.error(e),
+                        }
+                    }
+                    Err(e) => return client.error(e),
+                }
+            })
+        });
+
+        let b = blockchain.clone();
+        let c = cfg.clone();
+        api.get("info", move |endpoint| {
+            endpoint.handle(move |client, _| {
+                let (public_key, _) = {
+                    let r = {
+                        let cookies = client.request.cookies();
+                        load_keypair_from_cookies(&cookies)
+                    };
+                    match r {
+                        Ok((p, s)) => (p, s),
+                        Err(e) => return client.error(e),
+                    }
+                };
+                let currency_api = CurrencyApi::<D>::new(b.clone(), c.clone());
+                match currency_api.wallet_info(&public_key) {
+                    Ok(Some(info)) => client.json(&info.to_json()),
+                    _ => client.error(StorageError::new("Unable to get wallet info")),
+                }
+            })
+        })
+    });
+}
+
+fn run_node<D: Database>(blockchain: CurrencyBlockchain<D>,
+                         node_cfg: NodeConfig,
+                         port: Option<u16>) {
+    if let Some(port) = port {
+        let mut node = Node::new(blockchain.clone(), node_cfg.clone());
+        let channel = node.channel();
+
+        let api_thread = thread::spawn(move || {
+            let channel = channel.clone();
+            let blockchain = blockchain.clone();
+
+            let api = Api::build(move |api| {
+                // Specify API version
+                api.version("v1", Versioning::Path);
+                api.prefix("api");
+
+                api.error_formatter(|err, _media| {
+                    let body;
+                    let code;
+                    if let Some(e) = err.downcast::<StorageError>() {
+                        code = StatusCode::InternalServerError;
+                        body = format!("An error in backend occured: {}", e);
+                    } else if let Some(e) = err.downcast::<errors::NotMatch>() {
+                        code = StatusCode::NotFound;
+                        body = e.to_string();
+                    } else if let Some(e) = err.downcast::<errors::Validation>() {
+                        code = StatusCode::BadRequest;
+                        body = e.to_string();
+                    } else if let Some(e) = err.downcast::<ValueNotFound>() {
+                        code = StatusCode::NotFound;
+                        body = e.to_string();
+                    } else if let Some(e) = err.downcast::<FromHexError>() {
+                        code = StatusCode::BadRequest;
+                        body = e.to_string();
+                    } else if let Some(e) = err.downcast::<EventsError>() {
+                        code = StatusCode::ServiceUnavailable;
+                        body = e.to_string();
+                    } else {
+                        code = StatusCode::NotImplemented;
+                        body = format!("Unspecified error: {:?}", err);
+                    }
+                    trace!("RestApi: code={}, body={}", code, body);
+
+                    let json = &jsonway::object(|json| json.set("message", body)).unwrap();
+                    Some(Response::from_json(code, &json))
+                });
+
+                blockchain_explorer_api(api, blockchain.clone(), node_cfg.genesis.clone());
+                cryptocurrency_api(api, blockchain.clone(), channel.clone(), node_cfg.genesis);
+                api.mount(swagger::create_api("docs"));
+            });
+
+            let listen_address: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
+            println!("Cryptocurrency node server started on {}", listen_address);
+
+            let mut app = Application::new(api);
+
+            swagger::enable(&mut app,
+                            swagger::Spec {
+                                info: swagger::Info {
+                                    title: "Cryptocurrency API".to_string(),
+                                    description: Some("Simple API to demonstration".to_string()),
+                                    contact: Some(swagger::Contact {
+                                        name: "Aleksey Sidorov".to_string(),
+                                        url: Some("aleksei.sidorov@xdev.re".to_string()),
+                                        ..Default::default()
+                                    }),
+                                    license: Some(swagger::License {
+                                        name: "Demo".to_string(),
+                                        url: "http://exonum.com".to_string(),
+                                    }),
+                                    ..Default::default()
+                                },
+                                ..Default::default()
+                            });
+
+            let mut chain = iron::Chain::new(app);
+            let api_key = b"abacabasecretcoinblabla23nx8Hasojd8";
+            let cookie = ::rustless::batteries::cookie::new(api_key);
+            chain.link(cookie);
+            iron::Iron::new(chain).http(listen_address).unwrap();
+        });
+
+        node.run().unwrap();
+        api_thread.join().unwrap();
+    } else {
+        Node::new(blockchain, node_cfg).run().unwrap();
+    }
+}
+
+fn main() {
+    exonum::crypto::init();
+    blockchain_explorer::helpers::init_logger().unwrap();
+
+    let app = App::new("Simple cryptocurrency demo program")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Aleksey S. <aleksei.sidorov@xdev.re>")
+        .about("Demo cryptocurrency validator node")
+        .subcommand(GenerateCommand::new())
+        .subcommand(RunCommand::new().arg(Arg::with_name("HTTP_PORT")
+            .short("p")
+            .long("port")
+            .value_name("HTTP_PORT")
+            .help("Run http server on given port")
+            .takes_value(true)));
+    let matches = app.get_matches();
+
+    match matches.subcommand() {
+        ("generate", Some(matches)) => GenerateCommand::execute(matches),
+        ("run", Some(matches)) => {
+            let port: Option<u16> = matches.value_of("HTTP_PORT").map(|x| x.parse().unwrap());
+            let node_cfg = RunCommand::node_config(matches);
+            match RunCommand::db(matches) {
+                DatabaseType::LevelDB(db) => {
+                    run_node(CurrencyBlockchain { db: db }, node_cfg, port)
+                }
+                DatabaseType::MemoryDB(db) => {
+                    run_node(CurrencyBlockchain { db: db }, node_cfg, port)
+                }
+            }
+        }
+        _ => {
+            unreachable!("Wrong subcommand");
+        }
+    }
+}
