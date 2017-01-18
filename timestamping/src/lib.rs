31afd99c202b694c5ae80ad19f630c371ce6cf62
Aleksey Sidorov <aleksei.sidorov@xdev.re>
2017-01-18 13:19:45 +0700

    Add node state to service api
    Former-commit-id: dea0c5be62d7536eda05b8555c6bbd7e8619a00f

--- timestamping/src/lib.rs
+++ timestamping/src/lib.rs
@@ -5,6 +5,7 @@ use exonum::messages::{Message, RawTransaction, Error as MessageError};
 use exonum::crypto::{PublicKey, Hash, hash};
 use exonum::storage::{Error, View as StorageView};
 use exonum::blockchain::{Service, Transaction};
+use exonum::node::State;
 
 pub const TIMESTAMPING_SERVICE: u16 = 129;
 pub const TIMESTAMPING_TRANSACTION_MESSAGE_ID: u16 = 128;
@@ -71,7 +72,10 @@ impl Service for TimestampingService {
         TimestampTx::from_raw(raw).map(|tx| Box::new(tx) as Box<Transaction>)
     }
 
-    fn handle_commit(&self, _: &StorageView) -> Result<Vec<Box<Transaction>>, Error> {
+    fn handle_commit(&self,
+                     _: &StorageView,
+                     _: &mut State)
+                     -> Result<Vec<Box<Transaction>>, Error> {
         Ok(Vec::new())
     }
 }
