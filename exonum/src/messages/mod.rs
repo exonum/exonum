e3e27a2e641966a00606431d2c5d55b05532408a
Aleksey Sidorov <aleksei.sidorov@xdev.re>
2017-01-26 19:32:35 +0200

    Fix service id in Any message
    Former-commit-id: f0410f046109e689b790e87a3854e2dff0c62d20

--- exonum/src/messages/mod.rs
+++ exonum/src/messages/mod.rs
@@ -179,40 +179,44 @@ impl fmt::Debug for ConsensusMessage {
 impl Any {
     pub fn from_raw(raw: RawMessage) -> Result<Any, Error> {
         // TODO: check input message size
-        Ok(match raw.message_type() {
-            CONNECT_MESSAGE_ID => Any::Connect(Connect::from_raw(raw)?),
-            STATUS_MESSAGE_ID => Any::Status(Status::from_raw(raw)?),
-            BLOCK_MESSAGE_ID => Any::Block(Block::from_raw(raw)?),
+        let msg =
+            if raw.service_id() == CONSENSUS {
+                match raw.message_type() {
+                    CONNECT_MESSAGE_ID => Any::Connect(Connect::from_raw(raw)?),
+                    STATUS_MESSAGE_ID => Any::Status(Status::from_raw(raw)?),
+                    BLOCK_MESSAGE_ID => Any::Block(Block::from_raw(raw)?),
 
-            PROPOSE_MESSAGE_ID => {
-                Any::Consensus(ConsensusMessage::Propose(Propose::from_raw(raw)?))
-            }
-            PREVOTE_MESSAGE_ID => {
-                Any::Consensus(ConsensusMessage::Prevote(Prevote::from_raw(raw)?))
-            }
-            PRECOMMIT_MESSAGE_ID => {
-                Any::Consensus(ConsensusMessage::Precommit(Precommit::from_raw(raw)?))
-            }
+                    PROPOSE_MESSAGE_ID => {
+                        Any::Consensus(ConsensusMessage::Propose(Propose::from_raw(raw)?))
+                    }
+                    PREVOTE_MESSAGE_ID => {
+                        Any::Consensus(ConsensusMessage::Prevote(Prevote::from_raw(raw)?))
+                    }
+                    PRECOMMIT_MESSAGE_ID => {
+                        Any::Consensus(ConsensusMessage::Precommit(Precommit::from_raw(raw)?))
+                    }
 
-            REQUEST_PROPOSE_MESSAGE_ID => {
-                Any::Request(RequestMessage::Propose(RequestPropose::from_raw(raw)?))
-            }
-            REQUEST_TRANSACTIONS_MESSAGE_ID => {
-                Any::Request(RequestMessage::Transactions(RequestTransactions::from_raw(raw)?))
-            }
-            REQUEST_PREVOTES_MESSAGE_ID => {
-                Any::Request(RequestMessage::Prevotes(RequestPrevotes::from_raw(raw)?))
-            }
-            REQUEST_PRECOMMITS_MESSAGE_ID => {
-                Any::Request(RequestMessage::Precommits(RequestPrecommits::from_raw(raw)?))
-            }
-            REQUEST_PEERS_MESSAGE_ID => {
-                Any::Request(RequestMessage::Peers(RequestPeers::from_raw(raw)?))
-            }
-            REQUEST_BLOCK_MESSAGE_ID => {
-                Any::Request(RequestMessage::Block(RequestBlock::from_raw(raw)?))
-            }
-            _ => Any::Transaction(raw),
-        })
+                    REQUEST_PROPOSE_MESSAGE_ID => {
+                        Any::Request(RequestMessage::Propose(RequestPropose::from_raw(raw)?))
+                    }
+                    REQUEST_TRANSACTIONS_MESSAGE_ID => Any::Request(RequestMessage::Transactions(RequestTransactions::from_raw(raw)?)),
+                    REQUEST_PREVOTES_MESSAGE_ID => {
+                        Any::Request(RequestMessage::Prevotes(RequestPrevotes::from_raw(raw)?))
+                    }
+                    REQUEST_PRECOMMITS_MESSAGE_ID => {
+                        Any::Request(RequestMessage::Precommits(RequestPrecommits::from_raw(raw)?))
+                    }
+                    REQUEST_PEERS_MESSAGE_ID => {
+                        Any::Request(RequestMessage::Peers(RequestPeers::from_raw(raw)?))
+                    }
+                    REQUEST_BLOCK_MESSAGE_ID => {
+                        Any::Request(RequestMessage::Block(RequestBlock::from_raw(raw)?))
+                    }
+                    _ => panic!("Wrong consensus message type"),
+                }
+            } else {
+                Any::Transaction(raw)
+            };
+        Ok(msg)
     }
 }
