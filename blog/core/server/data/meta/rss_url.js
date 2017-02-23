4d3af517408b2f500450628c788f10ea49eed3ab
Vladimir Motylenko <vladimir.motylenko@xdev.re>
2017-02-23 19:20:24 +0200

    remove junk frontends

--- blog/core/server/data/meta/rss_url.js
+++
@@ -1,7 +0,0 @@
-var config = require('../../config');
-
-function getRssUrl(data, absolute) {
-    return config.urlFor('rss', {secure: data.secure}, absolute);
-}
-
-module.exports = getRssUrl;
--- drm-frontend/app/scripts/settings.js
+++
@@ -1,4 +0,0 @@
-var settings = {
-    landing_url: 'http://exonum.com/',
-    api_endpoint: 'http://exonum.com/api/v1/'
-};
