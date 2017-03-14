361adaea33b648bd1bd439e4fee504bb790fc9de
Oleg Kuzmin <oleg.kuzmin@xdev.re>
2017-03-14 20:24:21 +0200

    Added 2 regression cases to config service. Fixed handle_commit for ConfigurationService.

--- sandbox_tests/src/lib.rs
+++ sandbox_tests/src/lib.rs
@@ -34,11 +34,17 @@ mod tests {
         sandbox
     }
 
+    #[derive(Serialize)]
+    struct CfgStub {
+        cfg_string: String,
+    }
+
     #[test]
-    fn it_works() {
+    fn change_service_config() {
         blockchain_explorer::helpers::init_logger();
         let sandbox = configuration_sandbox();
         let sandbox_state = SandboxState::new();
+        let initial_cfg = sandbox.cfg();
 
         add_one_height_with_transactions(&sandbox, &sandbox_state, &[]);
         add_one_height_with_transactions(&sandbox, &sandbox_state, &[]);
@@ -46,15 +52,7 @@ mod tests {
         add_one_height_with_transactions(&sandbox, &sandbox_state, &[]);
         sandbox.assert_state(5, 1);
 
-        #[derive(Serialize)]
-        struct CfgStub {
-            cfg_string: String,
-        }
-
-        let mut services: BTreeMap<u16, Value> = BTreeMap::new();
-        let tmstmp_id = TimestampingService::new().service_id();
-        let service_cfg = CfgStub { cfg_string: "First_config_change".to_string() };
-        services.insert(tmstmp_id, service_cfg.to_json());
+        let services = gen_timestamping_cfg("First cfg");
 
         let new_cfg = StoredConfiguration {
             actual_from: 7,
@@ -89,8 +87,7 @@ mod tests {
         assert_eq!(vote1, votes.get(&sandbox.p(0)).unwrap().unwrap());
         assert_eq!(vote2, votes.get(&sandbox.p(1)).unwrap().unwrap());
         assert_eq!(None, votes.get(&sandbox.p(2)).unwrap());
-        //warn!("actual cfg: {:?}", sandbox.cfg());
-        assert_ne!(new_cfg, sandbox.cfg());
+        assert_eq!(initial_cfg, sandbox.cfg());
 
         let vote3 = TxConfigVote::new(&sandbox.p(2), 5, &propose_hash, 1700, false, sandbox.s(2));
         add_one_height_with_transactions(&sandbox, &sandbox_state, &[vote3.raw().clone()]);
@@ -100,5 +97,150 @@ mod tests {
         sandbox.assert_state(8, 1);
         assert_eq!(vote3, votes.get(&sandbox.p(2)).unwrap().unwrap());
         assert_eq!(new_cfg, sandbox.cfg());
+    }
+
+    fn gen_timestamping_cfg(cfg_message: &str) -> BTreeMap<u16, Value> {
+        let mut services: BTreeMap<u16, Value> = BTreeMap::new();
+        let tmstmp_id = TimestampingService::new().service_id();
+        let service_cfg = CfgStub { cfg_string: cfg_message.to_string() };
+        services.insert(tmstmp_id, service_cfg.to_json());
+        services
+    }
+
+    /// regression: votes' were summed for all proposes simultaneously, and not for the same propose
+    #[test]
+    fn regression_majority_votes_for_different_proposes() {
+
+        blockchain_explorer::helpers::init_logger();
+        let sandbox = configuration_sandbox();
+        let sandbox_state = SandboxState::new();
+
+        let initial_cfg = sandbox.cfg();
+        add_one_height_with_transactions(&sandbox, &sandbox_state, &[]);
+        sandbox.assert_state(2, 1);
+
+        let actual_from = 4;
+
+        let services = gen_timestamping_cfg("First cfg");
+        let new_cfg1 = StoredConfiguration {
+            actual_from: actual_from,
+            validators: sandbox.validators(),
+            consensus: sandbox.cfg().consensus,
+            services: services,
+        };
+        let services = gen_timestamping_cfg("Second cfg");
+        let new_cfg2 = StoredConfiguration {
+            actual_from: actual_from,
+            validators: sandbox.validators(),
+            consensus: sandbox.cfg().consensus,
+            services: services,
+        };
+
+        let propose_tx1 = TxConfigPropose::new(&sandbox.p(1),
+                                               5,
+                                               &to_vec(&new_cfg1).unwrap(),
+                                               actual_from,
+                                               sandbox.s(1));
+        let propose_tx2 = TxConfigPropose::new(&sandbox.p(1),
+                                               5,
+                                               &to_vec(&new_cfg2).unwrap(),
+                                               actual_from,
+                                               sandbox.s(1));
+
+        add_one_height_with_transactions(&sandbox,
+                                         &sandbox_state,
+                                         &[propose_tx1.raw().clone(), propose_tx2.raw().clone()]);
+        sandbox.assert_state(3, 1);
+
+        let pr_hash1 = propose_tx1.hash();
+        let pr_hash2 = propose_tx2.hash();
+        let prop1_validator0 = TxConfigVote::new(&sandbox.p(0), 5, &pr_hash1, 1700, false, sandbox.s(0));
+        let prop1_validator1 = TxConfigVote::new(&sandbox.p(1), 5, &pr_hash1, 1700, false, sandbox.s(1));
+
+        add_one_height_with_transactions(&sandbox,
+                                         &sandbox_state,
+                                         &[prop1_validator0.raw().clone(), prop1_validator1.raw().clone()]);
+        sandbox.assert_state(4, 1);
+        assert_eq!(initial_cfg, sandbox.cfg());
+
+        let prop2_validator2 = TxConfigVote::new(&sandbox.p(2), 5, &pr_hash2, 1700, false, sandbox.s(2));
+
+        add_one_height_with_transactions(&sandbox,
+                                         &sandbox_state,
+                                         &[prop2_validator2.raw().clone()]);
+        sandbox.assert_state(5, 1);
+        assert_eq!(initial_cfg, sandbox.cfg());
+    }
+
+    #[test]
+    fn regression_new_vote_for_older_config_applies_old_config() {
+
+        blockchain_explorer::helpers::init_logger();
+        let sandbox = configuration_sandbox();
+        let sandbox_state = SandboxState::new();
+
+        add_one_height_with_transactions(&sandbox, &sandbox_state, &[]);
+        sandbox.assert_state(2, 1);
+
+        let actual_from = 4;
+
+        let services = gen_timestamping_cfg("First cfg");
+        let new_cfg1 = StoredConfiguration {
+            actual_from: actual_from,
+            validators: sandbox.validators(),
+            consensus: sandbox.cfg().consensus,
+            services: services,
+        };
+        let services = gen_timestamping_cfg("Second cfg");
+        let new_cfg2 = StoredConfiguration {
+            actual_from: actual_from,
+            validators: sandbox.validators(),
+            consensus: sandbox.cfg().consensus,
+            services: services,
+        };
+        let propose_tx1 = TxConfigPropose::new(&sandbox.p(1),
+                                               5,
+                                               &to_vec(&new_cfg1).unwrap(),
+                                               actual_from,
+                                               sandbox.s(1));
+        let propose_tx2 = TxConfigPropose::new(&sandbox.p(1),
+                                               5,
+                                               &to_vec(&new_cfg2).unwrap(),
+                                               actual_from,
+                                               sandbox.s(1));
+
+        add_one_height_with_transactions(&sandbox,
+                                         &sandbox_state,
+                                         &[propose_tx1.raw().clone(), propose_tx2.raw().clone()]);
+        sandbox.assert_state(3, 1);
+
+        let pr_hash1 = propose_tx1.hash();
+        let pr_hash2 = propose_tx2.hash();
+        let prop1_validator0 = TxConfigVote::new(&sandbox.p(0), 5, &pr_hash1, 1700, false, sandbox.s(0));
+        let prop1_validator1 = TxConfigVote::new(&sandbox.p(1), 5, &pr_hash1, 1700, false, sandbox.s(1));
+        let prop1_validator2 = TxConfigVote::new(&sandbox.p(2), 5, &pr_hash1, 1700, false, sandbox.s(2));
+
+        add_one_height_with_transactions(&sandbox,
+                                         &sandbox_state,
+                                         &[prop1_validator0.raw().clone(), prop1_validator1.raw().clone(), prop1_validator2.raw().clone()]);
+        sandbox.assert_state(4, 1);
+        assert_eq!(new_cfg1, sandbox.cfg());
+
+        let prop2_validator0 = TxConfigVote::new(&sandbox.p(0), 5, &pr_hash2, 1701, false, sandbox.s(0));
+        let prop2_validator1 = TxConfigVote::new(&sandbox.p(1), 5, &pr_hash2, 1701, false, sandbox.s(1));
+        let prop2_validator2 = TxConfigVote::new(&sandbox.p(2), 5, &pr_hash2, 1701, false, sandbox.s(2));
+
+        add_one_height_with_transactions(&sandbox,
+                                         &sandbox_state,
+                                         &[prop2_validator0.raw().clone(), prop2_validator1.raw().clone(), prop2_validator2.raw().clone()]);
+        sandbox.assert_state(5, 1);
+        assert_eq!(new_cfg2, sandbox.cfg());
+
+        let prop1_validator0 = TxConfigVote::new(&sandbox.p(0), 5, &pr_hash1, 1702, false, sandbox.s(0));
+        add_one_height_with_transactions(&sandbox,
+                                         &sandbox_state,
+                                         &[prop1_validator0.raw().clone()]);
+        sandbox.assert_state(6, 1);
+        assert_eq!(new_cfg2, sandbox.cfg());
     }
 }
