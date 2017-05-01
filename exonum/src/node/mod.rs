d895fd4d2e6f1da6613a90ecb05d805ace1bef5e
Stanislav Tkach <stanislav.tkach@xdev.re>
2017-05-01 12:32:30 +0300

    Remove 'TimeoutAdjuster::init' method
    Former-commit-id: 8f4ed36ddf38f10080f6f622ce372caeb4655984

--- exonum/src/node/mod.rs
+++ exonum/src/node/mod.rs
@@ -124,7 +124,7 @@ impl<S> NodeHandler<S>
                                    last_height,
                                    sender.get_time());
 
-        let timeout = timeout_adjuster.init(&state);
+        let timeout = timeout_adjuster.adjust_timeout(&state);
         state.set_propose_timeout(timeout);
 
         NodeHandler {
