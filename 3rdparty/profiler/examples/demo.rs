10e3db3820fca882549c0ad15eedebf06cc06d15
Vladimir Motylenko <vladimir.motylenko@xdev.re>
2017-05-26 00:20:41 +0300

    Fix profile small errors, add Interruption handler.

--- 3rdparty/profiler/examples/demo.rs
+++ 3rdparty/profiler/examples/demo.rs
@@ -1,35 +1,35 @@
-extern crate profiler as flame;
+extern crate profiler;
 
 use std::fs::File;
 
 pub fn main() {
-    flame::start("update");
-        flame::start("process inputs");
-        flame::end();
+    profiler::start("update");
+        profiler::start("process inputs");
+        profiler::end();
 
-        flame::start("physics");
-        for i in 0..5 {
-            flame::start("broad phase");
-            flame::end();
+        profiler::start("physics");
+        for _ in 0..5 {
+            profiler::start("broad phase");
+            profiler::end();
 
-            flame::start("narrow phase");
-                flame::start("narrow phase");
-                flame::end();
-            flame::end();
+            profiler::start("narrow phase");
+                profiler::start("narrow phase");
+                profiler::end();
+            profiler::end();
         }
-        flame::end();
+        profiler::end();
 
-        flame::start("network sync");
-        flame::end();
-    flame::end();
+        profiler::start("network sync");
+        profiler::end();
+    profiler::end();
 
-    flame::start("render");
-        flame::start("build display lists");
-        flame::end();
+    profiler::start("render");
+        profiler::start("build display lists");
+        profiler::end();
 
-        flame::start("draw calls");
-        flame::end();
-    flame::end();
+        profiler::start("draw calls");
+        profiler::end();
+    profiler::end();
 
-    flame::dump_html(&mut File::create("out1.html").unwrap()).unwrap();
+    profiler::dump_html(&mut File::create("out1.html").unwrap()).unwrap();
 }
