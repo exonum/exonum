79524d01939bda0c2a42cee66653c60915699c32
Vladimir Motylenko <vladimir.motylenko@xdev.re>
2017-05-26 13:42:17 +0300

    add tests

--- exonum/src/node/whitelist.rs
+++ exonum/src/node/whitelist.rs
@@ -49,3 +49,96 @@ impl Whitelist {
         !self.whitelist_on
     }
 }
+
+
+#[cfg(test)]
+mod test {
+use super::Whitelist;
+use crypto::PublicKey;
+
+static VALIDATORS: [[[u8; 32];2];2] = [[
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2]
+],
+[
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2]
+]];
+static REGULAR_PEERS: [[u8;32]; 4] = [
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4],
+];
+
+    fn make_keys(source:&[[u8; 32]]) -> Vec<PublicKey> {
+        source.iter().map(|k|PublicKey::from_slice(k).unwrap()).collect()
+    }
+
+    fn check_in_whitelist(whitelist: &Whitelist,
+                          keys:&[PublicKey],
+                          in_whitelist: &[usize],
+                          not_in_whitelist: &[usize]) {
+        for i in in_whitelist {
+            assert_eq!(whitelist.contains(&keys[*i]), true);
+        }
+        for i in not_in_whitelist {
+            assert_eq!(whitelist.contains(&keys[*i]), false);
+        }
+    }
+
+    #[test]
+    fn test_whitelist() {
+        let regular = make_keys(&REGULAR_PEERS);
+
+        let mut whitelist = Whitelist::default();
+        whitelist.whitelist_on = true;
+        check_in_whitelist(&whitelist, &regular, &[], &[0, 1, 2, 3]);
+        whitelist.add(regular[0]);
+        check_in_whitelist(&whitelist, &regular, &[0], &[ 1, 2, 3]);
+        whitelist.add(regular[2]);
+        check_in_whitelist(&whitelist, &regular, &[0, 2], &[1, 3]);
+        assert_eq!(whitelist.collect_whitelist().len(), 2);
+    }
+
+    #[test]
+    fn test_wildcard() {
+        let regular = make_keys(&REGULAR_PEERS);
+
+        let whitelist = Whitelist::default();
+        check_in_whitelist(&whitelist, &regular, &[0, 1, 2, 3], &[]);
+        assert_eq!(whitelist.collect_whitelist().len(), 0);
+    }
+
+    #[test]
+    fn test_validators_in_whitelist() {
+        let regular = make_keys(&REGULAR_PEERS);
+        let validators = make_keys(&VALIDATORS[0]);
+        let mut whitelist = Whitelist::default();
+        whitelist.whitelist_on = true;
+        check_in_whitelist(&whitelist, &regular, &[], &[0, 1, 2, 3]);
+        check_in_whitelist(&whitelist, &validators, &[], &[0, 1]);
+        assert_eq!(whitelist.collect_whitelist().len(), 0);
+        whitelist.set_validators(validators.clone());
+        assert_eq!(whitelist.collect_whitelist().len(), 2);
+        check_in_whitelist(&whitelist, &regular, &[], &[0, 1, 2, 3]);
+        check_in_whitelist(&whitelist, &validators, &[0, 1], &[]);
+    }
+
+    #[test]
+    fn test_update_validators() {
+        let validators0 = make_keys(&VALIDATORS[0]);
+        let validators1 = make_keys(&VALIDATORS[1]);
+        let mut whitelist = Whitelist::default();
+        whitelist.whitelist_on = true;
+        assert_eq!(whitelist.collect_whitelist().len(), 0);
+        whitelist.set_validators(validators0.clone());
+        assert_eq!(whitelist.collect_whitelist().len(), 2);
+        check_in_whitelist(&whitelist, &validators0, &[0, 1], &[]);
+        check_in_whitelist(&whitelist, &validators1, &[], &[0, 1]);
+        whitelist.set_validators(validators1.clone());
+        assert_eq!(whitelist.collect_whitelist().len(), 2);
+        check_in_whitelist(&whitelist, &validators0, &[], &[0, 1]);
+        check_in_whitelist(&whitelist, &validators1, &[0, 1], &[]);
+    }
+}
