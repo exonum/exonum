b39dbdd3e2e13f4290cb1cc7feb3300e2e8ec21f
Stanislav Tkach <stanislav.tkach@xdev.re>
2017-06-13 12:12:58 +0300

    Rename 'validators'/'services' to 'validator_keys'/'service_keys'
    Former-commit-id: 1712c04f1331431f065f028f64545a70bbddaa86

--- exonum/src/blockchain/genesis.rs
+++ exonum/src/blockchain/genesis.rs
@@ -5,8 +5,8 @@ use super::config::ConsensusConfig;
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct GenesisConfig {
     pub consensus: ConsensusConfig,
-    pub validators: Vec<PublicKey>,
-    pub services: Vec<PublicKey>,
+    pub validator_keys: Vec<PublicKey>,
+    pub service_keys: Vec<PublicKey>,
 }
 
 impl GenesisConfig {
@@ -17,14 +17,14 @@ impl GenesisConfig {
     }
 
     pub fn new_with_consensus<I1, I2>(consensus: ConsensusConfig,
-                                      validators: I1,
-                                      services: I2) -> Self
+                                      validator_keys: I1,
+                                      service_keys: I2) -> Self
         where I1: Iterator<Item = PublicKey>, I2: Iterator<Item = PublicKey>
     {
         GenesisConfig {
             consensus: consensus,
-            validators: validators.collect(),
-            services: services.collect(),
+            validator_keys: validator_keys.collect(),
+            service_keys: service_keys.collect(),
         }
     }
 }
