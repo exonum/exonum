ad6d730f4d67fb069c960038a78e45f2765e96b9
Ivan Bohuslavskyi <ivan.bohuslavskyi@xdev.re>
2017-06-15 17:19:13 +0300

    optional command to set a path to the configuration file

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
-var api = require('./routes/api');
-var configuration = require('./routes/configuration');
 
+var commandLineArgs = require('command-line-args');
+var optionDefinitions = [{name: 'config', type: String}];
+var options = commandLineArgs(optionDefinitions);
+var configPath = options.config || './config.json';
+var config = require(configPath);
+app.set('config', config);
+
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/'));
 
+var api = require('./routes/api');
+var configuration = require('./routes/configuration');
 app.use('/api', api);
 app.use('/configuration', configuration);
 
--- frontend/routes/configuration.js
+++ frontend/routes/configuration.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var config = require('../config.json');
-
 router.get('/', function(req, res, next) {
+    var config = req.app.get('config');
     res.json({
         network_id: config.network_id,
         protocol_version: config.protocol_version,
