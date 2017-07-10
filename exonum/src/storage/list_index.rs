085b870772016d43e209f6122430a65d9f5a024f
Ivan Ivashchenko <ivan.ivashchenko@xdev.re>
2017-07-10 05:19:57 +0300

    Docs for ListIndex

--- exonum/src/storage/list_index.rs
+++ exonum/src/storage/list_index.rs
@@ -1,8 +1,14 @@
+//! An implementation of array list of items.
 use std::cell::Cell;
 use std::marker::PhantomData;
 
 use super::{BaseIndex, BaseIndexIter, Snapshot, Fork, StorageValue};
 
+/// A list of items that implemets `StorageValue` trait.
+///
+/// `ListIndex` implements an array list, storing the element as values and using `u64` as an index.
+/// `ListIndex` requires that the elements implement the [`StorageValue`] trait.
+/// [`StorageValue`]: ../trait.StorageValue.html
 #[derive(Debug)]
 pub struct ListIndex<T, V> {
     base: BaseIndex<T>,
@@ -10,12 +16,27 @@ pub struct ListIndex<T, V> {
     _v: PhantomData<V>,
 }
 
+/// An iterator over the items of a `ListIndex`.
+///
+/// This struct is created by the [`iter`] or
+/// [`iter_from`] methods on [`ListIndex`]. See its documentation for more.
+///
+/// [`iter`]: struct.ListIndex.html#method.iter
+/// [`iter_from`]: struct.ListIndex.html#method.iter_from
+/// [`ListIndex`]: struct.ListIndex.html
 #[derive(Debug)]
 pub struct ListIndexIter<'a, V> {
     base_iter: BaseIndexIter<'a, u64, V>,
 }
 
 impl<T, V> ListIndex<T, V> {
+    /// Creates a new index representation based on the common prefix of its keys and storage view.
+    ///
+    /// Storage view can be specified as [`&Snapshot`] or [`&mut Fork`]. In the first case only
+    /// immutable methods are available. In the second case both immutable and mutable methods are
+    /// available.
+    /// [`&Snapshot`]: ../trait.Snapshot.html
+    /// [`&mut Fork`]: ../struct.Fork.html
     pub fn new(prefix: Vec<u8>, base: T) -> Self {
         ListIndex {
             base: BaseIndex::new(prefix, base),
@@ -29,10 +50,12 @@ impl<T, V> ListIndex<T, V>
     where T: AsRef<Snapshot>,
           V: StorageValue
 {
+    /// Returns an element at that position or `None` if out of bounds.
     pub fn get(&self, index: u64) -> Option<V> {
         self.base.get(&index)
     }
 
+    /// Returns the last element of the list, or `None` if it is empty.
     pub fn last(&self) -> Option<V> {
         match self.len() {
             0 => None,
@@ -40,10 +63,12 @@ impl<T, V> ListIndex<T, V>
         }
     }
 
+    /// Returns `true` if the list contains no elements.
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Returns the number of elements in the list.
     pub fn len(&self) -> u64 {
         if let Some(len) = self.length.get() {
             return len;
@@ -53,10 +78,13 @@ impl<T, V> ListIndex<T, V>
         len
     }
 
+    /// Returns an iterator over the list. The iterator element type is T.
     pub fn iter(&self) -> ListIndexIter<V> {
         ListIndexIter { base_iter: self.base.iter_from(&(), &0u64) }
     }
 
+    /// Returns an iterator over the list starting from the specified position. The iterator
+    /// element type is T.
     pub fn iter_from(&self, from: u64) -> ListIndexIter<V> {
         ListIndexIter { base_iter: self.base.iter_from(&(), &from) }
     }
@@ -70,12 +98,14 @@ impl<'a, V> ListIndex<&'a mut Fork, V>
         self.length.set(Some(len));
     }
 
+    /// Appends an element to the back of the list.
     pub fn push(&mut self, value: V) {
         let len = self.len();
         self.base.put(&len, value);
         self.set_len(len + 1)
     }
 
+    /// Removes the last element from the list and returns it, or None if it is empty.
     pub fn pop(&mut self) -> Option<V> {
         // TODO: shoud we get and return dropped value?
         match self.len() {
@@ -89,6 +119,7 @@ impl<'a, V> ListIndex<&'a mut Fork, V>
         }
     }
 
+    /// Extends the list with the contents of an iterator.
     pub fn extend<I>(&mut self, iter: I)
         where I: IntoIterator<Item = V>
     {
@@ -101,6 +132,9 @@ impl<'a, V> ListIndex<&'a mut Fork, V>
         self.set_len(len);
     }
 
+    /// Shortens the list, keeping the first `len` elements and dropping the rest.
+    ///
+    /// If `len` is greater than the list's current length, this has no effect.
     pub fn truncate(&mut self, len: u64) {
         // TODO: optimize this
         while self.len() > len {
@@ -108,6 +142,10 @@ impl<'a, V> ListIndex<&'a mut Fork, V>
         }
     }
 
+    /// Changes a value at specified position.
+    ///
+    /// # Panics
+    /// Panics if `index` is equal or greater than the list's current length.
     pub fn set(&mut self, index: u64, value: V) {
         if index >= self.len() {
             panic!("index out of bounds: \
@@ -118,6 +156,12 @@ impl<'a, V> ListIndex<&'a mut Fork, V>
         self.base.put(&index, value)
     }
 
+    /// Clears the list, removing all values.
+    ///
+    /// # Notes
+    /// Currently this method is not optimized to delete large set of data. During the execution of
+    /// this method the amount of allocated memory is linearly dependent on the number of elements
+    /// in the index.
     pub fn clear(&mut self) {
         self.length.set(Some(0));
         self.base.clear()
