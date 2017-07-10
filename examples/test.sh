874bc4cdcdd5bb5a44e0ef621fe81683a09e721f
Denis Kolodin <denis.kolodin@xdev.re>
2017-07-10 15:36:53 +0300

    Add transactions examples

---
+++ examples/test.sh
@@ -0,0 +1,7 @@
+function send-transaction {
+    curl -H "Content-Type: application/json" -X POST -d @$1 http://127.0.0.1:8000/api/services/cryptocurrency/v1/wallets/transaction
+}
+
+send-transaction create-wallet-1.json
+send-transaction create-wallet-2.json
+send-transaction transfer-funds.json
