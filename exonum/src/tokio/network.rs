d259e1ba23afbb0c2c331e5407f7cce1d08b955a
Aleksei Sidorov <aleksei.sidorov@xdev.re>
2017-08-15 16:00:02 +0300

    Use events aggregator

--- exonum/src/tokio/network.rs
+++ exonum/src/tokio/network.rs
@@ -1,5 +1,5 @@
-use futures::{Future, Stream, Sink, IntoFuture};
-use futures::stream::MergedItem;
+use futures::{Future, Stream, Sink, IntoFuture, Poll, Async};
+use futures::stream::Fuse;
 use futures::future::Either;
 use futures::sync::mpsc;
 use tokio_core::net::{TcpListener, TcpStream};
@@ -14,11 +14,93 @@ use std::collections::hash_map::{HashMap, Entry};
 
 use messages::{Any, RawMessage};
 use events::{EventHandler, NetworkEvent};
+use node::{NodeTimeout, ExternalMessage};
 
 use super::error::{other_error, result_ok, forget_result, into_other, log_error};
 use super::codec::MessagesCodec;
 use super::Node;
 use super::handler::NetworkRequest;
+
+#[derive(Debug)]
+pub enum Event {
+    Network(NetworkEvent),
+    Timeout(NodeTimeout),
+    Api(ExternalMessage),
+}
+
+#[derive(Debug)]
+pub struct EventsAggregator<S1, S2, S3>
+where
+    S1: Stream,
+    S2: Stream,
+    S3: Stream,
+{
+    timeout: Fuse<S1>,
+    network: Fuse<S2>,
+    api: Fuse<S3>,
+}
+
+impl<S1, S2, S3> EventsAggregator<S1, S2, S3>
+where
+    S1: Stream,
+    S2: Stream,
+    S3: Stream,
+{
+    pub fn new(timeout: S1, network: S2, api: S3) -> EventsAggregator<S1, S2, S3> {
+        EventsAggregator {
+            network: network.fuse(),
+            timeout: timeout.fuse(),
+            api: api.fuse(),
+        }
+    }
+}
+
+impl<S1, S2, S3> Stream for EventsAggregator<S1, S2, S3>
+where
+    S1: Stream<Item = NodeTimeout>,
+    S2: Stream<
+        Item = NetworkEvent,
+        Error = S1::Error,
+    >,
+    S3: Stream<
+        Item = ExternalMessage,
+        Error = S1::Error,
+    >,
+{
+    type Item = Event;
+    type Error = S1::Error;
+
+    fn poll(&mut self) -> Poll<Option<Event>, Self::Error> {
+        let mut stream_finished = false;
+        // Check timeout events
+        match self.timeout.poll()? {
+            Async::Ready(Some(item)) => return Ok(Async::Ready(Some(Event::Timeout(item)))),
+            // Just finish stream
+            Async::Ready(None) => stream_finished = true,
+            Async::NotReady => {}
+        };
+        // Check network events
+        match self.network.poll()? {
+            Async::Ready(Some(item)) => return Ok(Async::Ready(Some(Event::Network(item)))),
+            // Just finish stream
+            Async::Ready(None) => stream_finished = true,
+            Async::NotReady => {}
+        };
+        // Check api events
+        match self.api.poll()? {
+            Async::Ready(Some(item)) => return Ok(Async::Ready(Some(Event::Api(item)))),
+            // Just finish stream
+            Async::Ready(None) => stream_finished = true,
+            Async::NotReady => {}
+        };
+
+        Ok(if stream_finished {
+            Async::Ready(None)
+        } else {
+            Async::NotReady
+        })
+    }
+}
 
 pub fn run_node_handler(node: Node) -> io::Result<()> {
     let (sender, receiver) = (node.channel.0, node.channel.1);
@@ -27,6 +109,7 @@ pub fn run_node_handler(node: Node) -> io::Result<()> {
     let (events_tx, events_rx) = mpsc::channel(64);
     let (requests_tx, requests_rx) = (sender.network, receiver.network);
     let timeouts_rx = receiver.timeout;
+    let api_rx = receiver.external;
 
     let events_tx = events_tx.clone();
     let requests_tx = requests_tx.clone();
@@ -183,21 +266,15 @@ pub fn run_node_handler(node: Node) -> io::Result<()> {
     let mut core = node.core;
     handler.initialize();
 
-    let events_handle = events_rx.merge(timeouts_rx).for_each(move |item| {
-        match item {
-            MergedItem::First(event) => {
-                handler.handle_network_event(event);
+    let events_handle = EventsAggregator::new(timeouts_rx, events_rx, api_rx)
+        .for_each(move |event| {
+            match event {
+                Event::Timeout(timeout) => handler.handle_timeout(timeout),
+                Event::Network(network) => handler.handle_network_event(network),
+                Event::Api(api) => handler.handle_application_event(api),
             }
-            MergedItem::Second(timeout) => {
-                handler.handle_timeout(timeout);
-            }
-            MergedItem::Both(event, timeout) => {
-                handler.handle_timeout(timeout);
-                handler.handle_network_event(event);
-            }
-        }
-        Ok(())
-    });
+            Ok(())
+        });
     core.run(events_handle).unwrap();
     debug!("Tokio: Handler thread is gone");
 
