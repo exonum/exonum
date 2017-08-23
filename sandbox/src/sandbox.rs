df6d6a30149e5802edb4bd074bf44ca1154b7e9d
Aleksei Sidorov <aleksei.sidorov@xdev.re>
2017-08-23 14:23:53 +0300

    Format code

--- sandbox/src/sandbox.rs
+++ sandbox/src/sandbox.rs
@@ -90,8 +90,8 @@ impl SandboxInner {
             while let Async::Ready(Some(network)) = self.network_requests_rx.poll()? {
                 match network {
                     NetworkRequest::SendMessage(peer, msg) => self.sent.push_back((peer, msg)),
-                    NetworkRequest::DisconnectWithPeer(_) => {},
-                    NetworkRequest::Shutdown => {},
+                    NetworkRequest::DisconnectWithPeer(_) => {}
+                    NetworkRequest::Shutdown => {}
                 }
             }
             Ok(())
