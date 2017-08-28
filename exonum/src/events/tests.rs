8c0600e090e173a676b52ecf6e2c7a29280afa40
Aleksei Sidorov <aleksei.sidorov@xdev.re>
2017-08-28 15:08:11 +0300

    Fix handshake test

--- exonum/src/events/tests.rs
+++ exonum/src/events/tests.rs
@@ -210,6 +210,7 @@ fn test_network_handshake() {
     let t1 = e1.spawn(move |e: &mut TestHandler| {
         e.connect_with(addrs[1]);
         assert_eq!(e.wait_for_connect(), c2);
+        assert_eq!(e.wait_for_disconnect(), addrs[1]);
     });
     let t2 = e2.spawn(move |e: &mut TestHandler| {
         assert_eq!(e.wait_for_connect(), c1);
