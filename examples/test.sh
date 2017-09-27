7e6802a322fc3800a0ee1bc6af0dfec8e5c809da
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-09-27 16:38:36 +0300

    Add comments to test script

--- examples/test.sh
+++ examples/test.sh
@@ -5,6 +5,8 @@ set -e
 # Exit status
 STATUS=0
 
+# Launches the cryptocurrency demo and waits until it starts listening
+# on the TCP port 8000.
 function launch-server {
     cd ..
     cargo run &
@@ -21,14 +23,24 @@ function launch-server {
     cd examples
 }
 
+# Kills whatever program is listening on the TCP port 8000, on which the cryptocurrency
+# demo needs to bind to.
 function kill-server {
     netstat -tlp 2>/dev/null | awk '{ if ($4 == "*:8000") { split($7, pid, /\//); print pid[1] } }' | xargs -r kill -KILL
 }
 
+# Sends a transaction to the cryptocurrency demo.
+#
+# Arguments:
+# - $1: filename with the transaction data
 function send-transaction {
     RESP=`curl -H "Content-Type: application/json" -X POST -d @$1 http://127.0.0.1:8000/api/services/cryptocurrency/v1/wallets/transaction 2>/dev/null`
 }
 
+# Checks a response to an Exonum transaction.
+#
+# Arguments:
+# - $1: expected start of the transaction hash returned by the server
 function check-transaction {
     if [[ `echo $RESP | jq .tx_hash` =~ ^\"$1 ]]; then
         echo "OK, got expected transaction hash $1"
@@ -38,6 +50,12 @@ function check-transaction {
     fi
 }
 
+# Checks a response to a read request.
+#
+# Arguments:
+# - $1: expected user name
+# - $2: expected user balance
+# - $3: response JSON that encodes user's wallet information
 function check-request {
     if [[ ( `echo $3 | jq .name` == "\"$1\"" ) && ( `echo $3 | jq .balance` == "\"$2\"" ) ]]; then
         echo "OK, got expected transaction balance $2 for user $1"
@@ -68,6 +86,9 @@ sleep 7
 
 echo "Retrieving info on all wallets..."
 RESP=`curl http://127.0.0.1:8000/api/services/cryptocurrency/v1/wallets 2>/dev/null`
+# Wallet records in the response are deterministically ordered by increasing
+# public key. As Johnny's pubkey is lexicographically lesser than Janie's, it it possible to
+# determine his wallet as .[0] and hers as .[1].
 check-request "Johnny Doe" 90 "`echo $RESP | jq .[0]`"
 check-request "Janie Roe" 110 "`echo $RESP | jq .[1]`"
 
