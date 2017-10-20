1fffa58e615fb9821a5277bd853d6c2f10e85368
Sergey Kletsun <sergey.kletsun@gmail.com>
2017-10-20 13:17:43 +0300

    Removed unused methods, added methods to work with list like with queue, various improvements of SparceListIndex methods.

--- exonum/src/storage/sparse_list_index.rs
+++ exonum/src/storage/sparse_list_index.rs
@@ -25,17 +25,13 @@ use super::{BaseIndex, BaseIndexIter, Snapshot, Fork, StorageValue};
 
 #[derive(Debug, Default, Clone, Copy)]
 struct SparseListSize {
-    /// Total list's length including spaces.
+    /// Total list's length including spaces. In fact points to the next index for a new element.
     capacity: u64,
-    /// Number of non-empty elements.
+    /// Amount of non-empty elements.
     length: u64,
 }
 
 impl SparseListSize {
-    fn zero() -> SparseListSize {
-        SparseListSize::default()
-    }
-
     fn to_array(&self) -> [u8; 16] {
         let mut buf = [0; 16];
         BigEndian::write_u64(&mut buf[0..8], self.capacity);
@@ -61,7 +57,9 @@ impl StorageValue for SparseListSize {
     }
 }
 
-/// The list of items is similar to the [`ListIndex`], but it may contain null items.
+/// The list of items is similar to the [`ListIndex`], but it may contain "spaces". For instance,
+/// list might contain six elements with indexes: "1, 2, 3, 5, 7, 8" (missing 4 and 6). And if you
+/// try to get element for index 4 or 6 you'll get None.
 ///
 /// `SparseListIndex` implements an array list, storing the element as values and using `u64`
 /// as an index.
@@ -85,6 +83,32 @@ pub struct SparseListIndex<T, V> {
 #[derive(Debug)]
 pub struct SparseListIndexIter<'a, V> {
     base_iter: BaseIndexIter<'a, u64, V>,
+}
+
+
+/// An iterator over the indices of a `SparseListIndex`.
+///
+/// This struct is created by the [`indices`] method on [`SparseListIndex`].
+/// See its documentation for more.
+///
+/// [`indices`]: struct.ListIndex.html#method.indices
+/// [`SparseListIndex`]: struct.SparseListIndex.html
+#[derive(Debug)]
+pub struct SparceListIndexKeys<'a> {
+    base_iter: BaseIndexIter<'a, u64, ()>,
+}
+
+
+/// An iterator over the values of a `SparseListIndex`.
+///
+/// This struct is created by the [`values`] method on [`SparseListIndex`].
+/// See its documentation for more.
+///
+/// [`values`]: struct.ListIndex.html#method.values
+/// [`SparseListIndex`]: struct.SparseListIndex.html
+#[derive(Debug)]
+pub struct SparceListIndexValues<'a, V> {
+    base_iter: BaseIndexIter<'a, (), V>,
 }
 
 impl<T, V> SparseListIndex<T, V> {
@@ -153,29 +177,6 @@ where
         self.base.get(&index)
     }
 
-
-    /// Returns the last element of the list, or `None` if it is empty.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use exonum::storage::{MemoryDB, Database, SparseListIndex};
-    ///
-    /// let db = MemoryDB::new();
-    /// let mut fork = db.fork();
-    /// let mut index = SparseListIndex::new(vec![1, 2, 3], &mut fork);
-    /// assert_eq!(None, index.last());
-    ///
-    /// index.push(42);
-    /// assert_eq!(Some(42), index.last());
-    /// ```
-    pub fn last(&self) -> Option<V> {
-        match self.len() {
-            0 => None,
-            l => self.get(l - 1),
-        }
-    }
-
     /// Returns `true` if the list contains no elements.
     ///
     /// # Examples
@@ -196,7 +197,10 @@ where
     }
 
 
-    /// Returns the total number of elements (including null elements) in the list.
+    /// Returns the total amount of elements (including "empty" elements) in the list. In fact
+    /// capacity reflects total amount of elements that have ever been put to the index and never
+    /// gets reduced. It's also used to produce an index value for new elements being appended to
+    /// the list.
     ///
     /// # Examples
     ///
@@ -221,7 +225,7 @@ where
         self.size().capacity
     }
 
-    /// Returns the total number of non-null elements in the list.
+    /// Returns the total amount of non-empty elements in the list.
     ///
     /// # Examples
     ///
@@ -245,7 +249,7 @@ where
         self.size().length
     }
 
-    /// Returns an iterator over the list. The iterator element type is V.
+    /// Returns an iterator over the list. The iterator element type is (u64, V).
     ///
     /// # Examples
     ///
@@ -259,15 +263,58 @@ where
     /// index.extend([1, 2, 3, 4, 5].iter().cloned());
     ///
     /// for val in index.iter() {
-    ///     println!("{}", val);
+    ///     println!("{:?}", val);
     /// }
     /// ```
     pub fn iter(&self) -> SparseListIndexIter<V> {
         SparseListIndexIter { base_iter: self.base.iter_from(&(), &0u64) }
     }
 
+    /// Returns an iterator over the indices of the 'SparceListIndex'.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use exonum::storage::{MemoryDB, Database, SparseListIndex};
+    ///
+    /// let db = MemoryDB::new();
+    /// let mut fork = db.fork();
+    /// let mut index = SparseListIndex::new(vec![1, 2, 3], &mut fork);
+    ///
+    /// index.extend([1, 2, 3, 4, 5].iter().cloned());
+    ///
+    /// for val in index.indices() {
+    ///     println!("{}", val);
+    /// }
+    /// ```
+    pub fn indices(&self) -> SparceListIndexKeys {
+        SparceListIndexKeys { base_iter: self.base.iter_from(&(), &0u64) }
+    }
+
+    /// Returns an iterator over the values of the 'SparceListIndex'. The iterator element type is
+    /// V.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use exonum::storage::{MemoryDB, Database, SparseListIndex};
+    ///
+    /// let db = MemoryDB::new();
+    /// let mut fork = db.fork();
+    /// let mut index = SparseListIndex::new(vec![1, 2, 3], &mut fork);
+    ///
+    /// index.extend([1, 2, 3, 4, 5].iter().cloned());
+    ///
+    /// for val in index.values() {
+    ///     println!("{}", val);
+    /// }
+    /// ```
+    pub fn values(&self) -> SparceListIndexValues<V> {
+        SparceListIndexValues { base_iter: self.base.iter_from(&(), &0u64) }
+    }
+
     /// Returns an iterator over the list starting from the specified position. The iterator
-    /// element type is V.
+    /// element type is (u64, V).
     ///
     /// # Examples
     ///
@@ -282,7 +329,7 @@ where
     /// index.remove(3);
     ///
     /// for val in index.iter_from(3) {
-    ///     println!("{}", val);
+    ///     println!("{:?}", val);
     /// }
     /// ```
     pub fn iter_from(&self, from: u64) -> SparseListIndexIter<V> {
@@ -338,14 +385,12 @@ where
     /// assert_eq!(Some(1), index.pop());
     /// ```
     pub fn pop(&mut self) -> Option<V> {
-        // TODO: shoud we get and return dropped value?
         let mut size = self.size();
-        match size.capacity {
+        match size.length {
             0 => None,
             l => {
                 let v = self.base.get(&(l - 1));
                 self.base.remove(&(l - 1));
-                size.capacity -= 1;
                 size.length -= 1;
                 self.set_size(size);
                 v
@@ -354,12 +399,7 @@ where
     }
 
     /// Removes the element with the given index from the list and returns it,
-    /// or None if it is empty. If elements count becomes zero after,
-    /// it also sets the `capacity` to zero.
-    ///
-    /// # Panics
-    ///
-    /// Panics if `index` is equal or greater than the list's current length.
+    /// or None if it is empty.
     ///
     /// # Examples
     ///
@@ -380,30 +420,24 @@ where
     /// assert_eq!(1, index.len());
 
     /// assert_eq!(Some(12), index.remove(1));
-    /// assert_eq!(0, index.capacity());
+    /// assert_eq!(2, index.capacity());
     /// ```
     pub fn remove(&mut self, index: u64) -> Option<V> {
         let mut size = self.size();
         if index >= size.capacity {
-            panic!(
-                "index out of bounds: \
+            warn!(
+                "Index out of bounds: \
                     the capacity is {} but the index is {}",
                 size.capacity,
                 index
             );
+            return None;
         }
         let v = self.base.get(&index);
         if v.is_some() {
-            if size.length == 1 {
-                self.clear();
-            } else {
-                self.base.remove(&index);
-                if index == size.capacity - 1 {
-                    size.capacity -= 1;
-                }
-                size.length -= 1;
-                self.set_size(size);
-            }
+            self.base.remove(&index);
+            size.length -= 1;
+            self.set_size(size);
         }
         v
     }
@@ -436,38 +470,10 @@ where
         self.set_size(size);
     }
 
-    /// Shortens the list, keeping the first `len` elements and dropping the rest.
-    ///
-    /// If `len` is greater than the list's current length, this has no effect.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use exonum::storage::{MemoryDB, Database, SparseListIndex};
-    ///
-    /// let db = MemoryDB::new();
-    /// let mut fork = db.fork();
-    /// let mut index = SparseListIndex::new(vec![1, 2, 3], &mut fork);
-    ///
-    /// index.extend([1, 2, 3, 4, 5].iter().cloned());
-    /// assert_eq!(5, index.capacity());
-    ///
-    /// index.truncate(3);
-    /// assert_eq!(3, index.capacity());
-    /// ```
-    pub fn truncate(&mut self, len: u64) {
-        // TODO: optimize this
-        while self.capacity() > len {
-            self.pop();
-        }
-    }
-
-    /// Changes a value at the specified position. If the position contains null value it
-    /// also increments elements count.
-    ///
-    /// # Panics
-    ///
-    /// Panics if `index` is equal or greater than the list's current length.
+    /// Changes a value at the specified position. If the position contains empty value it
+    /// also increments elements count. If a value of the index of new element is greater than
+    /// current capacity, the capacity of the list considered index + 1 and all further elements
+    /// without specific index value will be appended after this index.
     ///
     /// # Examples
     ///
@@ -486,17 +492,12 @@ where
     /// ```
     pub fn set(&mut self, index: u64, value: V) {
         let mut size = self.size();
-        if index >= size.capacity {
-            panic!(
-                "index out of bounds: \
-                    the capacity is {} but the index is {}",
-                size.capacity,
-                index
-            );
-        }
-        // Increment items count
+        // Update items count
         if self.base.get::<u64, V>(&index).is_none() {
             size.length += 1;
+            if index >= size.capacity {
+                size.capacity = index + 1;
+            }
             self.set_size(size);
         }
         self.base.put(&index, value)
@@ -527,8 +528,49 @@ where
     /// assert!(index.is_empty());
     /// ```
     pub fn clear(&mut self) {
-        self.size.set(Some(SparseListSize::zero()));
+        self.size.set(Some(SparseListSize::default()));
         self.base.clear()
+    }
+
+    /// Appends an element to the back of the 'SparceListIndex'.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use exonum::storage::{MemoryDB, Database, SparseListIndex};
+    ///
+    /// let db = MemoryDB::new();
+    /// let mut fork = db.fork();
+    /// let mut index = SparseListIndex::new(vec![1, 2, 3], &mut fork);
+    ///
+    /// index.push_back(1);
+    /// assert!(!index.is_empty());
+    /// ```
+    pub fn push_back(&mut self, value: V) {
+        self.push(value);
+    }
+
+    /// Removes the first element from the 'SparceListIndex' and returns it, or None if it is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use exonum::storage::{MemoryDB, Database, SparseListIndex};
+    ///
+    /// let db = MemoryDB::new();
+    /// let mut fork = db.fork();
+    /// let mut index = SparseListIndex::new(vec![1, 2, 3], &mut fork);
+    /// assert_eq!(None, index.pop());
+    ///
+    /// index.push_back(1);
+    /// assert_eq!(Some(1), index.pop_front());
+    /// ```
+    pub fn pop_front(&mut self) -> Option<V> {
+        let min_index = match self.indices().next() {
+            Some(index) => index,
+            None => 0,
+        };
+        self.remove(min_index)
     }
 }
 
@@ -538,7 +580,7 @@ where
     T: AsRef<Snapshot>,
     V: StorageValue,
 {
-    type Item = V;
+    type Item = (u64, V);
     type IntoIter = SparseListIndexIter<'a, V>;
 
     fn into_iter(self) -> Self::IntoIter {
@@ -547,6 +589,25 @@ where
 }
 
 impl<'a, V> Iterator for SparseListIndexIter<'a, V>
+where
+    V: StorageValue,
+{
+    type Item = (u64, V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.base_iter.next()
+    }
+}
+
+impl<'a> Iterator for SparceListIndexKeys<'a> {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.base_iter.next().map(|(k, ..)| k)
+    }
+}
+
+impl<'a, V> Iterator for SparceListIndexValues<'a, V>
 where
     V: StorageValue,
 {
@@ -574,7 +635,7 @@ mod tests {
 
         assert!(list_index.is_empty());
         assert_eq!(0, list_index.capacity());
-        assert!(list_index.last().is_none());
+        assert!(list_index.get(0).is_none());
 
         let extended_by = vec![45, 3422, 234];
         list_index.extend(extended_by);
@@ -587,7 +648,6 @@ mod tests {
 
         list_index.set(2, 777);
         assert_eq!(Some(777), list_index.get(2));
-        assert_eq!(Some(777), list_index.last());
         assert_eq!(3, list_index.capacity());
         assert_eq!(3, list_index.len());
 
@@ -603,29 +663,52 @@ mod tests {
         list_index.extend(extended_by_again);
         assert_eq!(7, list_index.capacity());
         assert_eq!(7, list_index.len());
-        assert_eq!(Some(1001), list_index.last());
 
         assert_eq!(Some(1001), list_index.pop());
-        assert_eq!(6, list_index.capacity());
+        assert_eq!(7, list_index.capacity());
         assert_eq!(6, list_index.len());
 
-        list_index.truncate(3);
+        list_index.pop();
+        list_index.pop();
+        list_index.pop();
 
-        assert_eq!(3, list_index.capacity());
-        assert_eq!(Some(777), list_index.last());
+        assert_eq!(7, list_index.capacity());
+        assert_eq!(Some(777), list_index.get(2));
 
         assert_eq!(Some(3422), list_index.remove(1));
         assert_eq!(None, list_index.remove(1));
-        assert_eq!(3, list_index.capacity());
+        assert_eq!(7, list_index.capacity());
         assert_eq!(2, list_index.len());
 
         assert_eq!(Some(777), list_index.remove(2));
-        assert_eq!(2, list_index.capacity());
+        assert_eq!(7, list_index.capacity());
         assert_eq!(1, list_index.len());
 
         assert_eq!(Some(45), list_index.remove(0));
-        assert_eq!(0, list_index.capacity());
+        assert_eq!(7, list_index.capacity());
         assert_eq!(0, list_index.len());
+        assert!(list_index.is_empty());
+
+        // check "queue methods"
+        list_index.push_back(42);
+        list_index.push_back(142);
+        list_index.push_back(242);
+        assert_eq!(10, list_index.capacity());
+        assert_eq!(3, list_index.len());
+        assert_eq!(Some(42), list_index.pop_front());
+        assert_eq!(10, list_index.capacity());
+        assert_eq!(2, list_index.len());
+        assert_eq!(Some(142), list_index.pop_front());
+        assert_eq!(10, list_index.capacity());
+        assert_eq!(1, list_index.len());
+        assert_eq!(Some(242), list_index.pop_front());
+        assert_eq!(10, list_index.capacity());
+        assert_eq!(0, list_index.len());
+        assert_eq!(None, list_index.pop_front());
+
+        // check that capacity gets overwritten by bigger index correctly
+        list_index.set(42, 1024);
+        assert_eq!(43, list_index.capacity());
     }
 
     fn list_index_iter(db: Box<Database>) {
@@ -633,17 +716,38 @@ mod tests {
         let mut list_index = SparseListIndex::new(vec![255], &mut fork);
 
         list_index.extend(vec![1u8, 15, 25, 2, 3]);
+        assert_eq!(
+            list_index.indices().collect::<Vec<u64>>(),
+            vec![0u64, 1, 2, 3, 4]
+        );
+        assert_eq!(
+            list_index.values().collect::<Vec<u8>>(),
+            vec![1u8, 15, 25, 2, 3]
+        );
+
         list_index.remove(1);
         list_index.remove(2);
 
-        assert_eq!(list_index.iter().collect::<Vec<u8>>(), vec![1, 2, 3]);
-
-        assert_eq!(list_index.iter_from(0).collect::<Vec<u8>>(), vec![1, 2, 3]);
-        assert_eq!(list_index.iter_from(1).collect::<Vec<u8>>(), vec![2, 3]);
         assert_eq!(
-            list_index.iter_from(5).collect::<Vec<u8>>(),
-            Vec::<u8>::new()
+            list_index.iter().collect::<Vec<(u64, u8)>>(),
+            vec![(0u64, 1u8), (3u64, 2u8), (4u64, 3u8)]
         );
+
+        assert_eq!(
+            list_index.iter_from(0).collect::<Vec<(u64, u8)>>(),
+            vec![(0u64, 1u8), (3u64, 2u8), (4u64, 3u8)]
+        );
+        assert_eq!(
+            list_index.iter_from(1).collect::<Vec<(u64, u8)>>(),
+            vec![(3u64, 2u8), (4u64, 3u8)]
+        );
+        assert_eq!(
+            list_index.iter_from(5).collect::<Vec<(u64, u8)>>(),
+            Vec::<(u64, u8)>::new()
+        );
+
+        assert_eq!(list_index.indices().collect::<Vec<u64>>(), vec![0u64, 3, 4]);
+        assert_eq!(list_index.values().collect::<Vec<u8>>(), vec![1u8, 2, 3]);
     }
 
     mod memorydb_tests {
