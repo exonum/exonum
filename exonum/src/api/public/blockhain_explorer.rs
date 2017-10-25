0ed45ca25678781eaf5219f64a21e4b21318c04f
Aleksei Sidorov <aleksei.sidorov@xdev.re>
2017-10-25 16:41:35 +0300

    Add block proofs range endpoint

--- exonum/src/api/public/blockhain_explorer.rs
+++ exonum/src/api/public/blockhain_explorer.rs
@@ -14,12 +14,13 @@
 
 use std::num::ParseIntError;
 use std::str::ParseBoolError;
+use std::cmp;
 
 use params::{Params, Value};
 use router::Router;
 use iron::prelude::*;
 
-use blockchain::{Blockchain, Block};
+use blockchain::{Block, Blockchain};
 use explorer::{BlockInfo, BlockchainExplorer};
 use api::{Api, ApiError};
 use helpers::Height;
@@ -60,11 +61,35 @@ impl ExplorerApi {
         let explorer = BlockchainExplorer::new(&self.blockchain);
         Ok(explorer.block_info(height))
     }
+
+    fn get_blocks_with_proofs(&self, from: Height, count: u64) -> Result<Vec<BlockInfo>, ApiError> {
+        if count > MAX_BLOCKS_PER_REQUEST {
+            return Err(ApiError::IncorrectRequest(
+                format!(
+                    "Max block count per request exceeded ({})",
+                    MAX_BLOCKS_PER_REQUEST
+                ).into(),
+            ));
+        }
+        let current_height = self.blockchain.last_block().height().0 + 1;
+        let from = from.0;
+        let count = cmp::min(current_height.saturating_sub(from), count);
+        let to = from + count;
+
+        let explorer = BlockchainExplorer::new(&self.blockchain);
+        let block_proofs = {
+            let mut block_proofs = Vec::new();
+            for height in from..to {
+                block_proofs.push(explorer.block_info(Height(height)).unwrap());
+            }
+            block_proofs
+        };
+        Ok(block_proofs)
+    }
 }
 
 impl Api for ExplorerApi {
     fn wire(&self, router: &mut Router) {
-
         let _self = self.clone();
         let blocks = move |req: &mut Request| -> IronResult<Response> {
             let map = req.get_ref::<Params>().unwrap();
@@ -81,11 +106,11 @@ impl Api for ExplorerApi {
                 }
             };
             let latest: Option<u64> = match map.find(&["latest"]) {
-                Some(&Value::String(ref from_str)) => {
-                    Some(from_str.parse().map_err(|e: ParseIntError| {
+                Some(&Value::String(ref from_str)) => Some(from_str.parse().map_err(
+                    |e: ParseIntError| {
                         ApiError::IncorrectRequest(Box::new(e))
-                    })?)
-                }
+                    },
+                )?),
                 _ => None,
             };
             let skip_empty_blocks: bool = match map.find(&["skip_empty_blocks"]) {
@@ -119,7 +144,43 @@ impl Api for ExplorerApi {
             }
         };
 
+        let _self = self.clone();
+        let blocks_with_proofs = move |req: &mut Request| -> IronResult<Response> {
+            let map = req.get_ref::<Params>().unwrap();
+            let count: u64 = match map.find(&["count"]) {
+                Some(&Value::String(ref count_str)) => {
+                    count_str.parse().map_err(|e: ParseIntError| {
+                        ApiError::IncorrectRequest(Box::new(e))
+                    })?
+                }
+                _ => {
+                    return Err(ApiError::IncorrectRequest(
+                        "Required parameter of blocks 'count' is missing".into(),
+                    ))?;
+                }
+            };
+            let from: u64 = match map.find(&["from"]) {
+                Some(&Value::String(ref from_str)) => {
+                    from_str.parse().map_err(|e: ParseIntError| {
+                        ApiError::IncorrectRequest(Box::new(e))
+                    })?
+                }
+                _ => {
+                    return Err(ApiError::IncorrectRequest(
+                        "Required parameter of blocks 'from' is missing".into(),
+                    ))?;
+                }
+            };
+            let info = _self.get_blocks_with_proofs(Height(from), count)?;
+            _self.ok_response(&::serde_json::to_value(info).unwrap())
+        };
+
         router.get("/v1/blocks", blocks, "blocks");
         router.get("/v1/blocks/:height", block, "height");
+        router.get(
+            "/v1/blocks-with-proofs",
+            blocks_with_proofs,
+            "blocks_with_proofs",
+        );
     }
 }
