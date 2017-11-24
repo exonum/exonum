db906bbcea5513474f323386e07e13951d0d4926
Serhii Oryshych <serhii.oryshych@xdev.re>
2017-11-24 14:59:23 +0200

    Change crate type. Rewrite 'execute' for transaction

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! This crate defines the oracle for Exonum.
-
 /*
 #![deny(missing_debug_implementations)]
 #![deny(missing_docs)]
