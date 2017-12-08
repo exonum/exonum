271ea883af7cc4bebf0900f090ea6306bb903922
Ivan Ivashchenko <defuz.net@gmail.com>
2017-12-08 14:00:20 +0200

    Make Change public again

--- exonum/src/storage/mod.rs
+++ exonum/src/storage/mod.rs
@@ -107,7 +107,7 @@
 //! [`HashSet`]: https://doc.rust-lang.org/std/collections/struct.HashSet.html
 
 pub use self::error::Error;
-pub use self::db::{Database, Snapshot, Fork, Patch, Changes, Iterator, Iter};
+pub use self::db::{Database, Snapshot, Fork, Patch, Change, Changes, Iterator, Iter};
 
 pub use self::rocksdb::{RocksDB, RocksDBOptions};
 pub use self::memorydb::MemoryDB;
