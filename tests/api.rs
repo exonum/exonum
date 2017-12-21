ba8e0e01b03dc08e6231941788720830a5b9d6d6
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-12-21 22:50:15 +0200

    Rename crate to `exonum-cryptocurrency`

--- tests/api.rs
+++ tests/api.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-extern crate cryptocurrency;
 extern crate exonum;
+extern crate exonum_cryptocurrency as cryptocurrency;
 extern crate exonum_testkit;
 
 use exonum::crypto::{self, PublicKey, SecretKey};
