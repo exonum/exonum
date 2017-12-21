e4f608b8a80d330aafb4afe622e824f4493b5dfd
Ilya Bogdanov <fumlead@gmail.com>
2017-12-21 23:01:34 +0300

    Add license to float module; fix doc tests

--- exonum/src/encoding/float.rs
+++ exonum/src/encoding/float.rs
@@ -1,3 +1,17 @@
+// Copyright 2017 The Exonum Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::mem;
 use std::error::Error;
 
@@ -42,11 +56,12 @@ impl F32 {
     ///
     /// ```
     /// use exonum::encoding::F32;
+    /// use std::f32;
     ///
     /// let val = F32::try_from(1.0);
     /// assert!(val.is_some());
     ///
-    /// let val = F32::try_from(f32::NaN);
+    /// let val = F32::try_from(f32::NAN);
     /// assert!(val.is_none());
     /// ```
     pub fn try_from(value: f32) -> Option<Self> {
@@ -104,11 +119,12 @@ impl F64 {
     ///
     /// ```
     /// use exonum::encoding::F64;
+    /// use std::f64;
     ///
     /// let val = F64::try_from(1.0);
     /// assert!(val.is_some());
     ///
-    /// let val = F64::try_from(f64::NaN);
+    /// let val = F64::try_from(f64::NAN);
     /// assert!(val.is_none());
     /// ```
     pub fn try_from(value: f64) -> Option<Self> {
