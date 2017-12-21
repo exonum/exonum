c943ac824fd46b30e5e955740a66200e796a0cb5
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-12-21 22:49:49 +0200

    Move `main.rs` to an example

--- src/lib.rs
+++ src/lib.rs
@@ -26,10 +26,9 @@ extern crate iron;
 
 // Import necessary types from crates.
 
-use exonum::blockchain::{Blockchain, Service, GenesisConfig, ValidatorKeys, Transaction,
-                         ApiContext};
+use exonum::blockchain::{Blockchain, Service, Transaction, ApiContext};
 use exonum::encoding::serialize::FromHex;
-use exonum::node::{NodeConfig, NodeApiConfig, TransactionSend, ApiSender};
+use exonum::node::{TransactionSend, ApiSender};
 use exonum::messages::{RawTransaction, Message};
 use exonum::storage::{Fork, MapIndex};
 use exonum::crypto::{PublicKey, Hash};
@@ -328,40 +327,5 @@ impl Service for CurrencyService {
         };
         api.wire(&mut router);
         Some(Box::new(router))
-    }
-}
-
-pub fn node_config() -> NodeConfig {
-    let (consensus_public_key, consensus_secret_key) = exonum::crypto::gen_keypair();
-    let (service_public_key, service_secret_key) = exonum::crypto::gen_keypair();
-
-    let validator_keys = ValidatorKeys {
-        consensus_key: consensus_public_key,
-        service_key: service_public_key,
-    };
-    let genesis = GenesisConfig::new(vec![validator_keys].into_iter());
-
-    let api_address = "0.0.0.0:8000".parse().unwrap();
-    let api_cfg = NodeApiConfig {
-        public_api_address: Some(api_address),
-        ..Default::default()
-    };
-
-    let peer_address = "0.0.0.0:2000".parse().unwrap();
-
-    NodeConfig {
-        listen_address: peer_address,
-        peers: vec![],
-        service_public_key,
-        service_secret_key,
-        consensus_public_key,
-        consensus_secret_key,
-        genesis,
-        external_address: None,
-        network: Default::default(),
-        whitelist: Default::default(),
-        api: api_cfg,
-        mempool: Default::default(),
-        services_configs: Default::default(),
     }
 }
