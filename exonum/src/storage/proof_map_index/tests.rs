5d334550be25a4c06d6a0a8c8f7a373f979e6786
Aleksei <aleksei.sidorov@xdev.re>
2018-01-10 12:28:45 +0300

    Add debug output

--- exonum/src/storage/proof_map_index/tests.rs
+++ exonum/src/storage/proof_map_index/tests.rs
@@ -136,6 +136,8 @@ fn insert_simple(db1: Box<Database>, db2: Box<Database>) {
 }
 
 fn insert_reverse(db1: Box<Database>, db2: Box<Database>) {
+    let _ = ::helpers::init_logger();
+
     let mut storage1 = db1.fork();
     let mut index1 = ProofMapIndex::new(IDX_NAME, &mut storage1);
     index1.put(&[42; 32], vec![1]);
@@ -144,6 +146,7 @@ fn insert_reverse(db1: Box<Database>, db2: Box<Database>) {
     index1.put(&[245; 32], vec![4]);
     index1.put(&[250; 32], vec![5]);
     index1.put(&[255; 32], vec![6]);
+    debug!("index1={:#?}", index1);
 
     let mut storage2 = db2.fork();
     let mut index2 = ProofMapIndex::new(IDX_NAME, &mut storage2);
@@ -153,7 +156,7 @@ fn insert_reverse(db1: Box<Database>, db2: Box<Database>) {
     index2.put(&[240; 32], vec![3]);
     index2.put(&[64; 32], vec![2]);
     index2.put(&[42; 32], vec![1]);
-
+    debug!("index2={:#?}", index2);
 
     assert!(index2.root_hash() != Hash::zero());
     assert_eq!(index2.root_hash(), index1.root_hash());
