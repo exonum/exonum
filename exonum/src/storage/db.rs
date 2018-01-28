52ed8a377e475e00055c7786c9ae9251d436ccf8
Alex Ostrovski <ostrovski.alex@gmail.com>
2018-01-28 02:15:54 +0200

    Clarify `Fork` docs

--- exonum/src/storage/db.rs
+++ exonum/src/storage/db.rs
@@ -150,25 +150,27 @@ pub enum Change {
 
 /// A combination of a database snapshot and a sequence of changes on top of it.
 ///
-/// A `Fork` provides both immutable and mutable operations over database. As well as [`Snapshot`],
-/// it provides read isolation. When mutable operations ([`put`], [`remove`] and
-/// [`remove_by_prefix`] methods) are performed for fork, the data is presented as if these changes
-/// are applied to the database. However, instead of applying changes directly to the database,
-/// fork only accumulates these changes in memory.
+/// A `Fork` provides both immutable and mutable operations over database. Like [`Snapshot`],
+/// `Fork` provides read isolation. When mutable operations ([`put`], [`remove`] and
+/// [`remove_by_prefix`]) are performed for a fork, the subsequent reads act as if the changes
+/// are applied to the database; in reality, these changes are accumulated in memory.
 ///
-/// To apply changes to the database, you need to convert a `Fork` into a [`Patch`] using method
-/// [`into_patch`] and then atomically merge it into the database using method [`merge`]. If two
-/// conflicting forks are merged into a database, this can lead to a non-consistent state. If you
+/// To apply changes to the database, you need to convert a `Fork` into a [`Patch`] using
+/// [`into_patch`] and then atomically [`merge`] it into the database. If two
+/// conflicting forks are merged into a database, this can lead to an inconsistent state. If you
 /// need to consistently apply several sets of changes for the same data, the next fork should be
 /// created after the previous fork has been merged.
 ///
-/// `Fork` also supports a checkpoint mechanism (methods [`checkpoint`], [`commit`] and
-/// [`rollback`]) that allows you to rollback some of the latest changes if for some reason you
-/// can not provide a consistent state after the changes due to a runtime error.
+/// `Fork` also supports checkpoints ([`checkpoint`], [`commit`] and
+/// [`rollback`] methods), which allows to rollback some of the latest changes (e.g., after
+/// a runtime error).
 ///
-/// `Fork` implements [`Snapshot`] trait and provides all the necessary methods for both reading and
-/// writing data from the database, so `&mut Fork` is used as a storage view for creating
+/// `Fork` implements the [`Snapshot`] trait and provides methods for both reading and
+/// writing data. Thus, `&mut Fork` is used as a storage view for creating
 /// read-write indices representation.
+///
+/// **Note.** Unless stated otherwise, "key" in the method descriptions below refers
+/// to a full key (a string column family name + key as an array of bytes within the family).
 ///
 /// [`Snapshot`]: trait.Snapshot.html
 /// [`put`]: #method.put
@@ -348,7 +350,7 @@ impl Fork {
     ///
     /// # Panics
     ///
-    /// Panics if another checkpoint was created before and it was not committed or rollbacked yet.
+    /// Panics if another checkpoint was created before and has not been committed or rolled back.
     pub fn checkpoint(&mut self) {
         if self.logged {
             panic!("call checkpoint before rollback or commit");
@@ -356,12 +358,12 @@ impl Fork {
         self.logged = true;
     }
 
-    /// Finalizes all changes after the last checkpoint.
+    /// Finalizes all changes after the latest checkpoint.
     ///
     /// # Panics
     ///
-    /// Panics if checkpoint was not created before or last checkpoint is already committed or
-    /// rollbacked.
+    /// Panics if there is no active checkpoint, or the latest checkpoint
+    /// is already committed or rolled back.
     pub fn commit(&mut self) {
         if !self.logged {
             panic!("call commit before checkpoint");
@@ -370,12 +372,12 @@ impl Fork {
         self.logged = false;
     }
 
-    /// Rollbacks all changes after the last checkpoint.
+    /// Rolls back all changes after the latest checkpoint.
     ///
     /// # Panics
     ///
-    /// Panics if checkpoint was not created before or last checkpoint is already committed or
-    /// rollbacked.
+    /// Panics if there is no active checkpoint, or the latest checkpoint
+    /// is already committed or rolled back.
     pub fn rollback(&mut self) {
         if !self.logged {
             panic!("call rollback before checkpoint");
@@ -391,7 +393,7 @@ impl Fork {
         self.logged = false;
     }
 
-    /// Inserts the key-value pair into the fork with the given name `name`.
+    /// Inserts a key-value pair into the fork.
     pub fn put(&mut self, name: &str, key: Vec<u8>, value: Vec<u8>) {
         let changes = self.patch.changes_entry(name.to_string()).or_insert_with(
             Changes::new,
@@ -407,7 +409,7 @@ impl Fork {
         }
     }
 
-    /// Removes the key from the fork with the given name `name`.
+    /// Removes the key from the fork.
     pub fn remove(&mut self, name: &str, key: Vec<u8>) {
         let changes = self.patch.changes_entry(name.to_string()).or_insert_with(
             Changes::new,
@@ -423,7 +425,8 @@ impl Fork {
         }
     }
 
-    /// Removes all keys starting with the specified prefix from the fork with name `name`.
+    /// Removes all keys starting with the specified prefix from the column family
+    /// with the given `name`.
     pub fn remove_by_prefix(&mut self, name: &str, prefix: Option<&Vec<u8>>) {
         let changes = self.patch.changes_entry(name.to_string()).or_insert_with(
             Changes::new,
@@ -467,8 +470,8 @@ impl Fork {
 
     /// Merges patch from another fork to this fork.
     ///
-    /// If both forks have changed the same data, this can lead to a non-consistent state. So this
-    /// method is useful only if you are sure that forks interacted with different indices.
+    /// If both forks have changed the same data, this can lead to an inconsistent state. Hence,
+    /// this method is useful only if you are sure that forks interacted with different indices.
     ///
     /// # Panics
     ///
