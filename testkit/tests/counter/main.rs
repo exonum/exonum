a311b63d299b71bb5ba025ffdf91b811040bb432
Stanislav Tkach <stanislav.tkach@gmail.com>
2018-02-25 01:26:10 +0200

    Update testkit test

--- testkit/tests/counter/main.rs
+++ testkit/tests/counter/main.rs
@@ -641,7 +641,7 @@ fn test_explorer_transaction_statuses() {
     assert_status(
         &api,
         &panicking_tx,
-        &json!({ "type": "panic", "description": "" }),
+        &json!({ "type": "panic", "description": "attempt to add with overflow" }),
     );
 }
 
