2096791b60944a6881b9acb1b2dbd75cc253af32
Alexander Ponimaskin <ponimaskin@gmail.com>
2018-03-07 18:45:29 +0300

    Comments fix

--- exonum/src/blockchain/config.rs
+++ exonum/src/blockchain/config.rs
@@ -46,9 +46,9 @@ pub struct StoredConfiguration {
     pub validator_keys: Vec<ValidatorKeys>,
     /// Consensus algorithm parameters.
     pub consensus: ConsensusConfig,
-    /// Number of votes to commit configuration
-    /// Should be greater or equal to 2/3 of validators count
     pub majority_count: Option<usize>,
+    /// Number of votes to commit configuration.
+    /// Should be greater than 2/3 and less or equal to the validators count.
     /// Services specific variables.
     /// Keys are `service_name` from `Service` trait and values are the serialized json.
     pub services: BTreeMap<String, serde_json::Value>,
