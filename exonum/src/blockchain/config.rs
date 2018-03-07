25b90e9244fed61afca116160e283176832193fa
mvlabat <mvlabat@gmail.com>
2018-03-07 14:23:45 +0200

    Rename DEFAULT_MAX_MESSAGE_LEN constant

--- exonum/src/blockchain/config.rs
+++ exonum/src/blockchain/config.rs
@@ -70,7 +70,7 @@ pub struct ConsensusConfig {
 
 impl ConsensusConfig {
     /// Default value for max_message_len.
-    pub const DEFAULT_MESSAGE_MAX_LEN: u32 = 1024 * 1024; // 1 MB
+    pub const DEFAULT_MAX_MESSAGE_LEN: u32 = 1024 * 1024; // 1 MB
 }
 
 impl Default for ConsensusConfig {
@@ -80,7 +80,7 @@ impl Default for ConsensusConfig {
             status_timeout: 5000,
             peers_timeout: 10_000,
             txs_block_limit: 1000,
-            max_message_len: Self::DEFAULT_MESSAGE_MAX_LEN,
+            max_message_len: Self::DEFAULT_MAX_MESSAGE_LEN,
             timeout_adjuster: TimeoutAdjusterConfig::Constant { timeout: 500 },
         }
     }
