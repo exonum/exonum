5dd8afdd932f4b34ffc69c0d8343071e1ebfb126
Ivan Bohuslavskyi <ivan.bohuslavskyi@xdev.re>
2018-03-16 16:17:11 +0200

    update js files formatting

--- frontend/routes/api.js
+++ frontend/routes/api.js
@@ -3,36 +3,36 @@ var request = require('request');
 var router = express.Router();
 
 router.get('/*', function(req, res, next) {
-    var query = req.params[0];
+  var query = req.params[0];
 
-    request.get({
-        url: req.app.get('apiRoot') + '/api/' + query,
-        qs: req.query
-    }, function(err, response, body) {
-        if (err) {
-            return next(err);
-        }
-        try {
-            res.json(JSON.parse(body));
-        } catch (e) {
-            res.json({});
-        }
-    });
+  request.get({
+    url: req.app.get('apiRoot') + '/api/' + query,
+    qs: req.query
+  }, function(err, response, body) {
+    if (err) {
+      return next(err);
+    }
+    try {
+      res.json(JSON.parse(body));
+    } catch (e) {
+      res.json({});
+    }
+  });
 });
 
 router.post('/*', function(req, res, next) {
-    var query = req.params[0];
+  var query = req.params[0];
 
-    request.post({
-            url: req.app.get('apiRoot') + '/api/' + query,
-            json: req.body
-        },
-        function(err, response, body) {
-            if (err) {
-                return next(err);
-            }
-            res.json(body);
-        });
+  request.post({
+      url: req.app.get('apiRoot') + '/api/' + query,
+      json: req.body
+    },
+    function(err, response, body) {
+      if (err) {
+        return next(err);
+      }
+      res.json(body);
+    });
 });
 
 module.exports = router;
