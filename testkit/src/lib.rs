1ea3af1a42993b824379ce1006516afb639bb291
Alex Ostrovski <ostrovski.alex@gmail.com>
2018-04-13 18:53:12 +0300

    Move testkit server docs to correct place

--- testkit/src/lib.rs
+++ testkit/src/lib.rs
@@ -184,6 +184,53 @@ mod poll_events;
 
 /// Builder for `TestKit`.
 ///
+/// # Testkit server
+///
+/// By calling the [`serve`] method, you can transform testkit into a web server useful for
+/// client-side testing. The testkit-specific APIs are exposed on the private address
+/// with the `/api/testkit` prefix (hereinafter denoted as `{baseURL}`).
+/// In all APIs, the request body (if applicable) and response are JSON-encoded.
+///
+/// ## Testkit status
+///
+/// GET `{baseURL}/v1/status`
+///
+/// Outputs the status of the testkit, which includes:
+///
+/// - Current blockchain height
+/// - Current [test network configuration][cfg]
+/// - Next network configuration if it is scheduled with [`commit_configuration_change`].
+///
+/// ## Create block
+///
+/// POST `{baseURL}/v1/blocks`
+///
+/// Creates a new block in the testkit blockchain. If the
+/// JSON body of the request is an empty object, the call is functionally equivalent
+/// to [`create_block`]. Otherwise, if the body has the `tx_hashes` field specifying an array
+/// of transaction hashes, the call is equivalent to [`create_block_with_tx_hashes`] supplied
+/// with these hashes.
+///
+/// Returns the latest block from the blockchain on success.
+///
+/// ## Roll back
+///
+/// DELETE `{baseURL}/v1/blocks/:height`
+///
+/// Acts as a rough [`rollback`] equivalent. The blocks are rolled back up and including the block
+/// at the specified `height` (a positive integer), so that after the request the blockchain height
+/// is equal to `height - 1`. If the specified height is greater than the blockchain height,
+/// the request performs no action.
+///
+/// Returns the latest block from the blockchain on success.
+///
+/// [`serve`]: #method.serve
+/// [cfg]: struct.TestNetworkConfiguration.html
+/// [`create_block`]: struct.TestKit.html#method.create_block
+/// [`create_block_with_tx_hashes`]: struct.TestKit.html#method.create_block_with_tx_hashes
+/// [`commit_configuration_change`]: struct.TestKit.html#method.commit_configuration_change
+/// [`rollback`]: struct.TestKit.html#method.rollback
+///
 /// # Example
 ///
 /// ```
@@ -309,62 +356,15 @@ impl TestKitBuilder {
     ///
     /// Unlike real Exonum nodes, the testkit web server does not create peer-to-peer connections
     /// with other nodes, and does not create blocks automatically. The only way to commit
-    /// transactions is thus to use the [testkit API](struct.TestKit.html#testkit-apis).
+    /// transactions is thus to use the [testkit API](#testkit-server).
     pub fn serve(self, public_api_address: SocketAddr, private_api_address: SocketAddr) {
         let testkit = self.create();
-        testkit.run(public_api_address, private_api_address)
+        testkit.run(public_api_address, private_api_address);
     }
 }
 
 /// Testkit for testing blockchain services. It offers simple network configuration emulation
 /// (with no real network setup).
-///
-/// # Testkit APIs
-///
-/// By calling the [`run`] method, you can transform testkit into a web server useful for
-/// client-side testing. The testkit-specific APIs are exposed on the private address
-/// with the `/api/testkit` prefix (hereinafter denoted as `{baseURL}`).
-/// In all APIs, the request body (if applicable) and response are JSON-encoded.
-///
-/// ## Testkit status
-///
-/// GET `{baseURL}/v1/status`
-///
-/// Outputs the status of the testkit, which includes:
-///
-/// - Current blockchain height
-/// - Current [test network configuration][cfg]
-/// - Next network configuration if it is scheduled with [`commit_configuration_change`].
-///
-/// ## Create block
-///
-/// POST `{baseURL}/v1/blocks`
-///
-/// Creates a new block in the testkit blockchain. If the
-/// JSON body of the request is an empty object, the call is functionally equivalent
-/// to [`create_block`]. Otherwise, if the body has the `tx_hashes` field specifying an array
-/// of transaction hashes, the call is equivalent to [`create_block_with_tx_hashes`] supplied
-/// with these hashes.
-///
-/// Returns the latest block from the blockchain on success.
-///
-/// ## Roll back
-///
-/// DELETE `{baseURL}/v1/blocks/:height`
-///
-/// Acts as a rough [`rollback`] equivalent. The blocks are rolled back up and including the block
-/// at the specified `height` (a positive integer), so that after the request the blockchain height
-/// is equal to `height - 1`. If the specified height is greater than the blockchain height,
-/// the request performs no action.
-///
-/// Returns the latest block from the blockchain on success.
-///
-/// [`run`]: #method.run
-/// [cfg]: struct.TestNetworkConfiguration.html
-/// [`create_block`]: #method.create_block
-/// [`create_block_with_tx_hashes`]: #method.create_block_with_tx_hashes
-/// [`commit_configuration_change`]: #method.commit_configuration_change
-/// [`rollback`]: #method.rollback
 pub struct TestKit {
     blockchain: Blockchain,
     db_handler: CheckpointDbHandler<MemoryDB>,
