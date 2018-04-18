d706b2c5ddecd053add6290486dc3870bc774deb
Igor Aleksanov <popzxc@yandex.ru>
2018-04-18 14:43:01 +0300

    Transactions are now rejected if they were already commited

--- exonum/src/node/consensus.rs
+++ exonum/src/node/consensus.rs
@@ -617,6 +617,16 @@ impl NodeHandler {
         let mut fork = self.blockchain.fork();
         {
             let mut schema = Schema::new(&mut fork);
+            if schema.transactions().contains(&hash)
+                && !schema.transactions_pool().contains(&hash)
+            {
+                error!(
+                    "Received already committed transaction, hash={:?}",
+                    hash
+                );
+                return;
+            }
+
             schema.add_transaction_into_pool(msg.raw().clone());
         }
         self.blockchain
