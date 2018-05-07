5d66e272c2e275699af54acb90204ab824071b72
Andrey Luchin <andreyluchin29@gmail.com>
2018-05-07 15:07:43 +0300

    Non-compiling implementation

--- exonum/src/events/internal.rs
+++ exonum/src/events/internal.rs
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use futures::{self, sync::mpsc, Future, Sink, Stream};
+use futures::{self, sync::mpsc, Future, Sink, Stream, future};
 use tokio_core::reactor::{Handle, Timeout};
+use tokio_threadpool::{Builder as ThreadPoolBuilder};
 
 use std::{
     io, time::{Duration, SystemTime},
@@ -22,6 +23,8 @@ use std::{
 use super::{
     error::{into_other, other_error}, to_box, InternalEvent, InternalRequest, TimeoutRequest,
 };
+use blockchain::Transaction;
+use crypto::Hash;
 
 #[derive(Debug)]
 pub struct InternalPart {
@@ -31,6 +34,23 @@ pub struct InternalPart {
 
 impl InternalPart {
     pub fn run(self, handle: Handle) -> Box<dyn Future<Item = (), Error = io::Error>> {
+        // Default number of threads = number of cores.
+        let thread_pool = ThreadPoolBuilder::new().build();
+
+        // Buffer in a channel wouldn't do anything except clutter the memory.
+        let (pool_tx, pool_rx) = mpsc::channel::<Box<Transaction>>(0);
+        let internal_tx = self.internal_tx.clone();
+        thread_pool.spawn(futures::lazy(move || {
+            pool_rx.for_each(|tx| {
+                if tx.verify() {
+                    internal_tx.send(InternalEvent::TxVerified(tx));
+                }
+                Ok(())
+            })
+        }));
+
+        let mut txs_in_verification = HashSet::<Hash>::new();
+
         let internal_tx = self.internal_tx.clone();
         let fut = self.internal_requests_rx
             .for_each(move |request| {
@@ -69,6 +89,19 @@ impl InternalPart {
                                 .map(drop)
                                 .map_err(into_other)
                         }).map_err(|_| panic!("Can't execute shutdown"));
+                        to_box(f)
+                    }
+                    InternalRequest::VerifyTx(tx) => {
+                        if !txs_in_verification.insert(tx.raw().hash()) {
+                            let f = future::ok::<(), ()>(());
+                            return to_box(f);
+                        }
+                        let f = futures::lazy(move || {
+                            pool_tx
+                                .send(tx)
+                                .map(drop)
+                                .map_err(into_other)
+                        }).map_err(|_| panic!("Can't send tx for verification to the thread pool"));
                         to_box(f)
                     }
                 };
