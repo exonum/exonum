878cf2dbacb129b810a0d0fb9051a60972f0bd45
VladYermakov <yermakov.v.o@gmail.com>
2018-05-23 13:27:12 +0300

    Add MetaInfo struct after exceptionally delete

--- exonum/src/api/public/system.rs
+++ exonum/src/api/public/system.rs
@@ -25,6 +25,12 @@ struct MemPoolInfo {
     pub size: usize,
 }
 
+#[derive(Serialize, Deserialize)]
+struct MetaInfo<'a> {
+    pub version: &'a str,
+    pub services: Vec<&'a str>
+}
+
 #[doc(hidden)]
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
 pub struct HealthCheckInfo {
