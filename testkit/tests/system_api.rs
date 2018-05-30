bbe3ce4f79329a237e04836456e30459e550717a
VladYermakov <yermakov.v.o@gmail.com>
2018-05-30 18:12:17 +0300

    Fix cargo fmt

--- testkit/tests/system_api.rs
+++ testkit/tests/system_api.rs
@@ -17,10 +17,10 @@ extern crate exonum_testkit;
 #[macro_use]
 extern crate pretty_assertions;
 
-use exonum::api::public::HealthCheckInfo;
 use exonum::api::private::NodeInfo;
-use exonum::messages::PROTOCOL_MAJOR_VERSION;
+use exonum::api::public::HealthCheckInfo;
 use exonum::helpers::user_agent;
+use exonum::messages::PROTOCOL_MAJOR_VERSION;
 use exonum_testkit::{ApiKind, TestKitBuilder};
 
 #[test]
@@ -49,7 +49,10 @@ fn test_network() {
     let api = testkit.api();
     let info: NodeInfo = api.get_private(ApiKind::System, "/v1/network");
 
-    assert_eq!(info.core_version, option_env!("CARGO_PKG_VERSION").map(|ver| ver.to_owned()));
+    assert_eq!(
+        info.core_version,
+        option_env!("CARGO_PKG_VERSION").map(|ver| ver.to_owned())
+    );
     assert_eq!(info.protocol_version, PROTOCOL_MAJOR_VERSION);
     assert!(info.services.is_empty());
 }
