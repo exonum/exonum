114afd437fef484e7e91aaed8b1c0c9a8e0775b1
Alexander Ponimaskin <ponimaskin@gmail.com>
2018-05-31 14:51:06 +0300

    docs updated

--- exonum/src/blockchain/service.rs
+++ exonum/src/blockchain/service.rs
@@ -180,8 +180,9 @@ pub trait Service: Send + Sync + 'static {
     /// A service execution. This method is invoked for each service after execution
     /// of all transactions in the block but before `handle_commit` handler.
     ///
-    /// The order of invoking `execute` method for every service depends on the service ID.
-    /// `execute` for the service with the smallest ID is invoked first up to the largest one.
+    /// The order of invoking `execute_before_commit` method for every service depends on the
+    /// service ID. `execute_before_commit` for the service with the smallest ID is invoked
+    /// first up to the largest one.
     /// Effectively, this means that services should not rely on a particular ordering of
     /// Service::execute invocations.
     fn execute_before_commit(&self, fork: &mut Fork) {}
