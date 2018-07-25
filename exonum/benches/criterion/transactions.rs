60220f5c9c5bbcc8bd06bf2df06f536d57ee934c
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2018-07-25 14:32:43 +0300

    Fix benchmarks

--- exonum/benches/criterion/transactions.rs
+++ exonum/benches/criterion/transactions.rs
@@ -167,7 +167,7 @@ impl TransactionsBenchmarkRunner {
         let handler = TransactionsHandler {
             inner: Some(NodeHandler::new(
                 blockchain,
-                node_config.external_address.unwrap(),
+                node_config.external_address,
                 channel.node_sender(),
                 system_state,
                 config,
@@ -231,7 +231,7 @@ impl TransactionsBenchmarkRunner {
 
         NodeConfig {
             listen_address: peer_address,
-            external_address: Some(peer_address),
+            external_address: "127.0.0.1:2000".parse().unwrap(),
             service_public_key,
             service_secret_key,
             consensus_public_key,
