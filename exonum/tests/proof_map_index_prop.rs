ec19a10d48007b330af111833359e0efb30b3972
Vitaly Drogan <vitaliy.drogan@gmail.com>
2018-08-17 09:23:06 +0300

    Fix spell

--- exonum/tests/proof_map_index_prop.rs
+++ exonum/tests/proof_map_index_prop.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// cspell:ignore oneof
+
 //! Property testing for proof map index as a rust collection.
 
 extern crate exonum;
