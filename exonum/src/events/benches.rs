57beefbd2829998f749f6edc0525d88c524e43f8
Vitaly Drogan <vitaliy.drogan@gmail.com>
2018-09-13 23:07:38 +0300

    Fix bench build

--- exonum/src/events/benches.rs
+++ exonum/src/events/benches.rs
@@ -49,10 +49,10 @@ fn bench_network(b: &mut Bencher, addrs: [SocketAddr; 2], cfg: &BenchConfig) {
         let mut connect_list = ConnectList::default();
 
         let mut params1 = ConnectionParams::from_address(first);
-        connect_list.add(params1.connect_info);
+        connect_list.add(params1.connect_info.clone());
 
         let mut params2 = ConnectionParams::from_address(second);
-        connect_list.add(params2.connect_info);
+        connect_list.add(params2.connect_info.clone());
 
         let connect_list = SharedConnectList::from_connect_list(connect_list);
 
