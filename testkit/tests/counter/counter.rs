69d55249e9cbfcf04b583acff36a206734498c29
Stanislav Tkach <stanislav.tkach@xdev.re>
2018-09-24 11:31:00 +0300

    Fix formatting

--- testkit/tests/counter/counter.rs
+++ testkit/tests/counter/counter.rs
@@ -50,7 +50,10 @@ impl<'a> CounterSchema<&'a mut Fork> {
     }
 
     fn inc_count(&mut self, inc: u64) -> u64 {
-        let count = self.count().unwrap_or(0).checked_add(inc).expect("attempt to add with overflow");
+        let count = self.count()
+            .unwrap_or(0)
+            .checked_add(inc)
+            .expect("attempt to add with overflow");
         self.entry_mut().set(count);
         count
     }
