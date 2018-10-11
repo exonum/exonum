59c97395ad30fff36e58fde997b5df055f974745
Ivan Bohuslavskyi <boguslavsky@users.noreply.github.com>
2018-10-11 10:40:27 +0300

    Update demos (#991)

--- examples/timestamping/frontend/tests/blockchain.test.js
+++ examples/timestamping/frontend/tests/blockchain.test.js
@@ -12,27 +12,17 @@ const hexRegex = /[0-9A-Fa-f]+/i;
 
 Vue.use(Blockchain)
 
-mock.onPost('/api/services/timestamping/v1/timestamps', {
-  'protocol_version': 0,
-  'service_id': 130,
-  'message_id': 0,
-  'signature': 'c79d59272f2734e65e2b657584025aa26512ba7d3784438f21ea8789c2dd3ffd2ca15be3b0cdff70e92f17bf8a8e9e9d2c582dcad64c46870bc09a4ef2491702',
-  'body': {
-    'pub_key': '0fe4d28f33b4c37ea2f6b433cc572f60d02b3f5b1638b0427dda2d7f5c028533',
-    'content': {
-      'content_hash': '966c80fec91149a85b2a496113aca0d9fefbc0edec6e4b2f8d0b24aaea9445f8',
-      'metadata': 'Some contract'
-    }
-  }
+mock.onPost('/api/explorer/v1/transactions', {
+  tx_body: '727b3af198eccbc76f4b942640f1f32c67731f1dd410859f191956ea3789b628000082000000080000002c000000504d270ff8e300b8b48aeb1b61e7f96f6a369fc93321187d678c06b4ac5b8d4f2800000004000000746573747ca84d8ddf5c7f1efcb3f96f6710208c4b590fc3fd6b6396d3fa9bc7dad4ade40e3b7b793b00e5311de945143da8c2537c946c6ada9eccfa0d01f4f185caaa04'
 }).reply(200)
 
-mock.onGet('/api/explorer/v1/transactions?hash=069020ce9a066404b8c527558146ea05b072e986d3fd586a9790d9d89829fc72').replyOnce(200, txNotAccepted)
+mock.onGet('/api/explorer/v1/transactions?hash=05d621b1a62163e132e74430afe8d01ed873e68bb8e9f7abd9b7c72e1c7dbdc2').replyOnce(200, txNotAccepted)
 
-mock.onGet('/api/explorer/v1/transactions?hash=069020ce9a066404b8c527558146ea05b072e986d3fd586a9790d9d89829fc72').replyOnce(200, txAccepted)
+mock.onGet('/api/explorer/v1/transactions?hash=05d621b1a62163e132e74430afe8d01ed873e68bb8e9f7abd9b7c72e1c7dbdc2').replyOnce(200, txAccepted)
 
 mock.onGet('/api/services/configuration/v1/configs/actual').reply(200, actual)
 
-mock.onGet('/api/services/timestamping/v1/timestamps/proof?hash=ce15c12c3d03d11f317acf503195b61853088f55b82b2495f243211927bc35d6').reply(200, proof)
+mock.onGet('/api/services/timestamping/v1/timestamps/proof?hash=504d270ff8e300b8b48aeb1b61e7f96f6a369fc93321187d678c06b4ac5b8d4f').reply(200, proof)
 
 describe('Interaction with blockchain', () => {
   it('should generate new signing key pair', () => {
@@ -46,28 +36,28 @@ describe('Interaction with blockchain', () => {
 
   it('should create new timestamp', async () => {
     const keyPair = {
-      publicKey: '0fe4d28f33b4c37ea2f6b433cc572f60d02b3f5b1638b0427dda2d7f5c028533',
-      secretKey: '1d061f0ed3fd21b6776b41c22d1e850621c647aa9b87b4e54b1564bc3e70e5480fe4d28f33b4c37ea2f6b433cc572f60d02b3f5b1638b0427dda2d7f5c028533'
+      publicKey: '727b3af198eccbc76f4b942640f1f32c67731f1dd410859f191956ea3789b628',
+      secretKey: '658f5106aecfa46129ada041dd080d7ae3007f78300cdc1411f5bb11cda0bfab727b3af198eccbc76f4b942640f1f32c67731f1dd410859f191956ea3789b628'
     }
-    const hash = '966c80fec91149a85b2a496113aca0d9fefbc0edec6e4b2f8d0b24aaea9445f8'
-    const metadata = 'Some contract'
+    const hash = '504d270ff8e300b8b48aeb1b61e7f96f6a369fc93321187d678c06b4ac5b8d4f'
+    const metadata = 'test'
 
     await expect(Vue.prototype.$blockchain.createTimestamp(keyPair, hash, metadata)).resolves
   })
 
   it('should get timestamp proof and verify it', async () => {
-    const hash = 'ce15c12c3d03d11f317acf503195b61853088f55b82b2495f243211927bc35d6'
+    const hash = '504d270ff8e300b8b48aeb1b61e7f96f6a369fc93321187d678c06b4ac5b8d4f'
 
     await expect(Vue.prototype.$blockchain.getTimestampProof(hash)).resolves.toEqual({
       'time': {
-        'nanos': 6577000,
-        'secs': '1531152169'
+        'nanos': 660266000,
+        'secs': '1538746046'
       },
       'timestamp': {
-        'content_hash': 'ce15c12c3d03d11f317acf503195b61853088f55b82b2495f243211927bc35d6',
-        'metadata': ''
+        'content_hash': hash,
+        'metadata': 'test'
       },
-      'tx_hash': 'a5fa42cb59197fde244307227b9d177a49fe443a87a566beead2dc48577f149a'
+      'tx_hash': '05d621b1a62163e132e74430afe8d01ed873e68bb8e9f7abd9b7c72e1c7dbdc2'
     })
   })
 })
