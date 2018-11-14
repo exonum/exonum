25b1a51cf2e1bf5ee7ef53637cf6015d1ad4b75d
Vitaly Drogan <vitaliy.drogan@gmail.com>
2018-11-14 18:01:51 +0200

    Use protobuf for tx in exonum crate

--- exonum/src/sandbox/timestamping.rs
+++ exonum/src/sandbox/timestamping.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub use encoding::protobuf::tests::TimestampTx;
+
 use rand::{RngCore, SeedableRng};
 use rand_xorshift::XorShiftRng;
 
