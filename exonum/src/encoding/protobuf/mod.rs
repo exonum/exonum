dd5f26ba36c3c924d05e3e846370203c12faf720
Aleksey Sidorov <alekseysidorov@users.noreply.github.com>
2018-11-30 15:27:53 +0300

    Update clippy and polish ci files. (#1073)

--- exonum/src/encoding/protobuf/mod.rs
+++ exonum/src/encoding/protobuf/mod.rs
@@ -222,7 +222,7 @@ where
 {
     type ProtoStruct = Vec<T::ProtoStruct>;
     fn to_pb(&self) -> Self::ProtoStruct {
-        self.into_iter().map(|v| v.to_pb()).collect()
+        self.iter().map(|v| v.to_pb()).collect()
     }
     fn from_pb(pb: Self::ProtoStruct) -> Result<Self, ()> {
         pb.into_iter()
