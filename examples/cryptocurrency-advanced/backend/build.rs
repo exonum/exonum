561fd2c70a04732012b52cc5648b49fcad77f77a
Vitaly Drogan <vitaliy.drogan@gmail.com>
2018-12-05 12:59:24 +0200

    Export function

--- examples/cryptocurrency-advanced/backend/build.rs
+++ examples/cryptocurrency-advanced/backend/build.rs
@@ -1,9 +1,9 @@
 extern crate exonum_build;
 
-use exonum_build::protobuf_generate;
+use exonum_build::{get_exonum_protobuf_files_path, protobuf_generate};
 
 fn main() {
-    let exonum_protos = exonum_build::get_exonum_protobuf_files_path();
+    let exonum_protos = get_exonum_protobuf_files_path();
     protobuf_generate(
         "src/proto",
         &["src/proto", &exonum_protos],
--- testkit/build.rs
+++ testkit/build.rs
@@ -1,11 +1,11 @@
 extern crate exonum_build;
 
-use exonum_build::protobuf_generate;
+use exonum_build::{get_exonum_protobuf_files_path, protobuf_generate};
 
 fn main() {
     protobuf_generate("src/proto", &["src/proto"], "testkit_protobuf_mod.rs");
 
-    let exonum_protos = exonum_build::get_exonum_protobuf_files_path();
+    let exonum_protos = get_exonum_protobuf_files_path();
     protobuf_generate(
         "tests/inflating_currency/proto",
         &["tests/inflating_currency/proto", &exonum_protos],
