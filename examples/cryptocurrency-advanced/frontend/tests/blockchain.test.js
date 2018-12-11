bc9eeabaacf814df055f310c0e92276fc7130d45
Kate Negrienko <katya@negrienko.com>
2018-12-11 11:29:45 +0200

    update string

--- examples/cryptocurrency-advanced/frontend/tests/blockchain.test.js
+++ examples/cryptocurrency-advanced/frontend/tests/blockchain.test.js
@@ -8,7 +8,7 @@ import proof from './data/proof.json'
 const mock = new MockAdapter(axios)
 const bigIntRegex = /[0-9]+/i;
 const hexRegex = /[0-9A-Fa-f]+/i;
-const TRANSACTION_URL = '/api/services/cryptocurrency/v1/wallets/transaction'
+const TRANSACTION_URL = '/api/explorer/v1/transactions'
 const TRANSACTION_EXPLORER_URL = '/api/explorer/v1/transactions?hash='
 const PROOF_URL = '/api/services/cryptocurrency/v1/wallets/info?pub_key='
 const keyPair = {
@@ -20,9 +20,7 @@ Vue.use(Blockchain)
 
 // Mock `createWallet` transaction
 const createWalletTxHash = 'c0649d252aed811b185d0868534dc11d86017c6d277ed87fdfb8d8a87df463d8'
-mock.onPost(TRANSACTION_URL, {
-  'tx_body': '24cf3dd648b98abd9f76b427bcf32c2db4c509efa323c07dfbdad54b2bb9e87b0000800002000a084a6f686e20446f65987e239da64e6bf40e8302f677c52f5ae39b928bdac17aaba516c6f492a0fef2bdd3cada6d62fea7f914c60c06eef3dc84eba88830bf0c05a688747447f3fb0b'
-}).replyOnce(200)
+mock.onPost(`${TRANSACTION_URL}`, {"tx_body":"24cf3dd648b98abd9f76b427bcf32c2db4c509efa323c07dfbdad54b2bb9e87b0000800002000a084a6f686e20446f65987e239da64e6bf40e8302f677c52f5ae39b928bdac17aaba516c6f492a0fef2bdd3cada6d62fea7f914c60c06eef3dc84eba88830bf0c05a688747447f3fb0b"}).replyOnce(200)
 
 mock.onGet(`${TRANSACTION_EXPLORER_URL}${createWalletTxHash}`).replyOnce(200, { 'type': 'in-pool' })
 
@@ -56,14 +54,14 @@ describe('Interaction with blockchain', () => {
 
     expect(seed).toMatch(bigIntRegex)
   })
-  /*
+  
 
   it('should create new wallet', async () => {
     const name = 'John Doe'
 
     await expect(Vue.prototype.$blockchain.createWallet(keyPair, name)).resolves
   })
-
+/*
   it('should add funds', async () => {
     const amountToAdd = '50'
     const seed = '6684106035020060923'
