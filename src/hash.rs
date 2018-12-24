a5d4c3f0c5df92c7806b6d0747731dafb2d6b317
pavel-mukhanov <mukhanovpv@gmail.com>
2018-12-24 18:22:03 +0300

    Fix fmt.

--- src/hash.rs
+++ src/hash.rs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-use byteorder::{LittleEndian, ByteOrder};
+use byteorder::{ByteOrder, LittleEndian};
 use hex::FromHex;
 
 use crate::BinaryValue;
