0878a771fcab7b57c888cc32f5cd6cc0e1ffd475
pavel-mukhanov <mukhanovpv@gmail.com>
2019-07-31 18:05:13 +0300

    Fix tests.

--- exonum/src/node/state.rs
+++ exonum/src/node/state.rs
@@ -935,7 +935,7 @@ impl State {
                 let mut unknown_txs = HashSet::new();
                 for hash in msg.transactions() {
                     if check_tx(hash, transactions, &self.tx_cache) {
-                        if !transaction_pool.contains(hash) {
+                        if !self.tx_cache.contains_key(hash) && !transaction_pool.contains(hash) {
                             bail!(
                                 "Received propose with already \
                                  committed transaction"
@@ -1001,7 +1001,7 @@ impl State {
         let mut unknown_txs = HashSet::new();
         for hash in msg.transactions() {
             if check_tx(hash, &txs, &self.tx_cache) {
-                if !txs_pool.contains(hash) {
+                if !self.tx_cache.contains_key(hash) && !txs_pool.contains(hash) {
                     panic!(
                         "Received block with already \
                          committed transaction"
