9f48181388468cf5dc98e76af0b49ed780edfaa4
SergNikolaev <iintersergey@gmail.com>
2019-08-13 18:26:46 +0500

    timestamping tests and view fixed cryptocurrency-advanced tests and links fixed

--- examples/timestamping/frontend/src/pages/Blockchain.vue
+++ examples/timestamping/frontend/src/pages/Blockchain.vue
@@ -1,28 +1,30 @@
 <template>
   <div>
-    <div class="container mt-5">
-      <div class="row justify-content-sm-center">
-        <div class="col-md-6 col-md-offset-3">
-          <h1>Latest blocks</h1>
-
-          <ul class="list-group mt-5">
-            <li class="list-group-item font-weight-bold">
-              <div class="row">
-                <div class="col-sm-6">Height</div>
-                <div class="col-sm-6">Transactions count</div>
-              </div>
-            </li>
-            <li v-for="block in blocks" :key="block.height" class="list-group-item">
-              <div class="row">
-                <div class="col-sm-6">
-                  <router-link :to="{ name: 'block', params: { height: block.height } }">{{ block.height }}</router-link>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="card mt-5">
+            <div class="card-header">Latest blocks</div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item font-weight-bold">
+                <div class="row">
+                  <div class="col-sm-6">Block height</div>
+                  <div class="col-sm-6">Transactions count</div>
                 </div>
-                <div class="col-sm-6">{{ block.tx_count }}</div>
-              </div>
-            </li>
-          </ul>
-
-          <button class="btn btn-lg btn-block btn-primary mt-3" @click.prevent="loadMore">Show previous blocks</button>
+              </li>
+              <li v-for="(block) in blocks" :key="block.height" class="list-group-item">
+                <div class="row">
+                  <div class="col-sm-6">
+                    <router-link :to="{ name: 'block', params: { height: block.height } }">{{ block.height }}</router-link>
+                  </div>
+                  <div class="col-sm-6">{{ block.tx_count }}</div>
+                </div>
+              </li>
+            </ul>
+            <div class="card-body text-center">
+              <a href="#" class="btn btn-primary" @click.prevent="loadMore">Load older blocks</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -40,8 +42,8 @@
     },
     data() {
       return {
-        blocks: [],
-        isSpinnerVisible: false
+        isSpinnerVisible: false,
+        blocks: []
       }
     },
     methods: {
