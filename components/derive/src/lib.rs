10ead94088b5c442823c15d3946392786f0f23da
Aleksei Sidorov <aleksei.sidorov@xdev.re>
2019-08-19 17:04:10 +0300

    Port ServiceFactory derive to darling

--- components/derive/src/lib.rs
+++ components/derive/src/lib.rs
@@ -14,7 +14,6 @@
 
 //! This crate provides macros for deriving some useful methods and traits for the exonum services.
 
-#![recursion_limit = "256"]
 #![deny(unsafe_code, bare_trait_objects)]
 #![warn(missing_docs, missing_debug_implementations)]
 
@@ -26,16 +25,11 @@ mod pb_convert;
 mod service_factory;
 
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{Lit, Meta, MetaList, MetaNameValue, NestedMeta, Path};
+use syn::{Lit, Meta};
 
 // Exonum derive attribute names, used as
 // `#[exonum( [ ATTRIBUTE_NAME = ATTRIBUTE_VALUE or ATTRIBUTE_NAME ],* )]`
 const CRATE_PATH_ATTRIBUTE: &str = "crate";
-const ARTIFACT_NAME: &str = "artifact_name";
-const ARTIFACT_VERSION: &str = "artifact_version";
-const PROTO_SOURCES: &str = "proto_sources";
-const SERVICE_CONSTRUCTOR: &str = "with_constructor";
 const SERVICE_DISPATCHER: &str = "dispatcher";
 
 /// Derives `ProtobufConvert` trait.
@@ -185,68 +179,4 @@ pub fn exonum_service(attr: TokenStream, item: TokenStream) -> TokenStream {
 #[proc_macro_derive(IntoExecutionError, attributes(exonum))]
 pub fn generate_into_execution_error(input: TokenStream) -> TokenStream {
     execution_error::implement_execution_error(input)
-}
-
-/// Exonum types should be imported with `crate::` prefix if inside crate
-/// or with `exonum::` when outside.
-fn get_exonum_types_prefix(attrs: &[Meta]) -> impl quote::ToTokens {
-    let map_attrs = get_exonum_name_value_attributes(attrs);
-    let crate_path = map_attrs.into_iter().find_map(|nv| match &nv {
-        MetaNameValue {
-            lit: Lit::Str(path),
-            ident,
-            ..
-        } if ident == CRATE_PATH_ATTRIBUTE => Some(
-            path.parse::<Path>()
-                .expect("failed to parse crate path in the attribute"),
-        ),
-        _ => None,
-    });
-
-    if let Some(path) = crate_path {
-        quote!(#path)
-    } else {
-        quote!(exonum)
-    }
-}
-
-/// Extract attributes in the form of `#[exonum(name = "value")]`
-fn get_exonum_attributes(exonum_meta: Option<Meta>) -> Vec<Meta> {
-    match exonum_meta {
-        Some(Meta::List(MetaList { nested: list, .. })) => list
-            .into_iter()
-            .filter_map(|n| match n {
-                NestedMeta::Meta(meta) => Some(meta),
-                _ => None,
-            })
-            .collect(),
-        Some(_) => panic!("`exonum` attribute should contain list of name value pairs"),
-        None => vec![],
-    }
-}
-
-fn get_exonum_name_value_attributes(meta_attrs: &[Meta]) -> Vec<MetaNameValue> {
-    let exonum_meta = meta_attrs.iter().find(|m| m.name() == "exonum").cloned();
-
-    get_exonum_attributes(exonum_meta)
-        .into_iter()
-        .filter_map(|meta| match meta {
-            Meta::NameValue(name_value) => Some(name_value),
-            _ => None,
-        })
-        .collect()
-}
-
-fn find_attribute_path(meta_attrs: &[Meta], name: &str) -> Option<Path> {
-    let map_attrs = get_exonum_name_value_attributes(meta_attrs);
-    map_attrs.into_iter().find_map(|nv| {
-        if nv.ident == name {
-            match nv.lit {
-                Lit::Str(path) => Some(path.parse::<Path>().unwrap()),
-                _ => None,
-            }
-        } else {
-            None
-        }
-    })
 }
--- components/derive/src/service_factory.rs
+++ components/derive/src/service_factory.rs
@@ -12,90 +12,121 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use darling::FromDeriveInput;
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{DeriveInput, Lit, Meta, Path};
+use proc_macro2::Span;
+use quote::{quote, ToTokens};
+use semver::Version;
+use syn::{DeriveInput, Ident, Path};
 
-use super::{
-    find_attribute_path, get_exonum_name_value_attributes, get_exonum_types_prefix, ARTIFACT_NAME,
-    ARTIFACT_VERSION, PROTO_SOURCES, SERVICE_CONSTRUCTOR,
-};
-
-fn find_exonum_literal_attribute(meta_attrs: &[Meta], ident_name: &str) -> Option<Lit> {
-    let map_attrs = get_exonum_name_value_attributes(meta_attrs);
-    map_attrs.into_iter().find_map(|nv| {
-        if nv.ident == ident_name {
-            Some(nv.lit)
-        } else {
-            None
-        }
-    })
+#[derive(Debug, FromDeriveInput)]
+#[darling(attributes(exonum), forward_attrs(allow, doc, cfg))]
+#[darling(default)]
+struct ServiceFactory {
+    ident: Ident,
+    #[darling(rename = "crate")]
+    cr: Path,
+    artifact_name: Option<String>,
+    artifact_version: Option<String>,
+    proto_sources: Option<Path>,
+    with_constructor: Option<Path>,
 }
 
-fn get_protobuf_sources_mod_path(meta_attrs: &[Meta]) -> Path {
-    let map_attrs = get_exonum_name_value_attributes(meta_attrs);
-    let struct_path = map_attrs.into_iter().find_map(|nv| {
-        if nv.ident == PROTO_SOURCES {
-            match nv.lit {
-                Lit::Str(path) => Some(path.parse::<Path>().unwrap()),
-                _ => None,
+impl Default for ServiceFactory {
+    fn default() -> Self {
+        Self {
+            ident: Ident::new("unreachable", Span::call_site()),
+            cr: syn::parse_str("exonum").unwrap(),
+            artifact_name: None,
+            artifact_version: None,
+            proto_sources: None,
+            with_constructor: None,
+        }
+    }
+}
+
+impl ServiceFactory {
+    fn artifact_name(&self) -> impl ToTokens {
+        if let Some(ref artifact_name) = self.artifact_name {
+            // TODO check artifact name
+            quote! { #artifact_name }
+        } else {
+            quote! { env!("CARGO_PKG_NAME") }
+        }
+    }
+
+    fn artifact_version(&self) -> impl ToTokens {
+        if let Some(ref artifact_version) = self.artifact_version {
+            // Check that artifact version is semver compatible.
+            Version::parse(artifact_version).expect("Unable to parse artifact version");
+
+            quote! { #artifact_version }
+        } else {
+            quote! { env!("CARGO_PKG_VERSION") }
+        }
+    }
+
+    fn artifact_id(&self) -> impl ToTokens {
+        let artifact_name = self.artifact_name();
+        let artifact_version = self.artifact_version();
+        quote! { concat!(#artifact_name, ":", #artifact_version) }
+    }
+
+    fn service_constructor(&self) -> impl ToTokens {
+        if let Some(ref path) = self.with_constructor {
+            quote! { #path(self) }
+        } else {
+            quote! { Box::new(Self) }
+        }
+    }
+
+    fn artifact_protobuf_spec(&self) -> impl ToTokens {
+        let cr = &self.cr;
+        let proto_sources_mod = self
+            .proto_sources
+            .as_ref()
+            .expect("`proto_sources` attribute is not set properly");
+
+        quote! {
+            #cr::runtime::ArtifactProtobufSpec {
+                sources: #proto_sources_mod::PROTO_SOURCES.as_ref(),
             }
-        } else {
-            None
         }
-    });
-    struct_path.unwrap_or_else(|| panic!("{} attribute is not set properly.", PROTO_SOURCES))
+    }
 }
 
-macro_rules! literal_or_default {
-    ($meta_attrs:expr, $name:expr, $default:expr) => {
-        if let Some(lit) = find_exonum_literal_attribute($meta_attrs, $name) {
-            quote! { #lit }
-        } else {
-            quote! { $default }
+impl ToTokens for ServiceFactory {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let name = &self.ident;
+        let cr = &self.cr;
+        let artifact_id = self.artifact_id();
+        let artifact_protobuf_spec = self.artifact_protobuf_spec();
+        let service_constructor = self.service_constructor();
+
+        let expanded = quote! {
+            impl #cr::runtime::rust::ServiceFactory for #name {
+                fn artifact_id(&self) -> #cr::runtime::rust::RustArtifactId {
+                    #artifact_id.parse().unwrap()
+                }
+
+                fn artifact_protobuf_spec(&self) -> #cr::runtime::ArtifactProtobufSpec {
+                    #artifact_protobuf_spec
+                }
+
+                fn create_instance(&self) -> Box<dyn #cr::runtime::rust::Service> {
+                    #service_constructor
+                }
+            }
         };
-    };
+        tokens.extend(expanded)
+    }
 }
 
 pub fn implement_service_factory(input: TokenStream) -> TokenStream {
     let input: DeriveInput = syn::parse(input).unwrap();
-    let meta_attrs = input
-        .attrs
-        .iter()
-        .filter_map(|a| a.parse_meta().ok())
-        .collect::<Vec<_>>();
 
-    let name = input.ident.clone();
-    let cr = get_exonum_types_prefix(&meta_attrs);
-    let proto_sources_mod = get_protobuf_sources_mod_path(&meta_attrs);
-
-    let artifact_name = literal_or_default!(&meta_attrs, ARTIFACT_NAME, env!("CARGO_PKG_NAME"));
-    let artifact_version =
-        literal_or_default!(&meta_attrs, ARTIFACT_VERSION, env!("CARGO_PKG_VERSION"));
-    let service_constructor =
-        if let Some(path) = find_attribute_path(&meta_attrs, SERVICE_CONSTRUCTOR) {
-            quote! { #path(self) }
-        } else {
-            quote! { Box::new(Self) }
-        };
-
-    let expanded = quote! {
-        impl #cr::runtime::rust::ServiceFactory for #name {
-            fn artifact_id(&self) -> #cr::runtime::rust::RustArtifactId {
-                concat!(#artifact_name, ":", #artifact_version).parse().unwrap()
-            }
-
-            fn artifact_protobuf_spec(&self) -> #cr::runtime::ArtifactProtobufSpec {
-                #cr::runtime::ArtifactProtobufSpec {
-                    sources: #proto_sources_mod::PROTO_SOURCES.as_ref(),
-                }
-            }
-
-            fn create_instance(&self) -> Box<dyn #cr::runtime::rust::Service> {
-                #service_constructor
-            }
-        }
-    };
-
-    expanded.into()
+    let service_factory = ServiceFactory::from_derive_input(&input)
+        .unwrap_or_else(|e| panic!("ServiceFactory: {}", e));
+    let tokens = quote! {#service_factory};
+    tokens.into()
 }
