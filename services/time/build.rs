9cedbfdccca4eb32d2ca7674c0642cab7d61be5f
Igor Aleksanov <popzxc@yandex.ru>
2019-09-05 10:22:43 +0300

    Add the service interface name option to the proto files

--- services/time/build.rs
+++ services/time/build.rs
@@ -1,13 +1,15 @@
 extern crate exonum_build;
 
-use exonum_build::protobuf_generate;
+use exonum_build::{get_exonum_protobuf_files_path, protobuf_generate};
 
 fn main() {
-    protobuf_generate("src/proto", &["src/proto"], "protobuf_mod.rs");
+    let exonum_protos = get_exonum_protobuf_files_path();
+
+    protobuf_generate("src/proto", &["src/proto", &exonum_protos], "protobuf_mod.rs");
 
     protobuf_generate(
         "examples/simple_service/proto",
-        &["examples/simple_service/proto"],
+        &["src/proto", "examples/simple_service/proto", &exonum_protos],
         "simple_service_protobuf_mod.rs",
     );
 }
