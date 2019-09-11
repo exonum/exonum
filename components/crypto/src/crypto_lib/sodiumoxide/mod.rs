84821c3a5ab7964159f118a69bbc24c780fb7b90
pavel-mukhanov <mukhanovpv@gmail.com>
2019-09-11 08:44:48 +0300

    Fix build.

--- components/crypto/src/crypto_lib/sodiumoxide/mod.rs
+++ components/crypto/src/crypto_lib/sodiumoxide/mod.rs
@@ -63,6 +63,12 @@ use self::sodiumoxide::crypto::{hash::sha256, sign::ed25519};
 pub mod kx;
 pub mod x25519;
 
+/// Hash of an empty slice.
+pub const EMPTY_SLICE_HASH: Hash = Hash([
+    227, 176, 196, 66, 152, 252, 28, 20, 154, 251, 244, 200, 153, 111, 185, 36, 39, 174, 65, 228,
+    100, 155, 147, 76, 164, 149, 153, 27, 120, 82, 184, 85,
+]);
+
 /// Number of bytes in a `Hash`.
 pub const HASH_SIZE: usize = sha256::DIGESTBYTES;
 
