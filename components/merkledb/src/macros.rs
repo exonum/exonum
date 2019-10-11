29d450c944363078bfd4d02a0e28f769b570f439
Pavel Mukhanov <pavel.mukhanov@xdev.re>
2019-10-11 11:38:16 +0300

    Update components/merkledb/src/macros.rs
    Co-Authored-By: Igor Aleksanov <popzxc@yandex.ru>

--- components/merkledb/src/macros.rs
+++ components/merkledb/src/macros.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Macros useful for working with types that implementing `BinaryKey` and `BinaryValue` traits.
+//! Macros useful for work with types that implement `BinaryKey` and `BinaryValue` traits.
 
 /// Fast concatenation of byte arrays and/or keys that implements
 /// `BinaryKey` trait.
