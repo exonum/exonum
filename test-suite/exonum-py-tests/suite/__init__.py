c7eed886e56d143ef8a03ea1d7d3b8866293b783
Igor Aleksanov <popzxc@yandex.ru>
2019-11-01 13:21:16 +0300

    Integration test python lib (#1516)

--- test-suite/exonum-py-tests/suite/__init__.py
+++ test-suite/exonum-py-tests/suite/__init__.py
@@ -1,1 +1,4 @@
 """Entry point of Python Exonum test library"""
+from suite.exonum import ExonumNetwork
+from suite.process_manager import ProcessOutput, ProcessExitResult
+from suite.common import *
