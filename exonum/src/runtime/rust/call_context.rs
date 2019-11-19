da7d1bc8c9b6d2b3066b46a3ddf8a9808d28233b
Igor Aleksanov <popzxc@yandex.ru>
2019-11-19 11:33:31 +0300

    Remove isolate method [ECR-3820] (#1550)

--- exonum/src/runtime/rust/call_context.rs
+++ exonum/src/runtime/rust/call_context.rs
@@ -3,7 +3,6 @@ use exonum_merkledb::{access::Prefixed, BinaryValue, Fork};
 use crate::blockchain::Schema as CoreSchema;
 use crate::runtime::{
     dispatcher::{Dispatcher, Error as DispatcherError},
-    error::catch_panic,
     ArtifactId, BlockchainData, CallInfo, Caller, ExecutionContext, ExecutionError,
     InstanceDescriptor, InstanceId, InstanceQuery, InstanceSpec, MethodId, SUPERVISOR_INSTANCE_ID,
 };
@@ -92,47 +91,6 @@ impl<'a> CallContext<'a> {
             inner: self.inner.child_context(self.instance.id),
             instance: descriptor,
         })
-    }
-
-    /// Isolates execution of the provided closure.
-    ///
-    /// This method should be used with extreme care, since it subverts the usual rules
-    /// of transaction roll-back provided by Exonum. Namely:
-    ///
-    /// - If the execution of the closure is successful, all changes to the blockchain state
-    ///   preceding the `isolate()` call are permanently committed. These changes **will not**
-    ///   be rolled back if the following transaction code exits with an error.
-    /// - If the execution of the closure errors, all changes to the blockchain state
-    ///   preceding the `isolate()` call are rolled back right away. That is, they are not
-    ///   persisted even if the following transaction code executes successfully.
-    ///
-    /// If there are several `isolate()` calls within the same execution context,
-    /// commitment / rollback rules are applied to changes since the last call.
-    ///
-    /// For these reasons, it is strongly advised to:
-    ///
-    /// - Make `isolate()` call(s) the last logic executed by a transaction, or at least
-    ///   not have failure cases after the call(s).
-    /// - Propagate errors returned by this method as a result of the transaction execution.
-    // TODO: Finalize interface and test [ECR-3740]
-    #[doc(hidden)]
-    pub fn isolate(
-        &mut self,
-        f: impl FnOnce(CallContext<'_>) -> Result<(), ExecutionError>,
-    ) -> Result<(), ExecutionError> {
-        let result = catch_panic(|| f(self.reborrow()));
-        match result {
-            Ok(()) => self.inner.fork.flush(),
-            Err(_) => self.inner.fork.rollback(),
-        }
-        result
-    }
-
-    fn reborrow(&mut self) -> CallContext<'_> {
-        CallContext {
-            inner: self.inner.reborrow(),
-            instance: self.instance,
-        }
     }
 
     /// Provides writeable access to core schema.
