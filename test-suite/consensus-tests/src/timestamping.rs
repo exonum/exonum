741c45f13a149903c62e57a5b040cf57d860e5d6
Alex Ostrovski <ostrovski.alex@gmail.com>
2019-12-18 14:41:27 +0200

    Remove obsolete Protobuf types

--- test-suite/consensus-tests/src/timestamping.rs
+++ test-suite/consensus-tests/src/timestamping.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub use crate::proto::TimestampTx;
-
 use exonum::{
     blockchain::ExecutionError,
     crypto::{gen_keypair, Hash, PublicKey, SecretKey, HASH_SIZE},
