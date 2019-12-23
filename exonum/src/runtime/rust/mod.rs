221c1afc0f8ce82984766ac0286958c8d1bbb856
Renat Mukhametzanov <renat.mukhametzanov@xdev.re>
2019-12-23 16:58:06 +0300

    Removed closure

--- exonum/src/runtime/rust/mod.rs
+++ exonum/src/runtime/rust/mod.rs
@@ -449,7 +449,7 @@ impl RustRuntime {
 
         self.available_artifacts
             .keys()
-            .map(|id| id.to_string())
+            .map(ToString::to_string)
             .collect::<Vec<String>>()
             .join(", ")
     }
