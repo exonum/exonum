385c7289caf5ae80b28e7fb989d1dd4d94e3c7ef
Renat Mukhametzanov <renat.mukhametzanov@xdev.re>
2020-01-06 16:50:23 +0300

    Fixed scopes in manual_migration

--- components/merkledb/examples/manual_migration.rs
+++ components/merkledb/examples/manual_migration.rs
@@ -40,21 +40,23 @@ mod migration;
 fn create_migration(fork: &Fork) {
     println!("\nStarted migration");
 
-    let new_data = Migration::new("test", fork);
-    let old_data = Prefixed::new("test", fork.readonly());
+    {
+        let new_data = Migration::new("test", fork.clone());
+        let old_data = Prefixed::new("test", fork.readonly());
 
-    let old_schema = v1::Schema::new(old_data);
-    let mut new_schema = v2::Schema::new(new_data);
+        let old_schema = v1::Schema::new(old_data);
+        let mut new_schema = v2::Schema::new(new_data);
 
-    // Move `ticker` and `divisibility` to `config`.
-    let config = v2::Config {
-        ticker: old_schema.ticker.get().unwrap(),
-        divisibility: old_schema.divisibility.get().unwrap_or(0),
-    };
-    new_schema.config.set(config);
-    // Mark these two indexes for removal.
-    new_data.create_tombstone("ticker");
-    new_data.create_tombstone("divisibility");
+        // Move `ticker` and `divisibility` to `config`.
+        let config = v2::Config {
+            ticker: old_schema.ticker.get().unwrap(),
+            divisibility: old_schema.divisibility.get().unwrap_or(0),
+        };
+        new_schema.config.set(config);
+        // Mark these two indexes for removal.
+        new_data.create_tombstone("ticker");
+        new_data.create_tombstone("divisibility");
+    }
 
     // Migrate wallets using schema:
     // `Wallet::public_key` field will be removed.
