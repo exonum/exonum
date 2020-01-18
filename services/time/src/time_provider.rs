34dea45d98ec8c2b3178265df22e3dcba3ea7337
Igor Aleksanov <popzxc@yandex.ru>
2020-01-18 16:31:50 +0300

    I am stupid (and lazy)

--- services/time/src/time_provider.rs
+++ services/time/src/time_provider.rs
@@ -73,7 +73,7 @@ impl TimeProvider for SystemTimeProvider {
 /// ```
 ///
 /// [`Arc`]: https://doc.rust-lang.org/std/sync/struct.Arc.html
-/// [`TimeService`]: ../struct.TimeService.html
+/// [`TimeService`]: struct.TimeService.html
 #[derive(Debug, Clone)]
 pub struct MockTimeProvider {
     /// Local time value.
