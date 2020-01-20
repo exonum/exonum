6ccc9ed6d72aff5ea86a518abee52478f514e0f5
Renat Mukhametzanov <renat.mukhametzanov@xdev.re>
2020-01-20 17:06:46 +0300

    Removed redundant dependencies

--- services/supervisor/tests/supervisor/main.rs
+++ services/supervisor/tests/supervisor/main.rs
@@ -22,8 +22,7 @@ use exonum::{
         SUPERVISOR_INSTANCE_ID,
     },
 };
-use exonum_api::Result as ApiResult;
-use exonum_rust_runtime::{RustRuntimeBuilder, ServiceFactory};
+use exonum_rust_runtime::{api, RustRuntimeBuilder, ServiceFactory};
 use exonum_supervisor::{
     ArtifactError, CommonError as SupervisorCommonError, ConfigPropose, DeployRequest,
     DeployResult, ServiceError, Supervisor, SupervisorInterface,
@@ -61,7 +60,7 @@ fn assert_count(api: &TestKitApi, service_name: &'static str, expected_count: u6
 
 /// Check that the service's counter isn't started yet (no Inc txs were received).
 fn assert_count_is_not_set(api: &TestKitApi, service_name: &'static str) {
-    let response: ApiResult<u64> = api.public(ApiKind::Service(service_name)).get("v1/counter");
+    let response: api::Result<u64> = api.public(ApiKind::Service(service_name)).get("v1/counter");
     assert!(response.is_err());
 }
 
