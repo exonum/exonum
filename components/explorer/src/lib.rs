6e6a2e780f2fa14625ceb494f015a76957f2442c
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2020-01-21 18:26:32 +0200

    Content -> payload in the description

--- components/explorer/src/lib.rs
+++ components/explorer/src/lib.rs
@@ -489,7 +489,7 @@ impl CommittedTransaction {
 /// Transactions in pool are represented with a 2-field object:
 ///
 /// - `type` field contains transaction type (`"in-pool"`).
-/// - `message` is the transaction contents serialized to the hexadecimal form.
+/// - `message` is the transaction payload.
 ///
 /// # Examples
 ///
