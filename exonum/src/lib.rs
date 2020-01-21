71bf1ef443ee4e1fac307330f620db949d51ceb2
Alex Ostrovski <ostrovski.alex@gmail.com>
2020-01-21 16:57:13 +0200

    Improve crate-level docs

--- exonum/src/lib.rs
+++ exonum/src/lib.rs
@@ -12,9 +12,66 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Exonum blockchain framework.
+//! Core library for the Exonum blockchain framework.
 //!
-//! For more information see the project readme.
+//! Exonum is an extensible open-source framework for
+//! creating blockchain applications. Exonum can be used to create cryptographically
+//! powered distributed ledgers in virtually any problem domain, including finance,
+//! governance, and legal. The Exonum framework is oriented towards creating
+//! permissioned blockchains, that is, blockchains with the known set of blockchain
+//! infrastructure providers.
+//!
+//! For more information about the framework see the [readme] and the [Exonum website].
+//!
+//! [readme]: https://github.com/exonum/exonum#readme
+//! [Exonum website]: https://exonum.com/
+//!
+//! # Crate Overview
+//!
+//! This crate provides the tools necessary to work with the Exonum framework.
+//!
+//! ## Re-exports
+//!
+//! The crate re-exports the following crates:
+//!
+//! | Crate | Exported name | Description |
+//! |-------|---------------|-------------|
+//! | [`exonum-crypto`] | `crypto` | Cryptographic utils used by Exonum |
+//! | [`exonum-merkledb`] | `merkledb` | Storage engine with automatic data merkelization |
+//! | [`exonum-keys`] | `keys` | Key tools for Exonum nodes |
+//!
+//! [`exonum-crypto`]: https://docs.rs/exonum-crypto/
+//! [`exonum-merkledb`]: https://docs.rs/exonum-merkledb/
+//! [`exonum-keys`]: https://docs.rs/exonum-keys/
+//!
+//! ## Blockchain Management
+//!
+//! The crate provides basic tools to build Exonum nodes ([`blockchain`] and [`messages`] modules),
+//! although the bulk of the node logic is placed [in a dedicated crate][`exonum-node`].
+//!
+//! [`blockchain`]: blockchain/index.html
+//! [`messages`]: messages/index.html
+//! [`exonum-node`]: https://docs.rs/exonum-node/
+//!
+//! ## Runtimes
+//!
+//! [Runtimes] are a way to attach user-provided business logic to an Exonum blockchain. This
+//! logic, bundled in *services*, allows to process user transactions and interact with
+//! the blockchain in other ways (e.g., via HTTP API).
+//!
+//! Exonum provides a [generic interface][`Runtime`] for runtimes, which allows to implement
+//! services in different programming languages, for example [Rust][rust-rt] and [Java][java-rt].
+//!
+//! [Runtimes]: runtime/index.html
+//! [`Runtime`]: runtime/trait.Runtime.html
+//! [rust-rt]: https://docs.rs/exonum-rust-runtime/
+//! [java-rt]: https://github.com/exonum/exonum-java-binding
+//!
+//! # Examples
+//!
+//! See the [GitHub repository][examples] for examples.
+//!
+//! [examples]: https://github.com/exonum/exonum/tree/master/examples
 
 #![warn(
     missing_debug_implementations,
