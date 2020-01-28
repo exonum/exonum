734dd0a636a8a52373f037ec3924856d659abd44
SergNikolaev <iintersergey@gmail.com>
2020-01-28 19:44:43 +0700

    cryptocurrency-advanced update to 0.13 without proofs

--- examples/cryptocurrency-advanced/frontend/src/plugins/blockchain.js
+++ examples/cryptocurrency-advanced/frontend/src/plugins/blockchain.js
@@ -4,93 +4,83 @@ import * as proto from '../../proto/stubs.js'
 
 const TRANSACTION_URL = '/api/explorer/v1/transactions'
 const PER_PAGE = 10
-const SERVICE_ID = 128
+const SERVICE_ID = 1
 const TX_TRANSFER_ID = 0
 const TX_ISSUE_ID = 1
 const TX_WALLET_ID = 2
-const TABLE_INDEX = 0
-const Wallet = Exonum.newType(proto.exonum.examples.cryptocurrency_advanced.Wallet)
 
-function TransferTransaction(publicKey) {
-  return Exonum.newTransaction({
-    author: publicKey,
-    service_id: SERVICE_ID,
-    message_id: TX_TRANSFER_ID,
+function TransferTransaction () {
+  return new Exonum.Transaction({
+    serviceId: SERVICE_ID,
+    methodId: TX_TRANSFER_ID,
     schema: proto.exonum.examples.cryptocurrency_advanced.Transfer
   })
 }
 
-function IssueTransaction(publicKey) {
-  return Exonum.newTransaction({
-    author: publicKey,
-    service_id: SERVICE_ID,
-    message_id: TX_ISSUE_ID,
-    schema: proto.exonum.examples.cryptocurrency_advanced.Issue
+function IssueTransaction () {
+  return new Exonum.Transaction({
+    schema: proto.exonum.examples.cryptocurrency_advanced.Issue,
+    serviceId: SERVICE_ID,
+    methodId: TX_ISSUE_ID
   })
 }
 
-function CreateTransaction(publicKey) {
-  return Exonum.newTransaction({
-    author: publicKey,
-    service_id: SERVICE_ID,
-    message_id: TX_WALLET_ID,
-    schema: proto.exonum.examples.cryptocurrency_advanced.CreateWallet
+function WalletTx () {
+  return new Exonum.Transaction({
+    schema: proto.exonum.examples.cryptocurrency_advanced.CreateWallet,
+    serviceId: SERVICE_ID,
+    methodId: TX_WALLET_ID
   })
 }
 
-function getTransaction(transaction, publicKey) {
-  if (transaction.name) {
-    return new CreateTransaction(publicKey)
+function deserializeWalletTx (transaction) {
+  const txTypes = [TransferTransaction(), IssueTransaction()]
+  for (const tx of txTypes) {
+    const txData = tx.deserialize(Exonum.hexadecimalToUint8Array(transaction))
+    if (txData) return Object.assign({}, txData.payload, {
+      hash: txData.hash(),
+      to: txData.payload.to ? Exonum.uint8ArrayToHexadecimal(txData.payload.to.data) : undefined
+    })
   }
-
-  if (transaction.to) {
-    return new TransferTransaction(publicKey)
-  }
-
-  return new IssueTransaction(publicKey)
+  return { name: 'initialTx' }
 }
 
 module.exports = {
-  install(Vue) {
+  install (Vue) {
     Vue.prototype.$blockchain = {
-      generateKeyPair() {
+      generateKeyPair () {
         return Exonum.keyPair()
       },
 
-      generateSeed() {
+      generateSeed () {
         return Exonum.randomUint64()
       },
 
-      createWallet(keyPair, name) {
-        // Describe transaction
-        const transaction = new CreateTransaction(keyPair.publicKey)
-
-        // Transaction data
-        const data = {
-          name: name
-        }
-
+      createWallet (keyPair, name) {
+        const walletTx = WalletTx()
+        const transaction = walletTx.create({ name }, keyPair).serialize()
         // Send transaction into blockchain
-        return transaction.send(TRANSACTION_URL, data, keyPair.secretKey)
+        return Exonum.send(TRANSACTION_URL, transaction)
       },
 
-      addFunds(keyPair, amountToAdd, seed) {
+      addFunds (keyPair, amountToAdd, seed) {
         // Describe transaction
-        const transaction = new IssueTransaction(keyPair.publicKey)
+        const issueTx = IssueTransaction()
 
         // Transaction data
         const data = {
           amount: amountToAdd.toString(),
           seed: seed
         }
+        const transaction = issueTx.create(data, keyPair).serialize()
 
         // Send transaction into blockchain
-        return transaction.send(TRANSACTION_URL, data, keyPair.secretKey)
+        return Exonum.send(TRANSACTION_URL, transaction)
       },
 
-      transfer(keyPair, receiver, amountToTransfer, seed) {
+      transfer (keyPair, receiver, amountToTransfer, seed) {
         // Describe transaction
-        const transaction = new TransferTransaction(keyPair.publicKey)
+        const transferTx = TransferTransaction()
 
         // Transaction data
         const data = {
@@ -98,105 +88,43 @@ module.exports = {
           amount: amountToTransfer,
           seed: seed
         }
+        const transaction = transferTx.create(data, keyPair).serialize()
 
         // Send transaction into blockchain
-        return transaction.send(TRANSACTION_URL, data, keyPair.secretKey)
+        return Exonum.send(TRANSACTION_URL, transaction)
       },
 
-      getWallet(publicKey) {
-        return axios.get('/api/services/configuration/v1/configs/actual').then(response => {
+      getWallet (publicKey) {
+        return axios.get('/api/services/supervisor/consensus-config').then(response => {
           // actual list of public keys of validators
-          const validators = response.data.config.validator_keys.map(validator => {
+          const validators = response.data.validator_keys.map(validator => {
             return validator.consensus_key
           })
 
-          return axios.get(`/api/services/cryptocurrency/v1/wallets/info?pub_key=${publicKey}`)
+          return axios.get(`/api/services/crypto/v1/wallets/info?pub_key=${publicKey}`)
             .then(response => response.data)
             .then(data => {
-              return Exonum.verifyBlock(data.block_proof, validators).then(() => {
-                // verify table timestamps in the root tree
-                const tableRootHash = Exonum.verifyTable(data.wallet_proof.to_table, data.block_proof.block.state_hash, SERVICE_ID, TABLE_INDEX)
+              const transactions = data.wallet_history.transactions.map(deserializeWalletTx)
 
-                // find wallet in the tree of all wallets
-                const walletProof = new Exonum.MapProof(data.wallet_proof.to_wallet, Exonum.PublicKey, Wallet)
-                if (walletProof.merkleRoot !== tableRootHash) {
-                  throw new Error('Wallet proof is corrupted')
-                }
-                const wallet = walletProof.entries.get(publicKey)
-                if (typeof wallet === 'undefined') {
-                  throw new Error('Wallet not found')
-                }
-
-                // get transactions
-                const transactionsMetaData = Exonum.merkleProof(
-                  Exonum.uint8ArrayToHexadecimal(new Uint8Array(wallet.history_hash.data)),
-                  wallet.history_len,
-                  data.wallet_history.proof,
-                  [0, wallet.history_len],
-                  Exonum.Hash
-                )
-
-                if (data.wallet_history.transactions.length !== transactionsMetaData.length) {
-                  // number of transactions in wallet history is not equal
-                  // to number of transactions in array with transactions meta data
-                  throw new Error('Transactions can not be verified')
-                }
-
-                // validate each transaction
-                const transactions = []
-                let index = 0
-
-                for (let transaction of data.wallet_history.transactions) {
-                  const hash = transactionsMetaData[index++]
-                  const buffer = Exonum.hexadecimalToUint8Array(transaction.message)
-                  const bufferWithoutSignature = buffer.subarray(0, buffer.length - 64)
-                  const author = Exonum.uint8ArrayToHexadecimal(buffer.subarray(0, 32))
-                  const signature = Exonum.uint8ArrayToHexadecimal(buffer.subarray(buffer.length - 64, buffer.length));
-
-                  const Transaction = getTransaction(transaction.debug, author)
-
-                  if (Exonum.hash(buffer) !== hash) {
-                    throw new Error('Invalid transaction hash')
-                  }
-
-                  // serialize transaction and compare with message
-                  if (!Transaction.serialize(transaction.debug).every(function (el, i) {
-                    return el === bufferWithoutSignature[i]
-                  })) {
-                    throw new Error('Invalid transaction message')
-                  }
-
-                  if (!Transaction.verifySignature(signature, author, transaction.debug)) {
-                    throw new Error('Invalid transaction signature')
-                  }
-
-                  const transactionData = Object.assign({ hash: hash }, transaction.debug)
-                  if (transactionData.to) {
-                    transactionData.to = Exonum.uint8ArrayToHexadecimal(new Uint8Array(transactionData.to.data))
-                  }
-                  transactions.push(transactionData)
-                }
-
-                return {
-                  block: data.block_proof.block,
-                  wallet: wallet,
-                  transactions: transactions
-                }
-              })
+              return {
+                block: data.block_proof.block,
+                wallet: {},
+                transactions: transactions
+              }
             })
         })
       },
 
-      getBlocks(latest) {
+      getBlocks (latest) {
         const suffix = !isNaN(latest) ? '&latest=' + latest : ''
         return axios.get(`/api/explorer/v1/blocks?count=${PER_PAGE}${suffix}`).then(response => response.data)
       },
 
-      getBlock(height) {
+      getBlock (height) {
         return axios.get(`/api/explorer/v1/block?height=${height}`).then(response => response.data)
       },
 
-      getTransaction(hash) {
+      getTransaction (hash) {
         return axios.get(`/api/explorer/v1/transactions?hash=${hash}`).then(response => response.data)
       }
     }
