153c431edcb6b8ec6057602a3a6141f5847c3702
Sergey Nikolaev <iintersergey@gmail.com>
2020-02-11 21:41:06 +0700

    FE timestamping demo update to master (#1774)

--- examples/timestamping/frontend/tests/blockchain.test.js
+++ examples/timestamping/frontend/tests/blockchain.test.js
@@ -3,8 +3,6 @@ import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import * as Blockchain from '../src/plugins/blockchain.js'
 import actual from './data/actual.json'
-import txNotAccepted from './data/not-accepted.json'
-import txAccepted from './data/accepted.json'
 import proof from './data/proof.json'
 import 'babel-polyfill';
 
@@ -13,17 +11,9 @@ const hexRegex = /[0-9A-Fa-f]+/i
 
 Vue.use(Blockchain)
 
-mock.onPost('/api/explorer/v1/transactions', {
-  tx_body: '25f7aad8046db3ce0cbbb8aa5444383305b4e2003fae578f57243a926148c5ae0000820000000a330a220a200cf4fd1634bcb6e0cfc0ae0e111931747c81f613534a66be8f8ec0eb0ed1acf9120d536f6d6520636f6e74726163740ad40aa875ae16524bd2ba51acbd804776e174efcb934dfb602aa9981a6361883c57571de141f8028e8f009c23f6567b985064c5e5b42a64c613ded2b1a89a0d'
-}).reply(200)
-
-mock.onGet('/api/explorer/v1/transactions?hash=ce0743cff6bdef0afa2f3af68b77612aa8f22190d3e94534268e3b784cfd7805').replyOnce(200, txNotAccepted)
-
-mock.onGet('/api/explorer/v1/transactions?hash=ce0743cff6bdef0afa2f3af68b77612aa8f22190d3e94534268e3b784cfd7805').replyOnce(200, txAccepted)
-
-mock.onGet('/api/services/configuration/v1/configs/actual').reply(200, actual)
-
-mock.onGet('/api/services/timestamping/v1/timestamps/proof?hash=67fa96da3465c77befabd75b53027e39b35c79d0ed69a175fff5141516353cc3').reply(200, proof)
+const hash = '4ee9e65a836e525042b22f8270f68b4e1fbd18ac969cd12dea24093380031eae'
+mock.onGet('/api/services/supervisor/consensus-config').reply(200, actual)
+mock.onGet(`/api/services/timestamping/v1/timestamps/proof?hash=${hash}`).reply(200, proof)
 
 describe('Interaction with blockchain', () => {
   it('should generate new signing key pair', () => {
@@ -47,18 +37,16 @@ describe('Interaction with blockchain', () => {
   })
 
   it('should get timestamp proof and verify it', async () => {
-    const hash = '67fa96da3465c77befabd75b53027e39b35c79d0ed69a175fff5141516353cc3'
+    const data = await Vue.prototype.$blockchain.getTimestampProof(hash)
 
     await expect(Vue.prototype.$blockchain.getTimestampProof(hash)).resolves.toEqual({
-      'time': {
-        'nanos': 93835000,
-        'seconds': 1565701488
-      },
-      'timestamp': {
-        'content_hash': '67fa96da3465c77befabd75b53027e39b35c79d0ed69a175fff5141516353cc3',
-        'metadata': 'Test data'
-      },
-      'tx_hash': '787f027f0f3e0e677f0de15163a7bcd4aacec49d960c5dac4fd31b3edb1a9092'
-    })
+        timestamp: {
+          content_hash: '4ee9e65a836e525042b22f8270f68b4e1fbd18ac969cd12dea24093380031eae',
+          metadata: 'hello world'
+        },
+        tx_hash: 'e5643f7a630573786fecc536a289b1c9689c9fe85e924f1cadb3115d5377d350',
+        time: { seconds: 1581090568, nanos: 112863000 }
+      }
+    )
   })
 })
