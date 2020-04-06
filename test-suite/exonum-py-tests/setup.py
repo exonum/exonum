750196a849452446f5d2d40449180b90e070d91c
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2020-04-06 12:56:17 +0300

    Update exonum-launcher version as the python tests dependency

--- test-suite/exonum-py-tests/setup.py
+++ test-suite/exonum-py-tests/setup.py
@@ -2,7 +2,7 @@
 """Setup Script for the Exonum Python Integration Tests."""
 import setuptools
 
-INSTALL_REQUIRES = ["exonum-launcher==0.1.3"]
+INSTALL_REQUIRES = ["exonum-launcher==0.2.0"]
 
 PYTHON_REQUIRES = ">=3.6"
 
